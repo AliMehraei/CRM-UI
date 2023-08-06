5acbf767861b8fb90ddf0caac2615ea3be3c2be1
davood.thinker <davood.thinker@gmail.com>
2023-08-06 11:12:18 +0330

    feat:done with adding tickets

--- resources/js/src/pages/Support/Add.tsx
+++ resources/js/src/pages/Support/Add.tsx
@@ -5,6 +5,10 @@ import { setPageTitle } from '../../store/themeConfigSlice';
 import Select from 'react-select';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import 'file-upload-with-preview/dist/file-upload-with-preview.min.css';
+import ImageUploading, { ImageListType } from 'react-images-uploading';
+
+
 
 const Add = () => {
     const dispatch = useDispatch();
@@ -14,6 +18,11 @@ const Add = () => {
     const clinetList = [{value: '1', label: 'Example Client 1'}, {value: '2', label: 'Example Client 2'}, {value: '3', label: 'Example Client 3'}, {value: '4', label: 'Example Client 4'}, {value: '5', label: 'Example Client 5'}, {value: '6', label: 'Example Client 6'}, {value: '7', label: 'Example Client 7'}];
     const porjectList = [{value: '1', label: 'Example Poroject 1'}, {value: '2', label: 'Example Poroject 2'}, {value: '3', label: 'Example Poroject 3'}, {value: '4', label: 'Example Poroject 4'}, {value: '5', label: 'Example Poroject 5'}, {value: '6', label: 'Example Poroject 6'}, {value: '7', label: 'Example Poroject 7'}];
 
+    const [images, setImages] = useState<any>([]);
+    const maxNumber = 69;
+    const onChange = (imageList: ImageListType, addUpdateIndex: number[] | undefined) => {
+        setImages(imageList as never[]);
+    };
     const [items, setItems] = useState<any>([
         {
             id: 1,
@@ -98,6 +107,39 @@ const Add = () => {
                             <div className="mt-4">
                             <label htmlFor="massage">Massage</label>
                             <ReactQuill theme="snow" value={value} onChange={setValue} />
+                            </div>
+                            <div className="custom-file-container mt-4" data-upload-id="myFirstImage">
+                                <div className="label-containe">
+                                    <label className='!text-black'>Upload </label>
+                                    <button
+                                        type="button"
+                                        className="custom-file-container__image-clear"
+                                        title="Clear Image"
+                                        onClick={() => {
+                                            setImages([]);
+                                        }}
+                                    >
+                                        ×
+                                    </button>
+                                </div>
+                                <label className="custom-file-container__custom-file"></label>
+                                <input type="file" className="custom-file-container__custom-file__custom-file-input" accept="image/*" />
+                                <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+                                <ImageUploading value={images} onChange={onChange} maxNumber={maxNumber}>
+                                    {({ imageList, onImageUpload, onImageRemoveAll, onImageUpdate, onImageRemove, isDragging, dragProps }) => (
+                                        <div className="upload__image-wrapper">
+                                            <button className="custom-file-container__custom-file__custom-file-control" onClick={onImageUpload}>
+                                                Choose Image...
+                                            </button>
+                                            &nbsp;
+                                            {imageList.map((image, index) => (
+                                                <div key={index} className="custom-file-container__image-preview relative">
+                                                    <img src={image.dataURL} alt="img" className="m-auto" />
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )}
+                                </ImageUploading>
                             </div>         
                         </div>
                     </div>
