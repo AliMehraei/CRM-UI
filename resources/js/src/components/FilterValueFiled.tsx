6f122efec02ba5e4adbbc57fd93b1be778f29bd8
davood.thinker <davood.thinker@gmail.com>
2023-08-11 17:21:11 +0330

    feat: add input number

--- resources/js/src/components/FilterValueFiled.tsx
+++ resources/js/src/components/FilterValueFiled.tsx
@@ -1,16 +1,18 @@
 import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import { usePopper } from 'react-popper';
-export const handleValueChange = (field, value) => {
+const handleValueChange = (field, value) => {
     const updatedFilters = { ...filters };
     updatedFilters[field] = { ...updatedFilters[field], value };
+    console.log(33333,value);
+    
     setFilters(updatedFilters);
 };
 
-export const handleInputValueChange = (field, event) => {
+const handleInputValueChange = (field, event) => {
     handleValueChange(field, event.target.value);
 };
 
-export const handelBetween = (field, event) => {
+const handelBetween = (field, event) => {
     const { name, value } = event.target;
     const existingFilter = filters[field];
     const existingValue = existingFilter ? existingFilter.value : '';
@@ -21,7 +23,7 @@ export const handelBetween = (field, event) => {
     handleValueChange(field, combinedValue);
 };
 
-export const handelDueIn = (field, event) => {
+const handelDueIn = (field, event) => {
     const { name, value } = event.target;
     const existingFilter = filters[field];
     const existingValue = existingFilter ? existingFilter.value : '2_days';
@@ -42,69 +44,36 @@ export const renderFilterValueFiled = (filterSelect, option) => {
     if (!option.condition[condition].input) {
         return null;
     }
+    console.log(2222, option);
     console.log(1111222, type_condition);
     if (type_condition == "number") {
         switch (condition) {
+            
             case 'between':
                 return (
                     <>
-                        <label className="block text-sm text-gray-600">From:</label>
-                        <input type="date" name="from" className="border p-2 w-full" onChange={(e) => handelBetween(option.value, e)} />
-                        <label className="block text-sm text-gray-600">To:</label>
-                        <input type="date" name="to" className="border p-2 w-full" onChange={(e) => handelBetween(option.value, e)} />
-                    </>
-                )
-                break;
-            case 'is_empty':
-            case 'is_not_empty':
-
-                break;
-            case 'in_the_last':
-            case 'due_in':
-                return (
-                    <>
-                        <div className="flex">
-                            <input
-                                type="number"
-                                placeholder='2'
-                                className="border p-2 w-1/2"
-                                min="1"
-                                name='period_val'
-                                onChange={(event) => handelDueIn(option.value, event)}
-                            />
-                            <select
-                                name='period'
-                                className="border p-2 w-1/2"
-                                defaultValue="days"
-                                onChange={(event) => handelDueIn(option.value, event)}
-                            >
-                                <option value="days">Days</option>
-                                <option value="weeks">Weeks</option>
-                                <option value="months">Months</option>
-                            </select>
-                        </div>
+                    <label className="block text-sm text-gray-600">From:</label>
+                    <input type="number" name="from" className="border p-2 w-full" onChange={(e) => handelBetween(option.value, e)} />
+                    <label className="block text-sm text-gray-600">To:</label>
+                    <input type="number" name="to" className="border p-2 w-full" onChange={(e) => handelBetween(option.value, e)} />
                     </>
                 );
-            case 'on':
-            case 'before':
-            case 'after':
+            case 'not_between':
                 return (
                     <>
-                        <input
-                            type="date"
-                            className="border p-2 w-full"
-                            onChange={(e) => handleInputValueChange(option.value, e)}
-                        />
+                    <label className="block text-sm text-gray-600">From:</label>
+                    <input type="number" name="from" className="border p-2 w-full" onChange={(e) => handelBetween(option.value, e)} />
+                    <label className="block text-sm text-gray-600">To:</label>
+                    <input type="number" name="to" className="border p-2 w-full" onChange={(e) => handelBetween(option.value, e)} />
                     </>
                 );
-
             default:
                 return (
                     <>
                         <label className="block font-semibold">Value:</label>
                         <input
-                            type="text"
-                            placeholder={`Search value that contains`}
+                            type="number"
+                            placeholder={`Search value`}
                             className="border p-2 w-full"
                             onChange={(e) => handleInputValueChange(option.value, e)}
                         />
