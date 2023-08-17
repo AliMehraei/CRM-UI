509c73d9a939dc1ecf6abc22499847b4c872d537
davood.thinker <davood.thinker@gmail.com>
2023-08-17 17:38:14 +0330

    style: fixed some style

--- resources/js/src/pages/RFQ/components/HeaderSection.tsx
+++ resources/js/src/pages/RFQ/components/HeaderSection.tsx
@@ -129,23 +129,25 @@ const Header = {
 
 const RFQHeaderSection = () => {
     return (<>
-        {Object.entries(Header).map(([sectionTitle, sectionContent], index) => (
-            <div className="lg:w-1/2 w-full ltr:lg:mr-6 rtl:lg:ml-6 mb-6">
-                <div key={sectionTitle} className="">
-                    <div className="text-lg">{sectionTitle ? `${sectionTitle}:` :
-                        <div className="mt-10"></div>}</div>
-                    {Object.entries(sectionContent).map(([label, inputComponent], index) => (
-                        <div key={label} className="mt-4 flex items-center">
-                            <label className="ltr:mr-2 rtl:ml-2 w-1/3 mb-0">
-                                {label}
-                                {inputComponent.props.required && <RequiredComponent/>}
-                            </label>
-                            {inputComponent}
-                        </div>
-                    ))}
+        <div className="flex justify-between lg:flex-row flex-col">
+            {Object.entries(Header).map(([sectionTitle, sectionContent], index) => (
+                <div className="lg:w-1/2 w-full ltr:lg:mr-6 rtl:lg:ml-6 mb-6">
+                    <div key={sectionTitle} className="">
+                        <div className="text-lg">{sectionTitle ? `${sectionTitle}:` :
+                            <div className="mt-10"></div>}</div>
+                        {Object.entries(sectionContent).map(([label, inputComponent], index) => (
+                            <div key={label} className="mt-4 flex items-center">
+                                <label className="ltr:mr-2 rtl:ml-2 w-1/3 mb-0">
+                                    {label}
+                                    {inputComponent.props.required && <RequiredComponent/>}
+                                </label>
+                                {inputComponent}
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        ))}
+            ))}
+        </div>
     </>)
 }
 
