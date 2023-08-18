673de5bb2a971101b4e5f35cec3f17d0e70560a0
davood.thinker <davood.thinker@gmail.com>
2023-08-18 10:19:00 +0330

    fix: fixed the route for quotes add

--- resources/js/src/pages/Quotes/Index.tsx
+++ resources/js/src/pages/Quotes/Index.tsx
@@ -244,7 +244,7 @@ const List = () => {
                             </svg>
                             Delete
                         </button>
-                        <Link to="/account/add" className="btn btn-primary gap-2">
+                        <Link to="/quotes/add" className="btn btn-primary gap-2">
                             <svg className="w-5 h-5" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                 <line x1="12" y1="5" x2="12" y2="19"></line>
                                 <line x1="5" y1="12" x2="19" y2="12"></line>
@@ -415,7 +415,7 @@ const List = () => {
                                 paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
                             />
                         </div>
-                    
+
                     </div>
                 </div>
             </div>
