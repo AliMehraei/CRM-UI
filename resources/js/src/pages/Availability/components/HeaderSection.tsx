25506e6ba980b821f8d9777f1d8749b83b11eb48
davood.thinker <davood.thinker@gmail.com>
2023-08-18 16:44:28 +0330

    fix: fixed import and typo mistake for select

--- resources/js/src/pages/Availability/components/HeaderSection.tsx
+++ resources/js/src/pages/Availability/components/HeaderSection.tsx
@@ -1,6 +1,6 @@
 import AsyncSelect from "react-select/async";
 import Select from "react-select";
-import {RequiredComponent} from "../../../../components/FormFields/RequiredComponent";
+import {RequiredComponent} from "../../../components/FormFields/RequiredComponent";
 
 const HeaderSection = () => {
 
@@ -52,35 +52,36 @@ const HeaderSection = () => {
                                         className="form-input file:py-2 file:px-4 file:border-0 file:font-semibold p-0 file:bg-primary/90 ltr:file:mr-5 rtl:file:ml-5 file:text-white file:hover:bg-primary flex-1"
                                         accept="image/*,.zip,.pdf,.xls,.xlsx,.txt.doc,.docx"
             />,
-            'Availability Source': <Selectr id="availability_source" name="availability_source" required
-                                           options={AvailabilitySources}
-                                           onChange={handleChangeAvailabilitySources} className="flex-1"/>,
+            'Availability Source': <Select id="availability_source" name="availability_source" required
+                                            options={AvailabilitySources}
+                                            onChange={handleChangeAvailabilitySources} className="flex-1"/>,
         }
 
     }
 
 
     return (<>
-        <div className="flex justify-between lg:flex-row flex-col">
-            {Object.entries(fields).map(([sectionTitle, sectionContent], index) => (
-                <div className="lg:w-1/2 w-full ltr:lg:mr-6 rtl:lg:ml-6 mb-6">
-                    <div key={sectionTitle} className="">
-                        <div className="text-lg">{sectionTitle ? `${sectionTitle}:` :
-                            <div className="mt-10"></div>}</div>
-                        {Object.entries(sectionContent).map(([label, inputComponent], index) => (
-                            <div key={label} className="mt-4 flex items-center">
-                                <label className="ltr:mr-2 rtl:ml-2 w-1/3 mb-0">
-                                    {label}
-                                    {inputComponent.props.required && <RequiredComponent/>}
-                                </label>
-                                {inputComponent}
-                            </div>
-                        ))}
+            <div className="flex justify-between lg:flex-row flex-col">
+                {Object.entries(fields).map(([sectionTitle, sectionContent], index) => (
+                    <div className="lg:w-1/2 w-full ltr:lg:mr-6 rtl:lg:ml-6 mb-6">
+                        <div key={sectionTitle} className="">
+                            <div className="text-lg">{sectionTitle ? `${sectionTitle}:` :
+                                <div className="mt-10"></div>}</div>
+                            {Object.entries(sectionContent).map(([label, inputComponent], index) => (
+                                <div key={label} className="mt-4 flex items-center">
+                                    <label className="ltr:mr-2 rtl:ml-2 w-1/3 mb-0">
+                                        {label}
+                                        {inputComponent.props.required && <RequiredComponent/>}
+                                    </label>
+                                    {inputComponent}
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
-    </>)
+                ))}
+            </div>
+        </>
+    )
 
 }
 
