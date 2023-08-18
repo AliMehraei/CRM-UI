4fd290bffef8f133a664c771744f59a4c0daea57
davood.thinker <davood.thinker@gmail.com>
2023-08-18 10:25:58 +0330

    feat: added QuotesFormFields

---
+++ resources/js/src/pages/Quotes/components/create/QuotesFormFields.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const QuotesFormFields = () => {
+    return (
+        <div className="mt-8 px-4">
+
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+        </div>
+    )
+
+
+}
+
+export default QuotesFormFields;
