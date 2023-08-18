7a5de1c88185a41f5255cc4843603e76c2425bda
davood.thinker <davood.thinker@gmail.com>
2023-08-18 16:28:54 +0330

    feat: added Header Section

--- resources/js/src/pages/Availability/components/AvailabilityFormFields.tsx
+++ resources/js/src/pages/Availability/components/AvailabilityFormFields.tsx
@@ -1,12 +1,5 @@
 import React from "react";
 import HeaderSection from "./HeaderSection";
-import StatusSection from "./StatusSection";
-import QuoteLineSection from "./QuoteLineSection";
-import SourceSection from "./SourceSection";
-import AddressInformationSection from "./AddressInformationSection";
-import TermsAndConditionsSection from "./TermsAndConditionsSection";
-import DescriptionInformationSection from "./DescriptionInformationSection";
-import QuoteItemSection from "./QuoteItemSection";
 
 
 const AvailabilityFormFields = () => {
@@ -14,21 +7,6 @@ const AvailabilityFormFields = () => {
         <div className="mt-8 px-4">
             <HeaderSection/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <StatusSection/>
-            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <QuoteItemSection/>
-            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <QuoteLineSection/>
-            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <SourceSection/>
-            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <AddressInformationSection/>
-            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <TermsAndConditionsSection/>
-            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <DescriptionInformationSection/>
-            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-
         </div>
     )
 
