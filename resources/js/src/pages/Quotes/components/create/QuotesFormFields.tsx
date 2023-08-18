910b5fadb65b801b4a892ea413c66c4947f98dde
davood.thinker <davood.thinker@gmail.com>
2023-08-18 10:27:37 +0330

    feat: added HeaderSection component

--- resources/js/src/pages/Quotes/components/create/QuotesFormFields.tsx
+++ resources/js/src/pages/Quotes/components/create/QuotesFormFields.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import HeaderSection from "./HeaderSection";
 
 
 const QuotesFormFields = () => {
     return (
         <div className="mt-8 px-4">
-
+            <HeaderSection/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
         </div>
