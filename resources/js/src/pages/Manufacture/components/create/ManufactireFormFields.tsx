6dabe0413d08162ddb35375ee68c45363d6592f4
davood.thinker <davood.thinker@gmail.com>
2023-08-19 09:09:05 +0330

    feat: added ManufactureForFields

---
+++ resources/js/src/pages/Manufacture/components/create/ManufactireFormFields.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ManufactureFormFields = () => {
+    return (
+        <div className="mt-8 px-4">
+            <ManufactureInformationSection/>
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+
+        </div>
+    )
+
+
+}
+
+export default ManufactureFormFields;
