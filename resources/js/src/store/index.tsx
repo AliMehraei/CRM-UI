326dd7673675fa48d82dccc23877061a51dc31f2
davood.thinker <davood.thinker@gmail.com>
2023-08-19 10:27:58 +0330

    feat: added     manufactureForm : manufactureFormSlice,

--- resources/js/src/store/index.tsx
+++ resources/js/src/store/index.tsx
@@ -1,10 +1,12 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import themeConfigSlice from './themeConfigSlice';
 import rfqFormSlice from "./rfqFormSlice";
+import manufactureFormSlice from "./manufactureFormSlice";
 
 const rootReducer = combineReducers({
     themeConfig: themeConfigSlice,
     rfqFormSlice: rfqFormSlice,
+    manufactureForm : manufactureFormSlice,
 });
 
 export default configureStore({
