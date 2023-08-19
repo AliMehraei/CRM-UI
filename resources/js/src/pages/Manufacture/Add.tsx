82ac9cfbef05435bd630adaf7b4f46bd1c7e3534
davood.thinker <davood.thinker@gmail.com>
2023-08-19 09:34:17 +0330

    fix: fixed link for back button

--- resources/js/src/pages/Manufacture/Add.tsx
+++ resources/js/src/pages/Manufacture/Add.tsx
@@ -1,8 +1,9 @@
-import { Link } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { setPageTitle } from '../../store/themeConfigSlice';
+import {Link} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {setPageTitle} from '../../store/themeConfigSlice';
 import ManufactireFormFields from "./components/create/ManufactireFormFields";
+import QuotesFormFields from "../Quotes/components/create/QuotesFormFields";
 
 const Add = () => {
     const dispatch = useDispatch();
@@ -11,11 +12,27 @@ const Add = () => {
     });
 
     return (
-        <div className="flex xl:flex-row flex-col gap-2.5">
-            <div className="panel px-0 flex-1 py-6 ltr:xl:mr-6 rtl:xl:ml-6">
-                <ManufactireFormFields/>
+        <div className='px-4'>
+            <div className="flex items-center lg:justify-end justify-center flex-wrap gap-4 mb-6">
+                <div className="flex items-center gap-2">
+                    <Link to="/manufacture/list" className="btn btn-danger gap-2">
+                        Back
+                    </Link>
+                    <button className="btn btn-primary gap-2">
+                        Save and new
+                    </button>
+                    <button className="btn btn-success gap-2">
+                        Save
+                    </button>
+                </div>
+            </div>
+            <div className="flex xl:flex-row flex-col gap-2.5">
+                <div className="panel px-0 flex-1 py-6 ltr:xl:mr-6 rtl:xl:ml-6">
+                    <ManufactireFormFields/>
+                </div>
             </div>
         </div>
+
     );
 };
 
