0edf9fd2039d145212ed158531ed457844c613b3
davood.thinker <davood.thinker@gmail.com>
2023-08-19 10:27:42 +0330

    feat: added Key for compoenents

--- resources/js/src/pages/Manufacture/components/create/ManufactireFormFields.tsx
+++ resources/js/src/pages/Manufacture/components/create/ManufactireFormFields.tsx
@@ -5,9 +5,9 @@ import LineCardSection from "./LineCardSection";
 const ManufactureFormFields = () => {
     return (
         <div className="mt-8 px-4">
-            <ManufactureInformationSection/>
+            <ManufactureInformationSection key="manufactureInfo" />
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <LineCardSection/>
+            <LineCardSection key="lineCard"  />
         </div>
     )
 
