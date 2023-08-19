bd0f9b6c098399cd6861c901ebc74f96bb2fd1ff
davood.thinker <davood.thinker@gmail.com>
2023-08-19 10:36:04 +0330

    feat: added Key for compoenents

--- resources/js/src/pages/Manufacture/components/create/LineCardSection.tsx
+++ resources/js/src/pages/Manufacture/components/create/LineCardSection.tsx
@@ -32,7 +32,7 @@ const LineCardSection = () => {
     return (<>
             <div className="flex justify-between lg:flex-row flex-col">
                 {Object.entries(fields).map(([sectionTitle, sectionContent], index) => (
-                    <div className="lg:w-1/2 w-full ltr:lg:mr-6 rtl:lg:ml-6 mb-6">
+                    <div key={`${sectionTitle}_${index}`} className="lg:w-1/2 w-full ltr:lg:mr-6 rtl:lg:ml-6 mb-6">
                         <div key={sectionTitle} className="">
                             <div className="text-lg">{sectionTitle ? `${sectionTitle}:` :
                                 <div className="mt-10"></div>}</div>
