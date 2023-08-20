197b18a4768c171122d8261cfe9e10efa8d92be7
davood.thinker <davood.thinker@gmail.com>
2023-08-20 11:47:49 +0330

    feat: added handling image fields on reset

--- resources/js/src/store/manufactureFormSlice.tsx
+++ resources/js/src/store/manufactureFormSlice.tsx
@@ -4,6 +4,15 @@ import {createSlice} from '@reduxjs/toolkit';
 
 const initState = {
     api: "createSingleManufacturer",
+    name: '',
+    octo_api_id: '',
+    is_active: false,
+    alias_names: '',
+    owner_id: '',
+    line_card_id: '',
+    strong_card_id: '',
+    image: '',
+
 };
 
 const manufactureFormSlice = createSlice({
@@ -17,9 +26,17 @@ const manufactureFormSlice = createSlice({
                 [field]: value,
             };
         },
-        resetForm: () => initState
+        resetForm: () => {
+            const fileInput = document.querySelector('[type="file"]') as HTMLInputElement | null;
+            for (let element in fileInput) {
+                fileInput.value = ''; // Clear the value
+                fileInput.dispatchEvent(new Event('change', {bubbles: true})); // Trigger a change event
+            }
+
+            return initState
+        }
     },
 });
 
-export const {updateFormData,resetForm} = manufactureFormSlice.actions;
+export const {updateFormData, resetForm} = manufactureFormSlice.actions;
 export default manufactureFormSlice.reducer;
