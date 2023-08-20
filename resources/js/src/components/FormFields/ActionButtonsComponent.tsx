4baeab4784f8fa2aa905ccd2bd2d1a4335ca5e72
davood.thinker <davood.thinker@gmail.com>
2023-08-20 09:44:28 +0330

    feat: handled submit form and errors

--- resources/js/src/components/FormFields/ActionButtonsComponent.tsx
+++ resources/js/src/components/FormFields/ActionButtonsComponent.tsx
@@ -1,22 +1,66 @@
-import {Link, useNavigate} from "react-router-dom";
-import React from "react";
+import {useNavigate} from "react-router-dom";
+import React, {useEffect} from "react";
 import api from "../../config/api";
-import any = jasmine.any;
-import {an} from "@fullcalendar/core/internal-common";
+import {useDispatch} from "react-redux";
+import {resetErrors, updateErrors} from "../../store/formErrorsSlice";
+import Swal from "sweetalert2";
+import {resetForm} from "../../store/manufactureFormSlice";
 
 
 const ActionButtonsComponent = ({formState}: any) => {
+    const dispatch = useDispatch();
+
     const navigate = useNavigate();
     const api_instance: any = new api();
-    const submitForm = async () => {
-        await callMethod(formState);
-    };
-    const callMethod = (parameters: any) => {
+
+    const submitForm = async (action: string = 'save') => {
+
+        dispatch(resetErrors);
+        const toast = Swal.mixin({
+            toast: true,
+            position: 'top',
+            showConfirmButton: false,
+            timer: 3000,
+        });
+
         const methodToCall = api_instance[formState.api];
-        if (typeof methodToCall === 'function') {
-            return methodToCall.call(api_instance, parameters);
+        if (typeof methodToCall !== 'function') {
+            return;
         }
-    }
+
+        const response = await methodToCall.call(api_instance, formState);
+        if (response.isOk) {
+            toast.fire({
+                icon: 'success',
+                timer: 2000,
+                title: 'Form submitted successfully',
+                padding: '10px 20px',
+
+            }).then(() => {
+                dispatch(resetForm());
+                if (action === 'save') {
+                    const pathToNavigate = location.pathname.replace('\/add', `\/edit/${response.data.data.id}`);
+                    navigate(pathToNavigate, {replace: true});
+                }
+            });
+
+        } else if (response.status === 422) {
+            const errorData = response.data.data;
+            const errorsToUpdate = {hasError: true, ...Object.fromEntries(Object.entries(errorData).map(([field, value]: any) => [field, value[0]]))};
+            dispatch(updateErrors(errorsToUpdate));
+        } else {
+            toast.fire({
+                icon: 'error',
+                title: 'Internal Server Error ,submitting form failed',
+                padding: '10px 20px',
+            });
+        }
+    };
+
+    useEffect(() => {
+        dispatch(resetErrors);
+    }, [])
+
     const handlePreviousPage = () => {
         const pathToNavigate = location.pathname.replace('\/add', '\/list');
         navigate(pathToNavigate, {replace: true});
@@ -29,10 +73,10 @@ const ActionButtonsComponent = ({formState}: any) => {
                     <button onClick={handlePreviousPage} className="btn btn-danger gap-2">
                         Back
                     </button>
-                    <button className="btn btn-primary gap-2">
+                    <button onClick={() => submitForm('new')} className="btn btn-primary gap-2">
                         Save and new
                     </button>
-                    <button onClick={submitForm} className="btn btn-success gap-2">
+                    <button onClick={() => submitForm('save')} className="btn btn-success gap-2">
                         Save
                     </button>
                 </div>
