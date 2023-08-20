c5ef89b9bbe8144c8c5a90b3266d93c6e0665a7d
davood.thinker <davood.thinker@gmail.com>
2023-08-20 09:15:08 +0330

    feat: handled errors for fields

--- resources/js/src/components/FormFields/GenerateFields.tsx
+++ resources/js/src/components/FormFields/GenerateFields.tsx
@@ -1,8 +1,10 @@
 import {RequiredComponent} from "./RequiredComponent";
-import {an} from "@fullcalendar/core/internal-common";
+import {useSelector} from "react-redux";
 
 
 const GenerateFields = ({fields}: any) => {
+    const formErrors = useSelector((state: any) => state.formErrors);
+
     return (
         <>
             {Object.entries(fields).map(([sectionTitle, sectionContent]: any, index) => (
@@ -10,13 +12,24 @@ const GenerateFields = ({fields}: any) => {
                     <div key={sectionTitle} className="">
                         <div className="text-lg">{sectionTitle ? `${sectionTitle}:` :
                             <div className="mt-10"></div>}</div>
-                        {Object.entries(sectionContent).map(([label, inputComponent] : any, index) => (
-                            <div key={label} className="mt-4 flex items-center">
-                                <label key={label} className="ltr:mr-2 rtl:ml-2 w-1/3 mb-0">
-                                    {label}
-                                    {inputComponent.props.required && <RequiredComponent/>}
-                                </label>
-                                {inputComponent}
+                        {Object.entries(sectionContent).map(([label, inputComponent]: any) => (
+                            <div key={label}
+                                 className={Object.keys(formErrors).length > 1 && formErrors[inputComponent.props.name] ? "mt-4 items-center has-error" : "mt-4 items-center"}>
+                                <div className="flex">
+                                    <label key={label} className="ltr:mr-2 rtl:ml-2 w-1/3 mb-0 mt-2">
+                                        {label}
+                                        {inputComponent.props.required && <RequiredComponent/>}
+                                    </label>
+                                    <div className="w-full">
+                                        {inputComponent}
+                                        {Object.keys(formErrors).length > 1 &&
+                                            formErrors[inputComponent.props.name] &&
+                                            <div
+                                                className="text-danger mt-1"> {formErrors[inputComponent.props.name]}</div>}
+                                    </div>
+                                </div>
+
+
                             </div>
                         ))}
                     </div>
