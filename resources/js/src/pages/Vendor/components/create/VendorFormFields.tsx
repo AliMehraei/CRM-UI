f14ad4bbac999de029dade412fad3ff477a8b200
davood.thinker <davood.thinker@gmail.com>
2023-08-21 22:59:03 +0330

    feat: add other section vendor

--- resources/js/src/pages/Vendor/components/create/VendorFormFields.tsx
+++ resources/js/src/pages/Vendor/components/create/VendorFormFields.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ContactInformationSection from "./ContactInformationSection";
 import VendorSection from "./VendorSection";
-
+import TermsSection from "./TermsSection";
+import AddressInformationSection from "./AddressInformationSection";
+import DescriptionInformationSection from "./DescriptionInformationSection";
 const VendorFormFields = () => {
     return (
         <div className="mt-8 px-4">
             <VendorSection key="vendorInfo" />
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <ContactInformationSection key="vendor"  />
+            <ContactInformationSection key="contact"  />
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+            <TermsSection key="Terms"  />
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+            <AddressInformationSection key="Address"  />
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+            <DescriptionInformationSection key="Description"  />
+
         </div>
     )
 
