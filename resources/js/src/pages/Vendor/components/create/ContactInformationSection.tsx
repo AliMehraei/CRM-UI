3345eaf7f398e445a8c08884067c9de0da6fb868
davood.thinker <davood.thinker@gmail.com>
2023-08-21 13:35:19 +0330

    chore: working vendor ui

---
+++ resources/js/src/pages/Vendor/components/create/ContactInformationSection.tsx
@@ -0,0 +1,120 @@
+import AsyncSelect from "react-select/async";
+import {useDispatch, useSelector} from "react-redux";
+import {updateFormData} from "../../../../store/vendorFormSlice";
+import api from "../../../../config/api";
+import GenerateFields from "../../../../components/FormFields/GenerateFields";
+import {handleUploadImage} from "../../../../components/Functions/CommonFunctions";
+
+const ContactInformationSection = () => {
+    const dispatch = useDispatch();
+    const api_instance = new api();
+    const formState = useSelector((state: any) => state.vendorForm);
+    const handleChangeField = (field: any, value: any) => {
+        dispatch(updateFormData({[field]: value}));
+    };
+
+
+    const loadOwners = async (e: any) => {
+        const result = await api_instance.loadAdminUsers(e);
+        const valField = 'id';
+        const nameField = 'name';
+        const avatarField = 'avatar';
+        const emailField = 'email';
+        if (result.status) {
+            return result.data.map((user: any) => ({
+                value: user[valField],
+                label: (
+                    <div key={user[valField]} className="flex items-center">
+                        <img src={user[avatarField]} alt="avatar" className="w-8 h-8 mr-2 rounded-full"/>
+                        <div>
+                            <div className="text-sm font-bold">{user[nameField]}</div>
+                            <div className="text-xs text-gray-500">{user[emailField]}</div>
+                        </div>
+                    </div>
+                ),
+            }));
+        }
+    };
+
+
+    const fields = {
+        'Vendor Information': {
+            'Vendor Image': (<input
+                    id="vendor_image"
+                    key="vendor_image"
+                    type="file"
+                    className="form-input file:py-2 file:px-4 file:border-0 file:font-semibold p-0 file:bg-primary/90 ltr:file:mr-5 rtl:file:ml-5 file:text-white file:hover:bg-primary flex-1"
+                    accept="image/*"
+                    onChange={(e) => handleUploadImage(e, (response: any) => {
+                        dispatch(updateFormData({field: 'image', value: `${response?.data.data.file_url}`}));
+                    })}
+                    name="vendorImage"
+                />
+            ),
+            'Vendor Name': (
+                <input
+                    id="name"
+                    required
+                    name="name"
+                    className="form-input flex-1 "
+                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                    value={formState.name}
+                />
+            ),
+            'Octo Api Id': (
+                <input
+                    id="octo_api_id"
+                    name="octo_api_id"
+                    className="form-input flex-1 "
+                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                    value={formState.octo_api_id}
+                />
+            ),
+            'Alias Names': (
+                <textarea
+                    id="alias_names"
+                    name="alias_names"
+                    rows={3}
+                    className="form-textarea flex-1"
+                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                    value={formState.alias_names}
+
+                ></textarea>
+            ),
+        },
+        '': {
+            'Active': (
+                <input
+                    id="is_active"
+                    type="checkbox"
+                    name="is_active"
+                    className="form-checkbox"
+                    onChange={(e) => handleChangeField(e.target.name, e.target.checked)}
+                    checked={formState.is_active}
+                />
+            ),
+            'Vendor Owner': (
+                <AsyncSelect
+                    isMulti={false}
+                    id="owner_id"
+                    placeholder="Type at least 2 characters to search..."
+                    name="owner_id"
+                    loadOptions={loadOwners}
+                    onChange={({value}: any) => {
+                        handleChangeField('owner_id', value)
+                    }} // Use 'owner_id' if it's the field name
+                    className="flex-1"
+                />
+            ),
+        }
+    }
+    return (
+        <>
+            <div className="flex justify-between lg:flex-row flex-col">
+                <GenerateFields fields={fields}/>
+            </div>
+        </>
+    )
+}
+
+export default ContactInformationSection;
