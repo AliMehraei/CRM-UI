e45927750c8b7c48de60cdd36bb9b6d8b1935af5
davood.thinker <davood.thinker@gmail.com>
2023-08-22 12:15:39 +0330

    fix: working on set the default value

--- resources/js/src/pages/Account/components/edit/DescriptionInformationSection.tsx
+++ resources/js/src/pages/Account/components/edit/DescriptionInformationSection.tsx
@@ -1,10 +1,12 @@
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {updateFormData} from "../../../../store/accountFormSlice";
 import Flatpickr from "react-flatpickr";
 
 const DescriptionInformationSection = () => {
     const dispatch = useDispatch();
+    const formState = useSelector((state: any) => state.accountForm);
+
     const handleChangeField = (field: any, value: any) => {
         dispatch(updateFormData({[field]: value}));
     };
@@ -13,15 +15,20 @@ const DescriptionInformationSection = () => {
             'Description Information': <textarea id="description" rows={3} name="description"
                                                  className="form-textarea flex-1"
                                                  placeholder=""
-                                                 onChange={(e) => handleChangeField(e.target.name, e.target.value)}></textarea>,
+                                                 onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                                 defaultValue={formState.description}
+            ></textarea>,
             'Last Activity Date': <Flatpickr
                 options={{
                     dateFormat: 'Y-m-d ',
                     position: 'auto left',
+                    defaultDate: `${formState.last_activity_date ? new Date(formState.last_activity_date) : ''}`,
                 }}
                 name="last_activity_date"
-                value=""
+                value={formState.last_activity_date ? new Date(formState.last_activity_date) : ''}
                 className="form-input flex-1"
+                onChange={(dates) => handleChangeField('last_activity_date', dates[0])} // Update the field value on change
+
             />
         },
     }
