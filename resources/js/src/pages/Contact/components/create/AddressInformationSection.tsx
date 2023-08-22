23a98c16d038c55152685218620bc2ba4eb64fd0
davood.thinker <davood.thinker@gmail.com>
2023-08-22 18:41:47 +0330

    feat: Added Address information section

---
+++ resources/js/src/pages/Contact/components/create/AddressInformationSection.tsx
@@ -0,0 +1,5 @@
+const AddressInformationSection = () => {
+    return (<>
+    </>)
+}
+export default AddressInformationSection;
--- resources/js/src/pages/Contact/components/create/ContactFormFields.tsx
+++ resources/js/src/pages/Contact/components/create/ContactFormFields.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ContactDetailsSection from "./ContactDetailsSection";
 import ContactInformationSection from "./ContactInformationSection";
+import AddressInformationSection from "./AddressInformationSection";
 
 
 const ContactFormFields = () => {
@@ -10,7 +11,7 @@ const ContactFormFields = () => {
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
             <ContactInformationSection/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-
+            <AddressInformationSection/>
         </div>
     )
 
