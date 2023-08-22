a21f2486b5a073961b63adc4a5bdf563d60e7562
davood.thinker <davood.thinker@gmail.com>
2023-08-22 19:33:47 +0330

    feat: added Status section

--- resources/js/src/pages/Excess/components/create/ExcessFormFields.tsx
+++ resources/js/src/pages/Excess/components/create/ExcessFormFields.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import HeaderSection from "./HeaderSection";
+import StatusSection from "./StatusSection";
 
 const ExcessFormFields = () => {
     return (
         <div className="mt-8 px-4">
             <HeaderSection/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+            <StatusSection/>
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+
         </div>
     )
 }
