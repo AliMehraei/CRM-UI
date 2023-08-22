fe97658fd91f46bb4307c6f3243be6807e32d380
davood.thinker <davood.thinker@gmail.com>
2023-08-22 12:32:26 +0330

    feat: added ContactDetailsSection

---
+++ resources/js/src/pages/Contact/components/create/ContactDetailsSection.tsx
@@ -0,0 +1,4 @@
+const ContactDetailsSection = () => {
+
+}
+export default ContactDetailsSection;
--- resources/js/src/pages/Contact/components/create/ContactFormFields.tsx
+++ resources/js/src/pages/Contact/components/create/ContactFormFields.tsx
@@ -1,4 +1,17 @@
+import React from "react";
+import ContactDetailsSection from "./ContactDetailsSection";
+
+
 const ContactFormFields = () => {
-    return (<></>)
+    return (
+        <div className="mt-8 px-4">
+            <ContactDetailsSection key="ContactDetailsSection"/>
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+
+        </div>
+    )
+
+
 }
+
 export default ContactFormFields;
