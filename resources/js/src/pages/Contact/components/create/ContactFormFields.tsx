8069577728de49c91344eec9b47751e58108226f
davood.thinker <davood.thinker@gmail.com>
2023-08-22 18:38:23 +0330

    feat: added ContactInformation section

--- resources/js/src/pages/Contact/components/create/ContactFormFields.tsx
+++ resources/js/src/pages/Contact/components/create/ContactFormFields.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ContactDetailsSection from "./ContactDetailsSection";
+import ContactInformationSection from "./ContactInformationSection";
 
 
 const ContactFormFields = () => {
     return (
         <div className="mt-8 px-4">
             <ContactDetailsSection key="ContactDetailsSection"/>
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+            <ContactInformationSection/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
 
         </div>
