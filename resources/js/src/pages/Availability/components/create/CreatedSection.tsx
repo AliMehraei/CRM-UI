477af06d759373d00894a2b1bcdfc553cb28383c
SMDM <yektadg.dev@gmail.com>
2023-08-23 19:26:00 +0330

    feat: added handleChange using redux

--- resources/js/src/pages/Availability/components/create/CreatedSection.tsx
+++ resources/js/src/pages/Availability/components/create/CreatedSection.tsx
@@ -2,17 +2,24 @@ import {RequiredComponent} from "../../../../components/FormFields/RequiredCompo
 import Tippy from "@tippyjs/react";
 import Flatpickr from "react-flatpickr";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
+import {useDispatch} from "react-redux";
+import {updateFormData} from "../../../../store/accountFormSlice";
 
 const CreatedSection = () => {
+    const dispatch = useDispatch();
+    const handleChangeField = (field: any, value: any) => {
+        dispatch(updateFormData({[field]: value}));
+    };
 
     const fields = {
         'Created': {
             'test': <input id="test" type="checkbox"
                            name="test"
-                           className="form-checkbox"/>,
+                           className="form-checkbox" onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
             'Send letter owner': <input id="send_letter_owner" type="checkbox"
                                         name="send_letter_owner"
-                                        className="form-checkbox"/>,
+                                        className="form-checkbox"
+                                        onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
         }
     }
     return (<>
