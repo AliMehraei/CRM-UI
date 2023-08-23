1db9451bb62aa04911f7cfc0858c1eac4a8df1a7
SMDM <yektadg.dev@gmail.com>
2023-08-23 18:54:49 +0330

    feat: added handleChange using redux

--- resources/js/src/pages/Quotes/components/create/SourceSection.tsx
+++ resources/js/src/pages/Quotes/components/create/SourceSection.tsx
@@ -3,41 +3,56 @@ import Select from "react-select";
 import AsyncSelect from "react-select/async";
 import Flatpickr from "react-flatpickr";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
+import {useDispatch} from "react-redux";
+import {updateFormData} from "../../../../store/quoteFormSlice";
+import {loadAvailability, loadVendor} from "../../../../components/Functions/CommonFunctions";
 
 const SourceSection = () => {
-    const loadVendors = () => {
-
-    };
-    const handleVendorChange = () => {
-
-    };
-    const handleAvailabilityChange = () => {
-
-    };
-    const loadAvailability = () => {
-
+    const dispatch = useDispatch();
+    const handleChangeField = (field: any, value: any) => {
+        dispatch(updateFormData({[field]: value}));
     };
     const fields = {
         'Source': {
             'Vendor': <AsyncSelect isMulti={false} id="vendor_id" name="vendor_id"
                                    placeholder="Type at least 2 characters to search..."
-                                   loadOptions={loadVendors} onChange={handleVendorChange} className="flex-1"/>,
-            'Availability No': <input id="availability_no" name="availability_no" className="form-input flex-1 "/>,
-            'Availability Date': <Flatpickr options={{dateFormat: 'd-m-Y'}} className="form-input flex-1"
-                                            placeholder="MM DD YYYY"/>,
-            'Availability': <AsyncSelect isMulti={false} id="availability" name="availability"
+                                   loadOptions={loadVendor}
+                                   onChange={({value}: any) => {
+                                       handleChangeField('product_id', value)
+                                   }}
+                                   className="flex-1"/>,
+
+            'Availability No': <input id="availability_no" name="availability_no" className="form-input flex-1 "
+                                      onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
+            'Availability Date': <Flatpickr name='availability_date' options={{dateFormat: 'd-m-Y'}}
+                                            className="form-input flex-1"
+                                            placeholder="MM DD YYYY"
+                                            onChange={(dates) => handleChangeField('availability_date', dates[0])}/>,
+
+            'Availability': <AsyncSelect isMulti={false} id="availability_id" name="availability_id"
                                          placeholder="Type at least 2 characters to search..."
-                                         loadOptions={loadAvailability} onChange={handleAvailabilityChange}
+                                         loadOptions={loadAvailability}
+                                         onChange={({value}: any) => {
+                                             handleChangeField('availability_id', value)
+                                         }}
                                          className="flex-1"/>,
 
         },
         '': {
 
-            'Cost': <input id="cost" name="cost" className="form-input flex-1 "/>,
-            'LT vendor': <input id="lt_vendor" name="lt_vendor" className="form-input flex-1 "/>,
-            'SPQ': <input id="spq" name="spq" className="form-input flex-1 "/>,
+            'Cost': <input id="cost" name="cost" className="form-input flex-1 "
+                           onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
+
+            'LT vendor': <input id="lt_vendor" name="lt_vendor" className="form-input flex-1 "
+                                onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
+
+            'SPQ': <input id="spq" name="spq" className="form-input flex-1 "
+                          onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
+
             'Quantity in Stock': <input id="quantity_in_stock" name="quantity_in_stock"
-                                        className="form-input flex-1 "/>,
+                                        className="form-input flex-1 "
+                                        onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
+
         }
     }
     return (<>
