c784029ffa301c97bbeb5ef9752dd3e8d34ae6e1
SMDM <yektadg.dev@gmail.com>
2023-08-23 18:38:31 +0330

    feat: added handleChange using redux

--- resources/js/src/pages/Quotes/components/create/QuoteLineSection.tsx
+++ resources/js/src/pages/Quotes/components/create/QuoteLineSection.tsx
@@ -2,26 +2,37 @@ import Select from "react-select";
 import {RequiredComponent} from "../../../../components/FormFields/RequiredComponent";
 import AsyncSelect from "react-select/async";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
+import {updateFormData} from "../../../../store/quoteFormSlice";
+import {loadProducts} from "../../../../components/Functions/CommonFunctions";
+import {useDispatch} from "react-redux";
 
 const QuoteLineSection = () => {
-    const loadProducts = () => {
+    const dispatch = useDispatch();
 
-    };
-    const handleChangeProduct = () => {
-
+    const handleChangeField = (field: any, value: any) => {
+        dispatch(updateFormData({[field]: value}));
     };
     const fields = {
         'Quote Line': {
             'Product Name': <AsyncSelect isMulti={false} id="product_id" name="product_id"
                                          placeholder="Type at least 2 characters to search..."
-                                         loadOptions={loadProducts} onChange={handleChangeProduct} className="flex-1"/>,
+                                         loadOptions={loadProducts}
+                                         onChange={({value}: any) => {
+                                             handleChangeField('product_id', value)
+                                         }}
+                                         className="flex-1"/>,
 
-            'Customer part ID': <input id="rating" name="rating" className="form-input flex-1 "/>
+            'Customer part ID': <input id="customer_part_iD" name="customer_part_iD" className="form-input flex-1 "
+                                       onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+            />
         },
         '': {
-            'Quantity': <input id="quantity" name="quantity" className="form-input flex-1 "/>,
-            'List Price': <input id="list_price" name="list_price" className="form-input flex-1 "/>,
-            'Lead Time': <input id="lead_time" name="lead_time" className="form-input flex-1 "/>,
+            'Quantity': <input id="quantity" name="quantity" className="form-input flex-1 "
+                               onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
+            'List Price': <input id="list_price" name="list_price" className="form-input flex-1 "
+                                 onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
+            'Lead Time': <input id="lead_time" name="lead_time" className="form-input flex-1 "
+                                onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
 
         }
 
