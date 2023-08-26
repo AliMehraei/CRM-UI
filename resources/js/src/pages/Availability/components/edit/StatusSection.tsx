4f71b54dd177c9ff754c63a6a228eda417d21498
davood.thinker <davood.thinker@gmail.com>
2023-08-26 17:35:12 +0330

    feat: handled default value for fields

--- resources/js/src/pages/Availability/components/edit/StatusSection.tsx
+++ resources/js/src/pages/Availability/components/edit/StatusSection.tsx
@@ -2,11 +2,13 @@ import Select from "react-select";
 import {RequiredComponent} from "../../../../components/FormFields/RequiredComponent";
 import Flatpickr from "react-flatpickr";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {updateFormData} from "../../../../store/accountFormSlice";
 
 const StatusSection = () => {
     const dispatch = useDispatch();
+    const formState = useSelector((state: any) => state.availabilityForm);
+
     const handleChangeField = (field: any, value: any) => {
         dispatch(updateFormData({[field]: value}));
     };
@@ -27,20 +29,30 @@ const StatusSection = () => {
                                          onChange={({value}: any) => {
                                              handleChangeField('availability_type', value)
                                          }}
+                                         defaultValue={AvailabilityTypes.find((data) => data.value == formState.availability_source)}
+
             />,
 
-            'Valid': <Flatpickr name="valid" id="valid" options={{dateFormat: 'd-m-Y'}} className="form-input flex-1"
+            'Valid': <Flatpickr name="valid" id="valid"
+                                options={{
+                                    dateFormat: 'd-m-Y',
+                                    defaultDate: `${formState.valid ? new Date(formState.valid) : ''}`,
+                                }}
+                                defaultValue={formState.valid}
+                                className="form-input flex-1"
                                 placeholder="MM DD YYYY"
-                                onChange={(_,dateString) => handleChangeField('valid', dateString)}
+                                onChange={(_, dateString) => handleChangeField('valid', dateString)}
             />,
 
         },
         '': {
             'Availability Name': <input required id="availability_name" name="availability_name"
                                         className="form-input flex-1 "
+                                        defaultValue={formState.availability_name}
                                         onChange={(e) => handleChangeField(e.target.name, e.target.value)}
             />,
             'Rating': <input id="rating" name="rating" className="form-input flex-1 "
+                             defaultValue={formState.rating}
                              onChange={(e) => handleChangeField(e.target.name, e.target.value)}
             />,
 
