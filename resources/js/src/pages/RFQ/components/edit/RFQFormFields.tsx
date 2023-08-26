17c9d02b1e3807280d7ca1a3253fd5367611da83
davood.thinker <davood.thinker@gmail.com>
2023-08-26 16:35:06 +0330

    fix: changed type of processed_for_vrfq_round_1 and processed_for_vrfq_round_2

--- resources/js/src/pages/RFQ/components/edit/RFQFormFields.tsx
+++ resources/js/src/pages/RFQ/components/edit/RFQFormFields.tsx
@@ -1,15 +1,18 @@
+import AsyncSelect from "react-select/async";
 
+import {RequiredComponent} from "../../../../components/FormFields/RequiredComponent";
 import 'flatpickr/dist/flatpickr.css';
-import {RFQHeaderSection} from "./HeaderSection";
+import Header from "../../../../components/Layouts/Header";
 import {LineSection} from "./LineSection";
 import React from "react";
 import {DevelopmentSection} from "./DevelopmentSection";
+import HeaderSection from "./HeaderSection";
 
 
 const RFQFormField = () => {
     return (
         <div className="mt-8 px-4">
-            <RFQHeaderSection/>
+            <HeaderSection/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
             <LineSection/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
