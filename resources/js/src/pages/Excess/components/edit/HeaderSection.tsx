364aeef7f1617948a32cbb51a708d04ca6213ce1
davood.thinker <davood.thinker@gmail.com>
2023-08-26 18:20:24 +0330

    feat: handled default value for fields

--- resources/js/src/pages/Excess/components/edit/HeaderSection.tsx
+++ resources/js/src/pages/Excess/components/edit/HeaderSection.tsx
@@ -63,54 +63,107 @@ const HeaderSection = () => {
                                          placeholder="Type at least 2 characters to search..."
                                          loadOptions={searchAccounts}
                                          onChange={({value}: any) => {
-                                             handleChangeField('parent_account_id', value)
+                                             handleChangeField('account_id', value)
+                                         }}
+                                         defaultValue={{
+                                             value: formState.account?.id,
+                                             label: (
+                                                 <div key={formState.account?.id} className="flex items-center">
+                                                     <img src={formState.account?.image} alt="avatar"
+                                                          className="w-8 h-8 mr-2 rounded-full"/>
+                                                     <div>
+                                                         <div
+                                                             className="text-sm font-bold">{formState.account?.name}</div>
+                                                         <div
+                                                             className="text-xs text-gray-500">{formState.account?.email}</div>
+                                                     </div>
+                                                 </div>
+                                             ),
                                          }}
                                          className="flex-1"/>,
             'Contact': <AsyncSelect isMulti={false} id="contact_id" name="contact_id"
                                     placeholder="Type at least 2 characters to search..."
                                     loadOptions={searchOwners}
                                     onChange={({value}: any) => {
-                                        handleChangeField('parent_account_id', value)
+                                        handleChangeField('contact_id', value)
+                                    }}
+                                    defaultValue={{
+                                        value: formState.contact?.id,
+                                        label: (
+                                            <div key={formState.contact?.id} className="flex items-center">
+                                                <img src={formState.contact?.image} alt="avatar"
+                                                     className="w-8 h-8 mr-2 rounded-full"/>
+                                                <div>
+                                                    <div
+                                                        className="text-sm font-bold">{formState.contact?.name}</div>
+                                                    <div
+                                                        className="text-xs text-gray-500">{formState.contact?.email}</div>
+                                                </div>
+                                            </div>
+                                        ),
                                     }}
                                     className="flex-1"/>,
             'Email': <input id="email" type="text" name="email"
                             className="form-input flex-1 "
+                            defaultValue={formState.email}
                             onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
             'Currency': <Select options={Currencies}
                                 onChange={({value}: any) => {
                                     handleChangeField('currency', value)
                                 }}
+                                defaultValue={Currencies.find((data) => data.value == formState.currency)}
                                 className="flex-1"/>,
             'Portal Excess Id': <input id="portal_excess_id" type="text" name="portal_excess_id"
                                        className="form-input flex-1 "
+                                       defaultValue={formState.portal_excess_id}
                                        onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
 
         },
         '': {
-            'Excess Owner': <AsyncSelect isMulti={false} id="excess_owner" name="excess_owner"
+            'Excess Owner': <AsyncSelect isMulti={false} id="owner_id" name="owner_id"
                                          placeholder="Type at least 2 characters to search..."
                                          loadOptions={searchOwners}
                                          onChange={({value}: any) => {
-                                             handleChangeField('excess_owner', value)
+                                             handleChangeField('owner_id', value)
+                                         }}
+                                         defaultValue={{
+                                             value: formState.owner?.id,
+                                             label: (
+                                                 <div key={formState.owner?.id} className="flex items-center">
+                                                     <img src={formState.owner?.image} alt="avatar"
+                                                          className="w-8 h-8 mr-2 rounded-full"/>
+                                                     <div>
+                                                         <div
+                                                             className="text-sm font-bold">{formState.owner?.name}</div>
+                                                         <div
+                                                             className="text-xs text-gray-500">{formState.owner?.email}</div>
+                                                     </div>
+                                                 </div>
+                                             ),
                                          }}
                                          className="flex-1"/>,
-            'Excess File': <input
-                id="excess_file"
-                key="excess_file"
-                type="file"
-                className="form-input file:py-2 file:px-4 file:border-0 file:font-semibold p-0 file:bg-primary/90 ltr:file:mr-5 rtl:file:ml-5 file:text-white file:hover:bg-primary flex-1"
-                onChange={(e) => handleUploadFile(e, (response: any) => {
-                    dispatch(updateFormData({field: 'excess_file', value: `${response?.data.data.file_url}`}));
-                })}
-                name="excess_file"
-            />,
+            'Excess File': <div>
+                <input
+                    id="excess_file"
+                    key="excess_file"
+                    type="file"
+                    className="form-input file:py-2 file:px-4 file:border-0 file:font-semibold p-0 file:bg-primary/90 ltr:file:mr-5 rtl:file:ml-5 file:text-white file:hover:bg-primary flex-1"
+                    onChange={(e) => handleUploadFile(e, (response: any) => {
+                        dispatch(updateFormData({field: 'excess_file', value: `${response?.data.data.file_url}`}));
+                    })}
+                    name="excess_file"
+                />
+                <a className="btn btn-outline-primary" href={formState.excess_file}>Download</a>
+            </div>,
 
-            'Excess Source': <Select options={ExcessSources}
+            'Excess Source': <Select name="excess_source" id="excess_source" options={ExcessSources}
                                      onChange={({value}: any) => {
-                                         handleChangeField('currency', value)
+                                         handleChangeField('excess_source', value)
                                      }}
+
                                      className="flex-1"/>,
             'Secondary Email': <input id="secondary_email" type="text" name="secondary_email"
+                                      defaultValue={formState.secondary_email}
                                       className="form-input flex-1 "
                                       onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
             'Email Opt Out': <input
@@ -118,6 +171,7 @@ const HeaderSection = () => {
                 type="checkbox"
                 name="email_opt_out"
                 className="form-checkbox"
+                checked={formState.email_opt_out}
                 onChange={(e) => handleChangeField(e.target.name, e.target.checked)}
             />,
             'Exchange Rate': <input id="exchangeRate" type="text" value="1" placeholder="Readonly input here…"
