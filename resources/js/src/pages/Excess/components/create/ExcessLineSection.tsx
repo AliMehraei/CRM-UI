47a2ebfee52f9c1eefa6d5c0abfa31785346fe2c
davood.thinker <davood.thinker@gmail.com>
2023-08-26 17:54:40 +0330

    refactor: removed unused imports

--- resources/js/src/pages/Excess/components/create/ExcessLineSection.tsx
+++ resources/js/src/pages/Excess/components/create/ExcessLineSection.tsx
@@ -1,62 +1,51 @@
 import AsyncSelect from "react-select/async";
-import Select from "react-select";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
 import {
-    Currencies,
-    handleUploadFile,
-    searchOwners,
     searchProducts
 } from "../../../../components/Functions/CommonFunctions";
-import { useDispatch } from "react-redux";
-import { updateFormData } from "../../../../store/excessFormSlice";
+import {useDispatch} from "react-redux";
+import {updateFormData} from "../../../../store/excessFormSlice";
 
 const ExcessLineSection = () => {
     const dispatch = useDispatch();
     const handleChangeField = (field: any, value: any) => {
-        dispatch(updateFormData({ [field]: value }));
+        dispatch(updateFormData({[field]: value}));
     };
 
-
-    const ExcessSources = [
-        { value: 'none', label: '-None-' },
-        { value: 'email', label: 'Email' },
-        { value: 'portal', label: 'Portal' },
-
-    ];
 
     const fields = {
         'Excess Line': {
             'Product name': <AsyncSelect isMulti={false} id="product_name" name="product_name"
-                required
-                placeholder="Type at least 2 characters to search..."
-                loadOptions={searchProducts}
-                onChange={({ value }: any) => {
-                    handleChangeField('parent_account_id', value)
-                }}
-                className="flex-1" />,
+                                         required
+                                         placeholder="Type at least 2 characters to search..."
+                                         loadOptions={searchProducts}
+                                         onChange={({value}: any) => {
+                                             handleChangeField('parent_account_id', value)
+                                         }}
+                                         className="flex-1"/>,
             'Customer Internal No.': <input id="customer_internal_no" type="text" name="customer_internal_no"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)} />,
+                                            className="form-input flex-1 "
+                                            onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
             'Quantity': <input id="quantity" type="text" name="quantity"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)} />,
+                               className="form-input flex-1 "
+                               onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
             'Cost': <input id="cost" type="text" name="cost"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)} />,
+                           className="form-input flex-1 "
+                           onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
         },
         '': {
             'Date Code': <input id="date_code" type="text" name="date_code"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)} />,
+                                className="form-input flex-1 "
+                                onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
             'SPQ': <input id="spq" type="text" name="spq"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)} />,
+                          className="form-input flex-1 "
+                          onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
             'MOQ': <input id="moq" type="text" name="moq"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)} />,
+                          className="form-input flex-1 "
+                          onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
             'Comment': <input id="comment" type="text" name="comment"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)} />,
+                              className="form-input flex-1 "
+                              onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
         }
 
     }
@@ -64,7 +53,7 @@ const ExcessLineSection = () => {
 
     return (<>
         <div className="flex justify-between lg:flex-row flex-col">
-            <GenerateFields fields={fields} />
+            <GenerateFields fields={fields}/>
 
         </div>
     </>)
