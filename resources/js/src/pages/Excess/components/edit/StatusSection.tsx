1235d3da7f54122785c53a04780f153957bc5f69
davood.thinker <davood.thinker@gmail.com>
2023-08-26 18:22:01 +0330

    feat: handled default value for fields

--- resources/js/src/pages/Excess/components/edit/StatusSection.tsx
+++ resources/js/src/pages/Excess/components/edit/StatusSection.tsx
@@ -1,37 +1,47 @@
 import AsyncSelect from "react-select/async";
 import Select from "react-select";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
-import { Currencies, handleUploadFile, searchAccounts, searchOwners } from "../../../../components/Functions/CommonFunctions";
-import { useDispatch } from "react-redux";
-import { updateFormData } from "../../../../store/excessFormSlice";
+import {
+    Currencies,
+    handleUploadFile,
+    searchAccounts,
+    searchOwners
+} from "../../../../components/Functions/CommonFunctions";
+import {useDispatch, useSelector} from "react-redux";
+import {updateFormData} from "../../../../store/excessFormSlice";
 
 const StatusSection = () => {
     const dispatch = useDispatch();
+    const formState = useSelector((state: any) => state.excessForm);
+
     const handleChangeField = (field: any, value: any) => {
-        dispatch(updateFormData({ [field]: value }));
+        dispatch(updateFormData({[field]: value}));
     };
     const ExcessTypes = [
-        { value: "none", label: "-None-" },
-        { value: "line_bid", label: "Line Bid" },
-        { value: "package_bid", label: "Package Bid" },
-        { value: "consignment", label: "Consignment" },
-        { value: "closed", label: "Closed" },
+        {value: "none", label: "-None-"},
+        {value: "line_bid", label: "Line Bid"},
+        {value: "package_bid", label: "Package Bid"},
+        {value: "consignment", label: "Consignment"},
+        {value: "closed", label: "Closed"},
     ]
 
     const fields = {
         'Status': {
             'Excess Type': <Select options={ExcessTypes}
-                name="excess_type"
-                id="excess_type"
-                onChange={({ value }: any) => {
-                    handleChangeField('excess_type', value)
-                }}
-                className="flex-1" />,
+                                   name="excess_type"
+                                   id="excess_type"
+                                   onChange={({value}: any) => {
+                                       handleChangeField('excess_type', value)
+                                   }}
+                                   defaultValue={ExcessTypes.find((data) => data.value == formState.excess_type)}
+
+                                   className="flex-1"/>,
         },
         '': {
             'Excess Name': <input id="excess_name" type="text" name="excess_name"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)} />,
+                                  className="form-input flex-1 "
+                                  defaultValue={formState.excess_name}
+                                  onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
 
         }
 
@@ -40,7 +50,7 @@ const StatusSection = () => {
 
     return (<>
         <div className="flex justify-between lg:flex-row flex-col">
-            <GenerateFields fields={fields} />
+            <GenerateFields fields={fields}/>
 
         </div>
     </>)
