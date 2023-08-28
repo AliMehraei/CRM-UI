1ac9b4c9ee6da5ba647e171e6861240c148961de
davood.thinker <davood.thinker@gmail.com>
2023-08-28 11:21:10 +0330

    feat: handled default value for fields

--- resources/js/src/pages/Invoice/components/edit/InvoiceInformationSection.tsx
+++ resources/js/src/pages/Invoice/components/edit/InvoiceInformationSection.tsx
@@ -1,7 +1,7 @@
 import AsyncSelect from "react-select/async";
-import { useDispatch, useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
-import { updateFormData } from "../../../../store/invoiceFormSlice";
+import {updateFormData} from "../../../../store/invoiceFormSlice";
 import Flatpickr from "react-flatpickr";
 import Select from "react-select";
 import {
@@ -17,15 +17,15 @@ const InvoiceInformationSection = () => {
     const formState = useSelector((state: any) => state.invoiceForm);
 
     const handleChangeField = (field: any, value: any) => {
-        dispatch(updateFormData({ [field]: value }));
+        dispatch(updateFormData({[field]: value}));
     };
 
 
     const Statuses = [
-        { value: 'erzeugt', label: 'Erzeugt' },
-        { value: 'genehmigt', label: 'Genehmigt' },
-        { value: 'geliefert', label: 'Geliefert' },
-        { value: 'abgesagt', label: 'Abgesagt' },
+        {value: 'erzeugt', label: 'Erzeugt'},
+        {value: 'genehmigt', label: 'Genehmigt'},
+        {value: 'geliefert', label: 'Geliefert'},
+        {value: 'abgesagt', label: 'Abgesagt'},
 
 
     ];
@@ -34,28 +34,44 @@ const InvoiceInformationSection = () => {
     const fields = {
         'Invoice Information': {
             'Invoice Owner': <AsyncSelect isMulti={false} id="owner_id" name="owner_id"
-                placeholder="Type at least 2 characters to search..."
-                loadOptions={searchOwners}
-                className="flex-1"
-                onChange={({ value }: any) => {
-                    handleChangeField('vendor_line_card_id', value)
-                }}
-            />,
+                                          placeholder="Type at least 2 characters to search..."
+                                          loadOptions={searchOwners}
+                                          className="flex-1"
+                                          onChange={({value}: any) => {
+                                              handleChangeField('vendor_line_card_id', value)
+                                          }}
+                                          defaultValue={{
+                                              value: formState.owner?.id,
+                                              label: (
+                                                  <div key={formState.owner?.id} className="flex items-center">
+                                                      <img src={formState.owner?.avatar} alt="avatar"
+                                                           className="w-8 h-8 mr-2 rounded-full"/>
+                                                      <div>
+                                                          <div
+                                                              className="text-sm font-bold">{formState.owner?.name}</div>
+                                                          <div
+                                                              className="text-xs text-gray-500">{formState.owner?.email}</div>
+                                                      </div>
+                                                  </div>
+                                              ),
+                                          }}/>,
             'Subject': (
                 <input
-                    id="name"
+                    id="subject"
                     required
-                    name="name"
+                    name="subject"
                     className="form-input flex-1 "
                     onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                    defaultValue={formState.subject}
                 />
             ),
             'Invoice Date': <Flatpickr
                 options={{
                     dateFormat: 'Y-m-d',
                     position: 'auto left',
+                    defaultDate: `${formState.invoice_date ? new Date(formState.invoice_date) : ''}`,
                 }}
-                value=""
+                value={formState.invoice_date}
                 className="form-input flex-1"
                 name="invoice_date"
                 onChange={(e) => handleChangeField('invoice_date', e[0])}
@@ -65,8 +81,9 @@ const InvoiceInformationSection = () => {
                 options={{
                     dateFormat: 'Y-m-d',
                     position: 'auto left',
+                    defaultDate: `${formState.invoice_date ? new Date(formState.invoice_date) : ''}`,
                 }}
-                value=""
+                value={formState.invoice_date}
                 name="due_date"
                 className="form-input flex-1"
                 onChange={(e) => handleChangeField('due_date', e[0])}
@@ -76,6 +93,7 @@ const InvoiceInformationSection = () => {
                 <input
                     id="sales_commission"
                     name="sales_commission"
+                    defaultValue={formState.sales_commission}
                     className="form-input flex-1 "
                     onChange={(e) => handleChangeField(e.target.name, e.target.value)}
                 />
@@ -86,18 +104,34 @@ const InvoiceInformationSection = () => {
                 placeholder="Type at least 2 characters to search..."
                 name="account_id"
                 loadOptions={searchAccounts}
-                onChange={({ value }: any) => {
+                onChange={({value}: any) => {
                     handleChangeField('account_id', value)
                 }}
                 className="flex-1"
+                defaultValue={{
+                    value: formState.account?.id,
+                    label: (
+                        <div key={formState.account?.id} className="flex items-center">
+                            <img src={formState.account?.image} alt="avatar"
+                                 className="w-8 h-8 mr-2 rounded-full"/>
+                            <div>
+                                <div
+                                    className="text-sm font-bold">{formState.account?.name}</div>
+                                <div
+                                    className="text-xs text-gray-500">{formState.account?.email}</div>
+                            </div>
+                        </div>
+                    ),
+                }}
             />,
             'Exchange Rate': <input id="exchangeRate" type="text" value="1" placeholder="Readonly input here…"
-                className="flex-1 form-input disabled:pointer-events-none disabled:bg-[#eee] dark:disabled:bg-[#1b2e4b] cursor-not-allowed"
-                disabled />,
+                                    className="flex-1 form-input disabled:pointer-events-none disabled:bg-[#eee] dark:disabled:bg-[#1b2e4b] cursor-not-allowed"
+                                    disabled/>,
             'ZohoBooksID': (
                 <input
                     id="zoho_books_id"
                     name="zoho_books_id"
+                    defaultValue={formState.zoho_books_id}
                     className="form-input flex-1 "
                     onChange={(e) => handleChangeField(e.target.name, e.target.value)}
                 />
@@ -111,8 +145,17 @@ const InvoiceInformationSection = () => {
                 placeholder="Type at least 2 characters to search..."
                 name="sales_order_id"
                 loadOptions={searchSalesOrder}
-                onChange={({ value }: any) => {
+                onChange={({value}: any) => {
                     handleChangeField('sales_order_id', value)
+                }}
+                defaultValue={{
+                    value: formState.sales_order?.id,
+                    label: (
+                        <div key={formState.sales_order?.id} className="flex items-center">
+                            <div
+                                className="text-sm font-bold">{formState.sales_order?.subject}</div>
+                        </div>
+                    ),
                 }}
                 className="flex-1"
             />,
@@ -120,6 +163,7 @@ const InvoiceInformationSection = () => {
                 id="purchase_order"
                 name="purchase_order"
                 className="form-input flex-1 "
+                defaultValue={formState.purchase_order}
                 onChange={(e) => handleChangeField(e.target.name, e.target.value)}
             />,
             'Deal Stage': <Select
@@ -127,7 +171,9 @@ const InvoiceInformationSection = () => {
                 id="deal_stage"
                 placeholder=""
                 options={Stages}
-                onChange={({ value }: any) => {
+                defaultValue={Stages.find((data) => data.value == formState.deal_stage)}
+
+                onChange={({value}: any) => {
                     handleChangeField('deal_stage', value)
                 }}
             />,
@@ -136,6 +182,7 @@ const InvoiceInformationSection = () => {
                     id="excise_duty"
                     name="excise_duty"
                     className="form-input flex-1 "
+                    defaultValue={formState.excise_duty}
                     onChange={(e) => handleChangeField(e.target.name, e.target.value)}
                 />
             ),
@@ -144,9 +191,10 @@ const InvoiceInformationSection = () => {
                 id="status"
                 placeholder=""
                 options={Statuses}
-                onChange={({ value }: any) => {
+                onChange={({value}: any) => {
                     handleChangeField('status', value)
                 }}
+                defaultValue={Statuses.find((data) => data.value == formState.status)}
             />,
             'Contact Name': <AsyncSelect
                 isMulti={false}
@@ -154,23 +202,32 @@ const InvoiceInformationSection = () => {
                 placeholder="Type at least 2 characters to search..."
                 name="contact_id"
                 loadOptions={searchContacts}
-                onChange={({ value }: any) => {
+                onChange={({value}: any) => {
                     handleChangeField('contact_id', value)
+                }}
+                defaultValue={{
+                    value: formState.contact?.id,
+                    label: (
+                        <div key={formState.contact?.id} className="flex items-center">
+                            <div
+                                className="text-sm font-bold">{formState.contact?.first_name} {formState.contact?.last_name}</div>
+                        </div>
+                    ),
                 }}
                 className="flex-1"
             />,
             'Currency': <Select id="currency" name="currency" options={Currencies}
-                className="flex-1" />,
+                                defaultValue={Currencies.find((data) => data.value == formState.currency)}
+                                className="flex-1"/>,
 
 
         }
     }
     return (<>
-        <div className="flex justify-between lg:flex-row flex-col">
-            <GenerateFields fields={fields} />
-
-        </div>
-    </>
+            <div className="flex justify-between lg:flex-row flex-col">
+                <GenerateFields fields={fields}/>
+            </div>
+        </>
     )
 }
 
