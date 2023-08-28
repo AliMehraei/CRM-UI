3268638ee797e9a72ec61eb9baf69b1c6603bbb0
davood.thinker <davood.thinker@gmail.com>
2023-08-28 10:41:36 +0330

    fix: changed name of the terms_and_conditions to terms_conditions

--- resources/js/src/pages/Invoice/components/create/TermsAndConditionsSection.tsx
+++ resources/js/src/pages/Invoice/components/create/TermsAndConditionsSection.tsx
@@ -17,7 +17,7 @@ const TermsAndConditionsSection = () => {
                         <label className="ltr:mr-2 rtl:ml-2 w-1/3 mb-0">
                             Terms and Conditions
                         </label>
-                        <textarea id="terms_and_conditions" rows={3} name="terms_and_conditions"
+                        <textarea id="terms_and_conditions" rows={3} name="terms_conditions"
                                   className="form-textarea flex-1"
                                   onChange={(e) => handleChangeField(e.target.name, e.target.value)}
                                   placeholder=""></textarea>
