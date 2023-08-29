79395ccf22ee626b3badf5986e5bb037e15c3b49
davood.thinker <davood.thinker@gmail.com>
2023-08-29 10:35:38 +0330

    feat: added using ActionButtonsComponent

--- resources/js/src/pages/Availability/Add.tsx
+++ resources/js/src/pages/Availability/Add.tsx
@@ -1,21 +1,27 @@
 import React, {useEffect} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {setPageTitle} from '../../store/themeConfigSlice';
 import AvailabilityFormFields from "./components/create/AvailabilityFormFields";
 import 'flatpickr/dist/flatpickr.css';
+import ActionButtonsComponent from "../../components/FormFields/ActionButtonsComponent";
 
 const Add = () => {
+    const formState = useSelector((state: any) => state.availabilityForm);
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(setPageTitle('Availability Add'));
     });
 
     return (
-        <div className="flex xl:flex-row flex-col gap-2.5">
-            <div className="panel px-0 flex-1 py-6 ltr:xl:mr-6 rtl:xl:ml-6 overflow-hidden">
-                <AvailabilityFormFields/>
+        <div className='px-4'>
+            <ActionButtonsComponent formState={formState}/>
+            <div className="flex xl:flex-row flex-col gap-2.5">
+                <div className="panel px-0 flex-1 py-6 ltr:xl:mr-6 rtl:xl:ml-6 overflow-hidden">
+                    <AvailabilityFormFields/>
+                </div>
             </div>
         </div>
+
     );
 };
 
