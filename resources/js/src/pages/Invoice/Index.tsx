0e1bb03e21d0fb203c9d8e0296244b70640bcd49
yektadg <yektadg.dev@gmail.com>
2023-08-30 12:47:26 +0330

    fix: fix filter and 2 time fetch data invoice

--- resources/js/src/pages/Invoice/Index.tsx
+++ resources/js/src/pages/Invoice/Index.tsx
@@ -83,7 +83,7 @@ const List = () => {
     const applyFilters = () => {
         setResetFilter(false);
         scrollToTop();
-        fetchDataInvoice(page, pageSize, filters);
+        fetchDataInvoice(page, pageSize, filters,sortStatus);
 
     };
 
@@ -202,12 +202,6 @@ const List = () => {
     }, [items, sortStatus]);
 
     useEffect(() => {
-        fetchDataInvoice(page, pageSize);
-        setInitialRecords(sortBy(items, sortStatus.columnAccessor));
-
-    }, [page, pageSize]); // Added page and pageSize as dependencies
-
-    useEffect(() => {
         setPage(1);
     }, [pageSize]);
 
@@ -219,11 +213,7 @@ const List = () => {
 
     useEffect(() => {
         fetchDataInvoice(page, pageSize, filters, sortStatus);
-    }, [page, pageSize, sortStatus]);
-    useEffect(() => {
-        if (resetFilter)
-            fetchDataInvoice(page, pageSize, filters, sortStatus);
-    }, [resetFilter]);
+    }, [page, pageSize, sortStatus, resetFilter]);
 
     const resetFilters = () => {
         setSelectedFields([]); // Reset selected fields
