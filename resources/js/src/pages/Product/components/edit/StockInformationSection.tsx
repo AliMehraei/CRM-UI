70ce341044828ef4487e69a129906dfc9293f8b0
davood.thinker <davood.thinker@gmail.com>
2023-09-06 10:58:06 +0330

    refactor: change qty to Quantity

--- resources/js/src/pages/Product/components/edit/StockInformationSection.tsx
+++ resources/js/src/pages/Product/components/edit/StockInformationSection.tsx
@@ -1,54 +1,54 @@
 import AsyncSelect from "react-select/async";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import api from "../../../../config/api";
-import {updateFormData} from "../../../../store/productFormSlice";
+import { updateFormData } from "../../../../store/productFormSlice";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
 import Select from "react-select";
-import {handleUploadFile,Currencies,PortalAccess} from "../../../../components/Functions/CommonFunctions";
+import { handleUploadFile, Currencies, PortalAccess } from "../../../../components/Functions/CommonFunctions";
 
 const StockInformationSection = () => {
     const dispatch = useDispatch();
     const formState = useSelector((state: any) => state.productForm);
     const handleChangeField = (field: any, value: any) => {
-        dispatch(updateFormData({[field]: value}));
+        dispatch(updateFormData({ [field]: value }));
     };
 
- 
+
 
 
     const fields = {
         'Stock Information': {
-            
-           
+
+
             'Quantity in Stock': (
                 <input
-                id="quantity_in_stock"
-                name="quantity_in_stock"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)}
-                defaultValue={formState.quantity_in_stock}
+                    id="quantity_in_stock"
+                    name="quantity_in_stock"
+                    className="form-input flex-1 "
+                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                    defaultValue={formState.quantity_in_stock}
                 />
             ),
-          
-            
+
+
         },
         '': {
-            'Qty Ordered': (
+            'Quantity Ordered': (
                 <input
-                id="qty_ordered"
-                name="qty_ordered"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)}
-                defaultValue={formState.qty_ordered}
+                    id="quantity_ordered"
+                    name="quantity_ordered"
+                    className="form-input flex-1 "
+                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                    defaultValue={formState.quantity_ordered}
                 />
             ),
             'Quantity in Demand': (
                 <input
-                id="quantity_in_demand"
-                name="quantity_in_demand"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)}
-                defaultValue={formState.quantity_in_demand}
+                    id="quantity_in_demand"
+                    name="quantity_in_demand"
+                    className="form-input flex-1 "
+                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                    defaultValue={formState.quantity_in_demand}
                 />
             ),
         }
@@ -56,7 +56,7 @@ const StockInformationSection = () => {
     return (
         <>
             <div className="flex justify-between lg:flex-row flex-col">
-                <GenerateFields fields={fields}/>
+                <GenerateFields fields={fields} />
             </div>
         </>
     )
