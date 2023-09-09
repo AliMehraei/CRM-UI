378030b8291e06753362ee7f7198e17c3025b92e
yektadg <yektadg.dev@gmail.com>
2023-09-09 12:45:54 +0330

    fear: change method for fetch user permission and check is login

--- resources/js/src/config/authCheck.ts
+++ resources/js/src/config/authCheck.ts
@@ -1,46 +1,54 @@
 import { useState, useEffect } from 'react';
-import axios from './axios';
 import { getToken } from './config';
-
-export const useUserStatus = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [permissions, setPermissions] = useState([]);
+import api from './api';
+type Permission = string;
+interface UserStatus {
+    isLoggedIn: boolean;
+    permissions: Permission[];
+    hasPermission: (permissionName: Permission) => boolean;
+}
+export const useUserStatus = (): UserStatus => {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [permissions, setPermissions] = useState<Permission[]>([]);
+    const api_instance = new api();
 
     useEffect(() => {
         const token = getToken('token');
+        let isCancelled = false;
 
         async function fetchPermissions() {
             if (!token) {
                 setIsLoggedIn(false);
                 return;
             }
-
             try {
-                const response = await axios.get('/user-permissions', {
-                    headers: {
-                        Authorization: `Bearer ${token}` // Assuming you use Bearer tokens
-                    }
-                });
+                const response = await api_instance.getUserPremissions();
                 
-                if (response.status === 200 && response.data.permissions) {
+                if (!isCancelled && response.status === 200 && Array.isArray(response.data.permissions)) {
                     setIsLoggedIn(true);
                     setPermissions(response.data.permissions);
                 } else {
                     setIsLoggedIn(false);
                 }
             } catch (error) {
-                setIsLoggedIn(false);
-                console.error('Error fetching permissions:', error);
+                if (!isCancelled) {
+                    setIsLoggedIn(false);
+                    console.error('Error fetching permissions:', error);
+                }
             }
         }
-
         fetchPermissions();
-    }, []); // Empty dependency array means this effect runs once when the component mounts
-
-    const hasPermission = (permissionName) => {
+        
+        return () => {
+            isCancelled = true; 
+        };
+    }, []); 
+    // useEffect(() => {
+    //     console.log(isLoggedIn, permissions);
+    // }, [isLoggedIn, permissions]);
+    const hasPermission = (permissionName: Permission): boolean => {
         return permissions.includes(permissionName);
     };
-
     return {
         isLoggedIn,
         permissions,
