a2e30642a27d7f9bf2b2d6547b91faf163d3cdc7
yektadg <yektadg.dev@gmail.com>
2023-09-09 12:45:02 +0330

    feat: add Protected Rote for check auth and user permission

--- resources/js/src/components/ProtectedRoute.tsx
+++ resources/js/src/components/ProtectedRoute.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { useUserStatus } from '../config/authCheck';
 import { useNavigate } from 'react-router-dom';
+import { useUserStatus } from '../config/authCheck';
+import App from '../App';
 
-export const ProtectedRoute = ({ requiredPermission, ...rest }) => {
+interface ProtectedRouteProps {
+    requiredPermission?: string;
+    path?: string;
+    element?: React.ReactNode;
+    children?: React.ReactNode;
+}
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, requiredPermission, path, element }) => {
     const { isLoggedIn, hasPermission } = useUserStatus();
     const navigate = useNavigate();
-
     if (!isLoggedIn) {
         navigate("/auth/login");
         return null;
     }
-
     if (requiredPermission && !hasPermission(requiredPermission)) {
         navigate("/permission-denied");
         return null;
-    }
+    }    
+    return (
+        <App>
+            <div className="text-black dark:text-white-dark min-h-screen">{children}</div>
+        </App>
+    );
+};
 
-    return <Route {...rest} />;
-}
+export default ProtectedRoute;
