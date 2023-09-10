3014a34012b9af18bb184e4df52d94000de9a8ee
davood.thinker <davood.thinker@gmail.com>
2023-09-10 17:43:53 +0330

    test: working on generating fields based on data from database

--- resources/js/src/pages/Product/components/create/ProductFormFields.tsx
+++ resources/js/src/pages/Product/components/create/ProductFormFields.tsx
@@ -1,36 +1,88 @@
-import React from "react";
-import SupplyChainSection from "./SupplyChainSection";
-import ProductInformationSection from "./ProductInformationSection";
-import StockInformationSection from "./StockInformationSection";
-import PhysicalPropertiesSection from "./PhysicalPropertiesSection";
-import TechDataSection from "./TechDataSection";
-import DimensionsSection from "./DimensionsSection";
-import AlternativesSection from "./AlternativesSection";
-import ExportImportDataSection from "./ExportImportDataSection";
-import APIDataSection from "./APIDataSection";
-import DescriptionInformationSection from "./DescriptionInformationSection";
+import React, {useEffect} from "react";
+import {useSelector} from "react-redux";
+import Select from "react-select";
+
 const ProductFormFields = () => {
+    const formState = useSelector((state: any) => state.productForm);
+
+    useEffect(() => {
+    }, []);
+
+    const getHtmlElement = (type: string) => {
+        const elements: any = {
+            'select': <Select/>,
+            'boolean': <input type="checkbox"/>,
+        };
+
+        return elements[type] || null;
+    }
+
+    interface ColumnData {
+        col: string;
+        order: number;
+    }
+
+    type GroupedData = Record<string, ColumnData[]>;
+
+    const groupColumns = (data: ColumnData[]) => {
+        const groupedData = data.reduce<{ [key: string]: ColumnData[] }>((result, item: any) => {
+            const {section_col, order} = item;
+            result[section_col] = [...(result[section_col] || []), item];
+            return result;
+        }, {});
+
+        return Object.values(groupedData).map((group) =>
+            group.sort((a, b) => a.order - b.order)
+        );
+    };
+
     return (
         <div className="mt-8 px-4">
-            <ProductInformationSection key="productInfo" />
+            {formState.layout.layout_sections.map((section: any, index: number) => {
+                const cols = groupColumns(section.layout_data);
+                return (
+                    <>
+                        <div key={`${section.title}_${index}`} className="lg:w-1/2 w-full ltr:lg:mr-6 rtl:lg:ml-6 mb-6">
+                            <div key={section.title} className="">
+                                <div className="text-lg">{section.title}</div>
+                                <div className="flex justify-between  flex-row">
+                                    {cols.map((col: any) => (
+                                        <div className="flex flex-col">
+                                            {col.map((data: any, innerIndex: any) => (
+                                                <div className="flex" key={innerIndex}>
+                                                    {innerIndex}
+                                                </div>
+                                            ))}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                        <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+                    </>
+                )
+            })
+            }
+            {/* <ProductInformationSection key="productInfo"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <SupplyChainSection key="SupplyChain"  />
+            <SupplyChainSection key="SupplyChain"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <PhysicalPropertiesSection key="PhysicalProperties"  />
+            <PhysicalPropertiesSection key="PhysicalProperties"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <TechDataSection key="TechData"  />
+            <TechDataSection key="TechData"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <DimensionsSection key="Dimensions"  />
+            <DimensionsSection key="Dimensions"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <StockInformationSection key="StockInformation"  />
+            <StockInformationSection key="StockInformation"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <AlternativesSection key="Alternatives"  />
+            <AlternativesSection key="Alternatives"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <ExportImportDataSection key="ExportImportData"  />
+            <ExportImportDataSection key="ExportImportData"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <APIDataSection key="APIData"  />
+            <APIDataSection key="APIData"/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-            <DescriptionInformationSection key="DescriptionInformation"  />
+            <DescriptionInformationSection key="DescriptionInformation"/>*/
+            }
         </div>
     )
 
