40c160a47f2cd3c88dcae320a345f5869fd2690e
yektadg <yektadg.dev@gmail.com>
2023-09-10 09:17:59 +0330

    fix: fix authcheck and Proteced routes checks

--- resources/js/src/components/ProtectedRoute.tsx
+++ resources/js/src/components/ProtectedRoute.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUserStatus } from '../config/authCheck';
 import App from '../App';
+import LoadingAlpyn from './LoadingAlpyn';
 
 interface ProtectedRouteProps {
     requiredPermission?: string;
@@ -10,21 +11,29 @@ interface ProtectedRouteProps {
     children?: React.ReactNode;
 }
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children, requiredPermission, path, element }) => {
-    const { isLoggedIn, hasPermission } = useUserStatus();
+    const { isLoggedIn, isLoading, hasPermission } = useUserStatus();
     const navigate = useNavigate();
+
+    if (isLoading) {
+        return <><LoadingAlpyn/></>;
+    }
+
     if (!isLoggedIn) {
         navigate("/auth/login");
         return null;
     }
+
     if (requiredPermission && !hasPermission(requiredPermission)) {
         navigate("/permission-denied");
         return null;
     }    
+    
     return (
         <App>
             <div className="text-black dark:text-white-dark min-h-screen">{children}</div>
         </App>
     );
 };
+
 
 export default ProtectedRoute;
--- resources/js/src/config/authCheck.ts
+++ resources/js/src/config/authCheck.ts
@@ -3,53 +3,51 @@ import { getToken } from './config';
 import api from './api';
 type Permission = string;
 interface UserStatus {
+    isLoading: boolean;
     isLoggedIn: boolean;
     permissions: Permission[];
     hasPermission: (permissionName: Permission) => boolean;
 }
+
 export const useUserStatus = (): UserStatus => {
+    const [isLoading, setIsLoading] = useState<boolean>(true); // Initializing with true
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
     const [permissions, setPermissions] = useState<Permission[]>([]);
     const api_instance = new api();
 
     useEffect(() => {
         const token = getToken('token');
-        let isCancelled = false;
-
+        if (!token) {
+            setIsLoggedIn(false);
+            setIsLoading(false);
+            return;
+        }
         async function fetchPermissions() {
-            if (!token) {
-                setIsLoggedIn(false);
-                return;
-            }
             try {
                 const response = await api_instance.getUserPremissions();
                 
-                if (!isCancelled && response.status === 200 && Array.isArray(response.data.permissions)) {
+                if (response.status === 200 && Array.isArray(response.data.permissions)) {
                     setIsLoggedIn(true);
                     setPermissions(response.data.permissions);
                 } else {
                     setIsLoggedIn(false);
                 }
             } catch (error) {
-                if (!isCancelled) {
-                    setIsLoggedIn(false);
-                    console.error('Error fetching permissions:', error);
-                }
+                setIsLoggedIn(false);
+                console.error('Error fetching permissions:', error);
+            } finally {
+                setIsLoading(false); // Ensure loading state is set to false here
             }
         }
         fetchPermissions();
-        
-        return () => {
-            isCancelled = true; 
-        };
-    }, []); 
-    // useEffect(() => {
-    //     console.log(isLoggedIn, permissions);
-    // }, [isLoggedIn, permissions]);
+    }, []);
+
     const hasPermission = (permissionName: Permission): boolean => {
         return permissions.includes(permissionName);
     };
+
     return {
+        isLoading,
         isLoggedIn,
         permissions,
         hasPermission
