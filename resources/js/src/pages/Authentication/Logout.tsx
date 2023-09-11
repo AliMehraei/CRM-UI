93b484a63e5e761b36c2b49362ad66e464f0930a
yektadg <yektadg.dev@gmail.com>
2023-09-11 18:19:39 +0330

    feat: handle logout function

--- resources/js/src/pages/Authentication/Logout.tsx
+++ resources/js/src/pages/Authentication/Logout.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { removeToken } from '../../config/config';
 import { useNavigate } from 'react-router-dom';
+import { useUserStatus } from '../../config/authCheck';
 
 const Logout = () => {
   const navigate = useNavigate();
+  const { isLoggedIn, logout } = useUserStatus(); 
 
   useEffect(() => {
     // Clear the token and then navigate to the login page
-    removeToken('token');
+   // removeToken('token');
+    logout();
     navigate('/auth/login');
   }, [navigate]);
 
