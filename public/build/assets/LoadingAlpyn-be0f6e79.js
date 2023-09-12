0a703709badda39bee4bda47b7b82369f64584bb
Mohammad Sadegh Maleki <masmaleki@gmail.com>
2023-09-12 11:12:11 +0400

    just build new resources

--- public/build/assets/LoadingAlpyn-be0f6e79.js
+++
@@ -1,1 +0,0 @@
-import{b as a,F as e}from"./main-fcf5e4ad.js";const n=()=>a(e,{children:a("div",{className:"screen_loader fixed inset-0 bg-[#fafafa] dark:bg-[#060818] z-[60] grid place-content-center animate__animated",children:a("img",{src:"/loader-2.gif",alt:"logo"})})});export{n as L};
