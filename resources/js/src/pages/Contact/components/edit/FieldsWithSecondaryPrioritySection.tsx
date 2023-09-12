012be35d3187f30e9e888fc18773c4073cb5baf8
davood.thinker <davood.thinker@gmail.com>
2023-09-12 09:59:52 +0330

    chore: changed name for contact activity

--- resources/js/src/pages/Contact/components/edit/FieldsWithSecondaryPrioritySection.tsx
+++ resources/js/src/pages/Contact/components/edit/FieldsWithSecondaryPrioritySection.tsx
@@ -1,32 +1,32 @@
 import AsyncSelect from "react-select/async";
-import { useDispatch, useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
-import { loadOrders, searchOwners } from "../../../../components/Functions/CommonFunctions";
+import {searchOwners} from "../../../../components/Functions/CommonFunctions";
 import Select from "react-select";
-import { updateFormData } from "../../../../store/contactFormSlice";
+import {updateFormData} from "../../../../store/contactFormSlice";
 
 const FieldsWithSecondaryPrioritySection = () => {
     const dispatch = useDispatch();
     const formState = useSelector((state: any) => state.contactForm);
 
     const handleChangeField = (field: any, value: any) => {
-        dispatch(updateFormData({ [field]: value }));
+        dispatch(updateFormData({[field]: value}));
     };
 
     const activities = [
-        { value: "none", label: "-None-" },
-        { value: "no_activity", label: "No Activity" },
-        { value: "more_1_year", label: "> 1 year Activity" },
-        { value: "more_1_month", label: "> 1 month Activity" },
-        { value: "regular_activity", label: "Regular Activity" },
+        {value: "none", label: "-None-"},
+        {value: "no_activity", label: "No Activity"},
+        {value: "more_1_year", label: "> 1 year Activity"},
+        {value: "more_1_month", label: "> 1 month Activity"},
+        {value: "regular_activity", label: "Regular Activity"},
 
     ];
 
     const doubleCheckStatuses = [
-        { value: "none", label: "-None-" },
-        { value: "back_to_lead", label: "Back to lead" },
-        { value: "converted_to_lead", label: "Converted to lead" },
-        { value: "must_be_deleted", label: "Must be deleted" },
+        {value: "none", label: "-None-"},
+        {value: "back_to_lead", label: "Back to lead"},
+        {value: "converted_to_lead", label: "Converted to lead"},
+        {value: "must_be_deleted", label: "Must be deleted"},
     ];
     const fields = {
         'Fields with Secondary Priority': {
@@ -36,7 +36,7 @@ const FieldsWithSecondaryPrioritySection = () => {
                 placeholder="Type at least 2 characters to search..."
                 name="approved_by_id"
                 loadOptions={searchOwners}
-                onChange={({ value }: any) => {
+                onChange={({value}: any) => {
                     handleChangeField('approved_by_id', value)
                 }}
                 className="flex-1"
@@ -45,9 +45,10 @@ const FieldsWithSecondaryPrioritySection = () => {
                     label: (
                         <div key={formState.approved_by?.id} className="flex items-center">
                             <img src={formState.approved_by?.avatar} alt="avatar"
-                                className="w-8 h-8 mr-2 rounded-full" />
+                                 className="w-8 h-8 mr-2 rounded-full"/>
                             <div>
-                                <div className="text-sm font-bold">{formState.approved_by?.first_name + " " + formState.approved_by?.last_name}</div>
+                                <div
+                                    className="text-sm font-bold">{formState.approved_by?.first_name + " " + formState.approved_by?.last_name}</div>
                                 <div className="text-xs text-gray-500">{formState.approved_by?.email}</div>
                             </div>
                         </div>
@@ -66,13 +67,13 @@ const FieldsWithSecondaryPrioritySection = () => {
 
             'Contact Activity':
                 <Select id="contact_activity"
-                    name="contact_activity"
-                    onChange={({ value }: any) => {
-                        handleChangeField('double_check_status', value)
-                    }}
-                    className="flex-1"
-                    options={activities}
-                    defaultValue={activities.find((title) => title.value == formState.contact_activity)}
+                        name="contact_activity"
+                        onChange={({value}: any) => {
+                            handleChangeField('contact_activity', value)
+                        }}
+                        className="flex-1"
+                        options={activities}
+                        defaultValue={activities.find((title) => title.value == formState.contact_activity)}
 
                 />,
 
@@ -110,50 +111,50 @@ const FieldsWithSecondaryPrioritySection = () => {
             />,
         },
         '':
-        {
-            'Email Opt Out': <input
-                id="email_opt_out"
-                type="checkbox"
-                name="email_opt_out"
-                className="form-checkbox"
-                onChange={(e) => handleChangeField(e.target.name, e.target.checked)}
-                checked={formState.email_opt_out}
-            />,
-            'lead_reference': <input
-                id="lead_reference"
-                name="lead_reference"
-                className="form-input flex-1 "
-                onChange={(e) => handleChangeField(e.target.name, e.target.value)}
-                defaultValue={formState.lead_reference}
-
-            />,
-            'Double Check Status':
-                <Select id="double_check_status"
-                    name="double_check_status"
-                    onChange={({ value }: any) => {
-                        handleChangeField('double_check_status', value)
-                    }}
-                    className="flex-1"
-                    options={doubleCheckStatuses}
-                    defaultValue={doubleCheckStatuses.find((title) => title.value == formState.double_check_status)}
+            {
+                'Email Opt Out': <input
+                    id="email_opt_out"
+                    type="checkbox"
+                    name="email_opt_out"
+                    className="form-checkbox"
+                    onChange={(e) => handleChangeField(e.target.name, e.target.checked)}
+                    checked={formState.email_opt_out}
+                />,
+                'lead_reference': <input
+                    id="lead_reference"
+                    name="lead_reference"
+                    className="form-input flex-1 "
+                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                    defaultValue={formState.lead_reference}
 
                 />,
+                'Double Check Status':
+                    <Select id="double_check_status"
+                            name="double_check_status"
+                            onChange={({value}: any) => {
+                                handleChangeField('double_check_status', value)
+                            }}
+                            className="flex-1"
+                            options={doubleCheckStatuses}
+                            defaultValue={doubleCheckStatuses.find((title) => title.value == formState.double_check_status)}
 
-            'DCheck': <input
-                id="d_check"
-                type="checkbox"
-                name="d_check"
-                className="form-checkbox"
-                onChange={(e) => handleChangeField(e.target.name, e.target.checked)}
-                checked={formState.d_check}
+                    />,
 
-            />,
-        }
+                'DCheck': <input
+                    id="d_check"
+                    type="checkbox"
+                    name="d_check"
+                    className="form-checkbox"
+                    onChange={(e) => handleChangeField(e.target.name, e.target.checked)}
+                    checked={formState.d_check}
+
+                />,
+            }
     }
     return (
         <>
             <div className="flex justify-between lg:flex-row flex-col">
-                <GenerateFields fields={fields} />
+                <GenerateFields fields={fields}/>
             </div>
         </>
     )
