81751d61d49a4aad31b523564af455845c685ce4
yektadg <yektadg.dev@gmail.com>
2023-09-15 13:25:59 +0330

    feat: done with check permissions in index of vendor-rfq module

--- resources/js/src/pages/VendorRFQ/Index.tsx
+++ resources/js/src/pages/VendorRFQ/Index.tsx
@@ -9,12 +9,23 @@ import Select from 'react-select';
 import Swal from 'sweetalert2';
 import api from '../../config/api';
 import { renderFilterValueFiled } from '../../components/FilterValueFiled'
+import { useUserStatus } from '../../config/authCheck';
+import LoadingSasCrm from '../../components/LoadingSasCrm';
 const List = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(setPageTitle('VendorRfq List'));
     });
+    const { hasPermission, isLoading, isLoggedIn } = useUserStatus();
     const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        if (!isLoading && !hasPermission('filter-vendor-rfq') && !hasPermission('read-vendor-rfq')) {
+            setLoading(true);
+            return <LoadingSasCrm />;
+        }
+            setLoading(false);
+            return;
+    }, [isLoading, isLoggedIn, hasPermission]);  
     const [resetFilter, setResetFilter] = useState(false);
     const api_instance = new api();
     const isDark = useSelector((state: IRootState) => state.themeConfig.theme) === 'dark' ? true : false;
@@ -275,10 +286,14 @@ const List = () => {
 
 
     return (
+        ( !hasPermission('read-vendor-rfq') || loading) ? (
+            <LoadingSasCrm />
+          ) : (
         <div className="panel px-0 border-white-light dark:border-[#1b2e4b]" >
             <div className="VendorRfq-table">
                 <div className="mb-4.5 px-5 flex md:items-center md:flex-row flex-col gap-5">
                     <div className="flex items-center gap-2">
+                    {hasPermission('delete-vendor-rfq') && (
                         <button type="button" className="btn btn-danger gap-2" onClick={() => deleteRow()}>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
                                 <path d="M20.5001 6H3.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"></path>
@@ -299,14 +314,17 @@ const List = () => {
                             </svg>
                             Delete
                         </button>
-                        <Link to="/vendor_rfq/add" className="btn btn-primary gap-2">
-                            Add New
-                        </Link>
-
+                        )}
+                    {hasPermission('create-vendor-rfq') && (
+                    <Link to="/vendor_rfq/add" className="btn btn-primary gap-2">
+                        Add New
+                    </Link>
+                    )}
                     </div>
 
                 </div>
                 <div className="grid grid-cols-5 gap-6 mb-6">
+                    {hasPermission('filter-vendor-rfq') && (
                     <div className="panel col-span-1">
                         <h2 className="text-xl font-bold mb-4">Filter By Fields</h2>
 
@@ -385,6 +403,7 @@ const List = () => {
 
                         )}
                     </div>
+                    )}
                     <div className="panel col-span-4">
                         <div className="datatables pagination-padding" >
                             {loading ? (
@@ -406,11 +425,15 @@ const List = () => {
                                             accessor: 'vendor_rfq_name',
                                             sortable: true,
                                             render: ({ vendor_rfq_name,id }) => (
-                                                <NavLink to={`/vendor_rfq/edit/${id}`}>
-                                                    <div className="text-primary underline hover:no-underline font-semibold">{`#${vendor_rfq_name}`}</div>
-                                                </NavLink>
-                                            ),
-                                        },
+                                                hasPermission('update-vendor-rfq') ? (
+                                                    <NavLink to={`/vendor_rfq/edit/${id}`}>
+                                                        <div className="text-primary underline hover:no-underline font-semibold">{`#${vendor_rfq_name}`}</div>
+                                                  </NavLink>
+                                                ) : (
+                                                  <div className="font-semibold">{`#${vendor_rfq_name}`}</div>
+                                                )
+                                              )
+                                            },
                                         {
                                             accessor: 'status',
                                             sortable: true,
@@ -443,63 +466,67 @@ const List = () => {
                                             textAlignment: 'center',
                                             render: ({ id }) => (
                                                 <div className="flex gap-4 items-center w-max mx-auto">
-                                                    <NavLink to={`/vendor_rfq/edit/${id}`} className="flex hover:text-info">
-                                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-4.5 h-4.5">
-                                                            <path
-                                                                opacity="0.5"
-                                                                d="M22 10.5V12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2H13.5"
-                                                                stroke="currentColor"
-                                                                strokeWidth="1.5"
-                                                                strokeLinecap="round"
-                                                            ></path>
-                                                            <path
-                                                                d="M17.3009 2.80624L16.652 3.45506L10.6872 9.41993C10.2832 9.82394 10.0812 10.0259 9.90743 10.2487C9.70249 10.5114 9.52679 10.7957 9.38344 11.0965C9.26191 11.3515 9.17157 11.6225 8.99089 12.1646L8.41242 13.9L8.03811 15.0229C7.9492 15.2897 8.01862 15.5837 8.21744 15.7826C8.41626 15.9814 8.71035 16.0508 8.97709 15.9619L10.1 15.5876L11.8354 15.0091C12.3775 14.8284 12.6485 14.7381 12.9035 14.6166C13.2043 14.4732 13.4886 14.2975 13.7513 14.0926C13.9741 13.9188 14.1761 13.7168 14.5801 13.3128L20.5449 7.34795L21.1938 6.69914C22.2687 5.62415 22.2687 3.88124 21.1938 2.80624C20.1188 1.73125 18.3759 1.73125 17.3009 2.80624Z"
-                                                                stroke="currentColor"
-                                                                strokeWidth="1.5"
-                                                            ></path>
-                                                            <path
-                                                                opacity="0.5"
-                                                                d="M16.6522 3.45508C16.6522 3.45508 16.7333 4.83381 17.9499 6.05034C19.1664 7.26687 20.5451 7.34797 20.5451 7.34797M10.1002 15.5876L8.4126 13.9"
-                                                                stroke="currentColor"
-                                                                strokeWidth="1.5"
-                                                            ></path>
-                                                        </svg>
-                                                    </NavLink>
-                                                    {/* <NavLink to="/vendor_rfq/preview" className="flex hover:text-primary">
-                                                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path
-                                                                opacity="0.5"
-                                                                d="M3.27489 15.2957C2.42496 14.1915 2 13.6394 2 12C2 10.3606 2.42496 9.80853 3.27489 8.70433C4.97196 6.49956 7.81811 4 12 4C16.1819 4 19.028 6.49956 20.7251 8.70433C21.575 9.80853 22 10.3606 22 12C22 13.6394 21.575 14.1915 20.7251 15.2957C19.028 17.5004 16.1819 20 12 20C7.81811 20 4.97196 17.5004 3.27489 15.2957Z"
-                                                                stroke="currentColor"
-                                                                strokeWidth="1.5"
-                                                            />
-                                                            <path
-                                                                d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
-                                                                stroke="currentColor"
-                                                                strokeWidth="1.5"
-                                                            />
-                                                        </svg>
-                                                    </NavLink> */}
-                                                    {/* <NavLink to="" className="flex"> */}
-                                                    <button type="button" className="flex hover:text-danger" onClick={(e) => deleteRow(id)}>
-                                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
-                                                            <path d="M20.5001 6H3.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"></path>
-                                                            <path
-                                                                d="M18.8334 8.5L18.3735 15.3991C18.1965 18.054 18.108 19.3815 17.243 20.1907C16.378 21 15.0476 21 12.3868 21H11.6134C8.9526 21 7.6222 21 6.75719 20.1907C5.89218 19.3815 5.80368 18.054 5.62669 15.3991L5.16675 8.5"
-                                                                stroke="currentColor"
-                                                                strokeWidth="1.5"
-                                                                strokeLinecap="round"
-                                                            ></path>
-                                                            <path opacity="0.5" d="M9.5 11L10 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"></path>
-                                                            <path opacity="0.5" d="M14.5 11L14 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"></path>
-                                                            <path
-                                                                opacity="0.5"
-                                                                d="M6.5 6C6.55588 6 6.58382 6 6.60915 5.99936C7.43259 5.97849 8.15902 5.45491 8.43922 4.68032C8.44784 4.65649 8.45667 4.62999 8.47434 4.57697L8.57143 4.28571C8.65431 4.03708 8.69575 3.91276 8.75071 3.8072C8.97001 3.38607 9.37574 3.09364 9.84461 3.01877C9.96213 3 10.0932 3 10.3553 3H13.6447C13.9068 3 14.0379 3 14.1554 3.01877C14.6243 3.09364 15.03 3.38607 15.2493 3.8072C15.3043 3.91276 15.3457 4.03708 15.4286 4.28571L15.5257 4.57697C15.5433 4.62992 15.5522 4.65651 15.5608 4.68032C15.841 5.45491 16.5674 5.97849 17.3909 5.99936C17.4162 6 17.4441 6 17.5 6"
-                                                                stroke="currentColor"
-                                                                strokeWidth="1.5"
-                                                            ></path>
-                                                        </svg>
-                                                    </button>
+                                                   {hasPermission('delete-vendor-rfq') && (
+                                                <NavLink to={`/vendor_rfq/edit/${id}`} className="flex hover:text-info">
+                                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-4.5 h-4.5">
+                                                        <path
+                                                            opacity="0.5"
+                                                            d="M22 10.5V12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2H13.5"
+                                                            stroke="currentColor"
+                                                            strokeWidth="1.5"
+                                                            strokeLinecap="round"
+                                                        ></path>
+                                                        <path
+                                                            d="M17.3009 2.80624L16.652 3.45506L10.6872 9.41993C10.2832 9.82394 10.0812 10.0259 9.90743 10.2487C9.70249 10.5114 9.52679 10.7957 9.38344 11.0965C9.26191 11.3515 9.17157 11.6225 8.99089 12.1646L8.41242 13.9L8.03811 15.0229C7.9492 15.2897 8.01862 15.5837 8.21744 15.7826C8.41626 15.9814 8.71035 16.0508 8.97709 15.9619L10.1 15.5876L11.8354 15.0091C12.3775 14.8284 12.6485 14.7381 12.9035 14.6166C13.2043 14.4732 13.4886 14.2975 13.7513 14.0926C13.9741 13.9188 14.1761 13.7168 14.5801 13.3128L20.5449 7.34795L21.1938 6.69914C22.2687 5.62415 22.2687 3.88124 21.1938 2.80624C20.1188 1.73125 18.3759 1.73125 17.3009 2.80624Z"
+                                                            stroke="currentColor"
+                                                            strokeWidth="1.5"
+                                                        ></path>
+                                                        <path
+                                                            opacity="0.5"
+                                                            d="M16.6522 3.45508C16.6522 3.45508 16.7333 4.83381 17.9499 6.05034C19.1664 7.26687 20.5451 7.34797 20.5451 7.34797M10.1002 15.5876L8.4126 13.9"
+                                                            stroke="currentColor"
+                                                            strokeWidth="1.5"
+                                                        ></path>
+                                                    </svg>
+                                                </NavLink>
+                                                )}
+                                                {/* <NavLink to="/vendor_rfq/preview" className="flex hover:text-primary">
+                                                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path
+                                                            opacity="0.5"
+                                                            d="M3.27489 15.2957C2.42496 14.1915 2 13.6394 2 12C2 10.3606 2.42496 9.80853 3.27489 8.70433C4.97196 6.49956 7.81811 4 12 4C16.1819 4 19.028 6.49956 20.7251 8.70433C21.575 9.80853 22 10.3606 22 12C22 13.6394 21.575 14.1915 20.7251 15.2957C19.028 17.5004 16.1819 20 12 20C7.81811 20 4.97196 17.5004 3.27489 15.2957Z"
+                                                            stroke="currentColor"
+                                                            strokeWidth="1.5"
+                                                        />
+                                                        <path
+                                                            d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
+                                                            stroke="currentColor"
+                                                            strokeWidth="1.5"
+                                                        />
+                                                    </svg>
+                                                </NavLink> */}
+                                                {/* <NavLink to="" className="flex"> */}
+                                                {hasPermission('delete-vendor-rfq') && (
+                                                <button type="button" className="flex hover:text-danger" onClick={(e) => deleteRow(id)}>
+                                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5">
+                                                        <path d="M20.5001 6H3.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"></path>
+                                                        <path
+                                                            d="M18.8334 8.5L18.3735 15.3991C18.1965 18.054 18.108 19.3815 17.243 20.1907C16.378 21 15.0476 21 12.3868 21H11.6134C8.9526 21 7.6222 21 6.75719 20.1907C5.89218 19.3815 5.80368 18.054 5.62669 15.3991L5.16675 8.5"
+                                                            stroke="currentColor"
+                                                            strokeWidth="1.5"
+                                                            strokeLinecap="round"
+                                                        ></path>
+                                                        <path opacity="0.5" d="M9.5 11L10 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"></path>
+                                                        <path opacity="0.5" d="M14.5 11L14 16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"></path>
+                                                        <path
+                                                            opacity="0.5"
+                                                            d="M6.5 6C6.55588 6 6.58382 6 6.60915 5.99936C7.43259 5.97849 8.15902 5.45491 8.43922 4.68032C8.44784 4.65649 8.45667 4.62999 8.47434 4.57697L8.57143 4.28571C8.65431 4.03708 8.69575 3.91276 8.75071 3.8072C8.97001 3.38607 9.37574 3.09364 9.84461 3.01877C9.96213 3 10.0932 3 10.3553 3H13.6447C13.9068 3 14.0379 3 14.1554 3.01877C14.6243 3.09364 15.03 3.38607 15.2493 3.8072C15.3043 3.91276 15.3457 4.03708 15.4286 4.28571L15.5257 4.57697C15.5433 4.62992 15.5522 4.65651 15.5608 4.68032C15.841 5.45491 16.5674 5.97849 17.3909 5.99936C17.4162 6 17.4441 6 17.5 6"
+                                                            stroke="currentColor"
+                                                            strokeWidth="1.5"
+                                                        ></path>
+                                                    </svg>
+                                                </button>
+                                                )}
                                                     {/* </NavLink> */}
                                                 </div>
                                             ),
@@ -526,6 +553,7 @@ const List = () => {
                 </div>
             </div>
         </div>
+          )
     );
 };
 
