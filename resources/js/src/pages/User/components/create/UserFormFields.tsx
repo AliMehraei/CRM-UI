fcb9b04756f611774a0cc6742cb0151f217448d5
SMDM <yektadg.dev@gmail.com>
2023-09-17 10:43:10 +0330

    feat: added PreferenceSection

--- resources/js/src/pages/User/components/create/UserFormFields.tsx
+++ resources/js/src/pages/User/components/create/UserFormFields.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderSection from "./HeaderSection";
 import ContactInformationSection from "./ContactInformationSection";
+import PreferencesSection from "./PreferencesSection";
 
 const UserFormFields = () => {
     return (
@@ -9,7 +10,7 @@ const UserFormFields = () => {
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
             <ContactInformationSection/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
-
+            <PreferencesSection/>
         </div>
     )
 
