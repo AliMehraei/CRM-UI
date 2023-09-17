762d93f856a35f0267f08362980264c845a2448f
SMDM <yektadg.dev@gmail.com>
2023-09-17 10:36:14 +0330

    feat: added Contact Information Section

--- resources/js/src/pages/User/components/create/UserFormFields.tsx
+++ resources/js/src/pages/User/components/create/UserFormFields.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import HeaderSection from "./HeaderSection";
+import ContactInformationSection from "./ContactInformationSection";
 
 const UserFormFields = () => {
     return (
         <div className="mt-8 px-4">
             <HeaderSection/>
             <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+            <ContactInformationSection/>
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+
         </div>
     )
 
