7bb4fc6d40404bb0e28c5ffdd1dbc172b2668595
SMDM <yektadg.dev@gmail.com>
2023-09-17 10:09:13 +0330

    feat: added user form field

---
+++ resources/js/src/pages/User/components/create/UserFormFields.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserFormFields = () => {
+    return (
+        <div className="mt-8 px-4">
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
+        </div>
+    )
+
+
+}
+
+export default UserFormFields;
