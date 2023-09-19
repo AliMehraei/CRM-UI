bc46bd9547663168ba8f4b3aa71425d8fabef498
SMDM <yektadg.dev@gmail.com>
2023-09-19 18:00:54 +0330

    refactor: revert the FilterSection , because of functionality issues

--- resources/js/src/components/FormFields/GenerateIndexTable.tsx
+++ resources/js/src/components/FormFields/GenerateIndexTable.tsx
@@ -331,87 +331,6 @@ const GenerateIndexTable = ({modelName, tableColumns}: any) => {
         Delete
     </button>)
 
-    const FilterSection = () => (<div className="panel col-span-1">
-        <h2 className="text-xl font-bold mb-4">Filter By Fields</h2>
-
-        {/* Search input */}
-        <div className="mb-4">
-            <input
-                type="text"
-                placeholder="Search fields..."
-                className="border p-2 w-full"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-            />
-        </div>
-
-        {/* Filter by options */}
-        <div className="mb-4">
-            <label className="block font-semibold">Filter by:</label>
-            {filteredOptions.map((option: any, index: any) => (
-                <div key={option.value + index}>
-                    <div key={option.value} className="mb-2">
-                        <label className="flex items-center cursor-pointer">
-                            <input type="checkbox"
-                                   value={option.value}
-                                   onChange={(e) => handleFieldChange(e, option)}
-                                   checked={selectedFields.includes(option.value)}
-                                   className="form-checkbox"/>
-                            <span className=" text-dark">{option.label}</span>
-                        </label>
-
-                    </div>
-                    {/* Search options and Input text for selected fields */}
-                    {selectedFields.length > 0 && (
-                        <>
-                            {
-                                selectedFields.includes(option.value) ? (
-                                    <div key={option.value}>
-                                        <h3 className="text-lg font-semibold mt-4">Search
-                                            Options</h3>
-                                        <div className="mb-4">
-                                            <div className="mb-2">
-                                                <label className="block font-semibold">Search
-                                                    include for {option.value}:</label>
-                                                <Select
-                                                    onChange={(e) => handleConditionChange(option.value, e)}
-                                                    placeholder="Select an include"
-                                                    options={option.conditions}/>
-                                            </div>
-                                            {filters[option.value] != null && (
-                                                <>
-                                                    <div className="mb-2">
-                                                        {renderFilterValueFiled(filters[option.value], option, setFilters, filters)}
-                                                    </div>
-                                                </>
-                                            )}
-                                        </div>
-                                    </div>
-                                ) : null
-                            }
-                        </>
-                    )}
-                </div>
-            ))}
-        </div>
-
-
-        {/* Apply filter button */}
-        {selectedFields.length > 0 && (
-            <div className="flex flex-wrap justify-between space-x-2 md:space-x-4">
-                <button onClick={() => applyFilters({page, pageSize, filters, sortStatus})}
-                        className="btn btn-sm btn-primary">
-                    Apply Filter
-                </button>
-
-                <button onClick={resetFilters} className="btn btn-sm btn-outline-primary">
-                    Reset
-                </button>
-            </div>
-
-        )}
-    </div>)
-
     return (
         (!hasPermission(`read-${modelName}`) || loading) ? (
             <LoadingSasCrm/>
@@ -431,7 +350,88 @@ const GenerateIndexTable = ({modelName, tableColumns}: any) => {
                         </div>
                     </div>
                     <div className="grid grid-cols-5 gap-6 mb-6">
-                        {hasPermission(`filter-${modelName}`) && <FilterSection/>}
+                        {hasPermission(`filter-${modelName}`) && (
+                            <div className="panel col-span-1">
+                                <h2 className="text-xl font-bold mb-4">Filter By Fields</h2>
+
+                                {/* Search input */}
+                                <div className="mb-4">
+                                    <input
+                                        type="text"
+                                        placeholder="Search fields..."
+                                        className="border p-2 w-full"
+                                        value={searchQuery}
+                                        onChange={(e) => setSearchQuery(e.target.value)}
+                                    />
+                                </div>
+
+                                {/* Filter by options */}
+                                <div className="mb-4">
+                                    <label className="block font-semibold">Filter by:</label>
+                                    {filteredOptions.map((option: any, index: any) => (
+                                        <div key={option.value + index}>
+                                            <div key={option.value} className="mb-2">
+                                                <label className="flex items-center cursor-pointer">
+                                                    <input type="checkbox"
+                                                           value={option.value}
+                                                           onChange={(e) => handleFieldChange(e, option)}
+                                                           checked={selectedFields.includes(option.value)}
+                                                           className="form-checkbox"/>
+                                                    <span className=" text-dark">{option.label}</span>
+                                                </label>
+
+                                            </div>
+                                            {/* Search options and Input text for selected fields */}
+                                            {selectedFields.length > 0 && (
+                                                <>
+                                                    {
+                                                        selectedFields.includes(option.value) ? (
+                                                            <div key={option.value}>
+                                                                <h3 className="text-lg font-semibold mt-4">Search
+                                                                    Options</h3>
+                                                                <div className="mb-4">
+                                                                    <div className="mb-2">
+                                                                        <label className="block font-semibold">Search
+                                                                            include for {option.value}:</label>
+                                                                        <Select
+                                                                            onChange={(e) => handleConditionChange(option.value, e)}
+                                                                            placeholder="Select an include"
+                                                                            options={option.conditions}/>
+                                                                    </div>
+                                                                    {filters[option.value] != null && (
+                                                                        <>
+                                                                            <div className="mb-2">
+                                                                                {renderFilterValueFiled(filters[option.value], option, setFilters, filters)}
+                                                                            </div>
+                                                                        </>
+                                                                    )}
+                                                                </div>
+                                                            </div>
+                                                        ) : null
+                                                    }
+                                                </>
+                                            )}
+                                        </div>
+                                    ))}
+                                </div>
+
+
+                                {/* Apply filter button */}
+                                {selectedFields.length > 0 && (
+                                    <div className="flex flex-wrap justify-between space-x-2 md:space-x-4">
+                                        <button onClick={() => applyFilters({page, pageSize, filters, sortStatus})}
+                                                className="btn btn-sm btn-primary">
+                                            Apply Filter
+                                        </button>
+
+                                        <button onClick={resetFilters} className="btn btn-sm btn-outline-primary">
+                                            Reset
+                                        </button>
+                                    </div>
+
+                                )}
+                            </div>
+                        )}
 
                         <div className="panel col-span-4">
                             <div className="datatables pagination-padding">
