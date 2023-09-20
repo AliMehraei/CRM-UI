90835988eb710e425a90003002d62cb40fd09fc0
SMDM <yektadg.dev@gmail.com>
2023-09-20 09:30:57 +0330

    chore: changed methods  names

--- resources/js/src/store/filterSlice.tsx
+++ resources/js/src/store/filterSlice.tsx
@@ -8,15 +8,15 @@ const filterSlice = createSlice({
     name: 'filters',
     initialState: initState,
     reducers: {
-        updateFormData: (state, action) => {
+        updateFilterSlice: (state, action) => {
             return {
                 ...state,
                 ...action.payload,
             };
         },
-        resetForm: () => initState
+        resetFilterSlice: () => initState
     },
 });
 
-export const {updateFormData, resetForm} = filterSlice.actions;
+export const {updateFilterSlice, resetFilterSlice} = filterSlice.actions;
 export default filterSlice.reducer;
