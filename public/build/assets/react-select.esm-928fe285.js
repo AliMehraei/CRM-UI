a2d9d1e8840035afc1fa6a2d69ac1f6754a1c3b4
davood.thinker <davood.thinker@gmail.com>
2023-09-26 21:40:36 +0330

    build: react

---
+++ public/build/assets/react-select.esm-928fe285.js
@@ -0,0 +1,1 @@
+import{u as s,S as o,_ as S}from"./Select-aecb2a80.esm-a9fa599c.js";import{r as e}from"./main-5b2834b9.js";var n=e.forwardRef(function(a,t){var r=s(a);return e.createElement(o,S({ref:t},r))}),l=n;export{l as S};
