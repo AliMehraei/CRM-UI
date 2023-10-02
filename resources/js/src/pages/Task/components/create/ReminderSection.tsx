d193efd35da7d9149cef396ae18bbc7c98866a74
yektadg.dev <yektadg.dev@gmail.com>
2023-10-02 13:04:20 +0330

    feat:Add min and max validation for Days before due in ReminderSection

--- resources/js/src/pages/Task/components/create/ReminderSection.tsx
+++ resources/js/src/pages/Task/components/create/ReminderSection.tsx
@@ -1,20 +1,20 @@
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Flatpickr from "react-flatpickr";
+import Select from "react-select";
 import api from "../../../../config/api";
 import { updateFormData } from "../../../../store/taskFormSlice";
-import Select from "react-select";
-import Flatpickr from "react-flatpickr";
-import { useState } from "react";
 
 const ReminderSection = () => {
     const dispatch = useDispatch();
     const api_instance = new api();
-    const formState = useSelector((state: any) => state.taskForm);
+    const formState = useSelector((state) => state.taskForm);
 
     const [isReminderChecked, setIsReminderChecked] = useState(false);
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [selectedTime, setSelectedTime] = useState(new Date());
-    const [daysBeforeDue, setDaysBeforeDue] = useState('');
-    const [repeatFrequency, setRepeatFrequency] = useState('');
+    const [daysBeforeDue, setDaysBeforeDue] = useState("");
+    const [repeatFrequency, setRepeatFrequency] = useState("");
     const [selectedDays, setSelectedDays] = useState({
         Monday: false,
         Tuesday: false,
@@ -35,24 +35,54 @@ const ReminderSection = () => {
         { value: "none", label: "None" },
         { value: "daily", label: "Daily" },
         { value: "weekly", label: "Weekly" },
-        { value: "specific", label: "Specific days before due date" },
-    ];
+        formState.due_date
+            ? { value: "specific", label: "Specific days before due date" }
+            : null, // Conditionally add the "Specific" option based on formState.due_date
+    ].filter(Boolean);
 
-    const handleChangeField = (field: any, value: any) => {
-        if (field === 'reminder') {
+    const handleChangeField = (field, value) => {
+        if (field === "reminder") {
             setIsReminderChecked(value);
-        } 
+        }
         dispatch(updateFormData({ [field]: value }));
     };
 
-    const handleDayToggle = (day: string) => {
-        setSelectedDays(prev => {
+    const handleDayToggle = (day) => {
+        setSelectedDays((prev) => {
             const updatedDays = { ...prev, [day]: !prev[day] };
             dispatch(updateFormData({ selected_days: updatedDays }));
             return updatedDays;
         });
     };
-    
+
+    // Function to check all days
+    const handleCheckAll = () => {
+        setSelectedDays((prev) => {
+            const updatedDays = {};
+            for (const day in prev) {
+                updatedDays[day] = true;
+            }
+            dispatch(updateFormData({ selected_days: updatedDays }));
+            return updatedDays;
+        });
+    };
+
+    // Function to uncheck all days
+    const handleUncheckAll = () => {
+        setSelectedDays((prev) => {
+            const updatedDays = {};
+            for (const day in prev) {
+                updatedDays[day] = false;
+            }
+            dispatch(updateFormData({ selected_days: updatedDays }));
+            return updatedDays;
+        });
+    };
+
+    // Calculate the maximum number of days before due date
+    const maxDaysBeforeDue = formState.due_date
+        ? Math.floor((new Date(formState.due_date) - new Date()) / (1000 * 60 * 60 * 24))
+        : 0;
 
     return (
         <div className="p-6 border rounded-md shadow-sm bg-white">
@@ -73,32 +103,35 @@ const ReminderSection = () => {
                 <div>
                     <div className="flex items-center space-x-4 my-4">
                         <label>On:</label>
-                        <Flatpickr 
+                        <Flatpickr
                             className="p-2 border rounded-md form-input flex-1"
                             data-enable-time={false}
                             placeholder="YYYY-MM-DD"
-                            value={selectedDate}   
-                            onChange={(_,dateString) => handleChangeField('reminder_on_date', dateString)}
-                            options={{ dateFormat: 'Y-m-d ' }}              
+                            value={selectedDate}
+                            onChange={(_, dateString) =>
+                                handleChangeField("reminder_on_date", dateString)
+                            }
+                            options={{ dateFormat: "Y-m-d " }}
                         />
-                        
+
                         <label>At:</label>
-                        <Flatpickr 
+                        <Flatpickr
                             className="p-2 border rounded-md form-input flex-1"
                             data-enable-time={true}
-                            data-no-calendar={true}   
+                            data-no-calendar={true}
                             value={selectedTime}
-                            onChange={time => {
+                            onChange={(time) => {
                                 setSelectedTime(time[0]);
-                                handleChangeField('reminder_on_time', time[0]);
+                                handleChangeField("reminder_on_time", time[0]);
                             }}
                         />
 
-                        <label>Notify Type:</label> 
-                        <Select id="reminder_type"
+                        <label>Notify Type:</label>
+                        <Select
+                            id="reminder_type"
                             name="reminder_type"
-                            onChange={({ value }: any) => {
-                                handleChangeField('reminder_type', value)
+                            onChange={({ value }) => {
+                                handleChangeField("reminder_type", value);
                             }}
                             className="flex-1"
                             options={reminderType}
@@ -108,40 +141,78 @@ const ReminderSection = () => {
                     <div className="flex items-center space-x-4 mb-4">
                         <label className="w-24">Repeat:</label>
                         <Select
-                            value={repeatOptions.find(option => option.value === repeatFrequency)}
+                            value={repeatOptions.find(
+                                (option) => option.value === repeatFrequency
+                            )}
                             options={repeatOptions}
                             className="flex-1"
                             name="repeat_options"
-                            onChange={({ value }: any) => {
-                                handleChangeField('repeat_options', value)
-                                setRepeatFrequency(value)}}
+                            onChange={({ value }) => {
+                                handleChangeField("repeat_options", value);
+                                setRepeatFrequency(value);
+                            }}
                         />
                     </div>
 
-                    {repeatFrequency === 'specific' && (
-                        <>
-                            <div className="flex items-center space-x-4 mb-4">
-                                <label className="w-24">Days before due:</label>
-                                <input
-                                    type="number"
-                                    min="1"
-                                    className="p-2 border rounded-md form-input flex-1"
-                                    value={daysBeforeDue}
-                                    onChange={(e) => {
-                                        setDaysBeforeDue(e.target.value);
-                                        handleChangeField('days_before_due', e.target.value);
-                                    }}
-                                />
-                            </div>
+                    {repeatFrequency === "specific" && formState.due_date && (
+    <>
+        <div className="flex items-center space-x-4 mb-4">
+            <label className="w-24">Days before due:</label>
+            <input
+                type="number"
+                min="1" // Minimum value allowed is 1
+                max={maxDaysBeforeDue} // Maximum value allowed is the calculated maxDaysBeforeDue
+                className="p-2 border rounded-md form-input flex-1"
+                value={daysBeforeDue}
+                onChange={(e) => {
+                    const inputValue = e.target.value;
+                    // Ensure the input value is within the allowed range
+                    if (inputValue >= 1 && inputValue <= maxDaysBeforeDue) {
+                        setDaysBeforeDue(inputValue);
+                        handleChangeField("days_before_due", inputValue);
+                    }
+                }}
+            />
+            {formState.due_date && (
+                <button
+                    type="button"
+                    className="text-blue-500"
+                    onClick={() => {
+                        setDaysBeforeDue(maxDaysBeforeDue.toString());
+                        handleChangeField(
+                            "days_before_due",
+                            maxDaysBeforeDue.toString()
+                        );
+                    }}
+                >
+                    Max: {maxDaysBeforeDue}
+                </button>
+            )}
+        </div>
+    </>
+)}
 
-                        </>
-                    )}
-
-                    {repeatFrequency === 'weekly' && (
+                    {repeatFrequency === "weekly" && (
                         <div className="mb-4">
                             <label className="block mb-2 w-24">Select Days:</label>
                             <div className="flex space-x-4">
-                                {Object.keys(selectedDays).map(day => (
+                                <div>
+                                    <button
+                                        type="button"
+                                        onClick={handleCheckAll}
+                                        className="bg-blue-500 text-white px-3 py-1 rounded-md mr-2"
+                                    >
+                                        Check All
+                                    </button>
+                                    <button
+                                        type="button"
+                                        onClick={handleUncheckAll}
+                                        className="bg-red-500 text-white px-3 py-1 rounded-md"
+                                    >
+                                        Uncheck All
+                                    </button>
+                                </div>
+                                {Object.keys(selectedDays).map((day) => (
                                     <div key={day} className="flex items-center space-x-2">
                                         <input
                                             type="checkbox"
@@ -159,7 +230,7 @@ const ReminderSection = () => {
                 </div>
             )}
         </div>
-    )
-}
+    );
+};
 
 export default ReminderSection;
