6b70997eb5b8fe7f9f369fd1dc41ee3ca94b7f09
davood.thinker <davood.thinker@gmail.com>
2023-10-10 11:06:30 +0330

    feat: handled autofill address after selecting the sales order

--- resources/js/src/pages/Purchase/components/create/AddressInformationSection.tsx
+++ resources/js/src/pages/Purchase/components/create/AddressInformationSection.tsx
@@ -1,46 +1,65 @@
 import AsyncSelect from "react-select/async";
 import {useDispatch, useSelector} from "react-redux";
-import api from "../../../../config/api";
 import {updateFormData} from "../../../../store/purchaseOrderFormSlice";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
-import Select from "react-select";
 
 
 const AddressInformationSection = () => {
     const dispatch = useDispatch();
-    const api_instance = new api();
     const formState = useSelector((state: any) => state.purchaseOrderForm);
+
     const handleChangeField = (field: any, value: any) => {
         dispatch(updateFormData({[field]: value}));
     };
 
-
-   
     const fields = {
         'Address Information': {
-            'Billing Street': <input required id="billing_street" name="billing_street" className="form-input flex-1"
-                                     onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
-            'Billing City': <input required id="billing_city" name="billing_city" className="form-input flex-1"
-                                   onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
-            'Billing State': <input id="billing_state" name="billing_state" className="form-input flex-1"
-                                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
-            'Billing Code': <input required id="billing_code" name="billing_code" className="form-input flex-1"
-                                   onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
-            'Billing Country': <input required id="billing_country" name="billing_country"
-                                      className="form-input flex-1"
-                                      onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
+            'Billing Street': <input id="billing_street" name="billing_street"
+                                     required
+                                     value={formState.billing_street ?? formState.sales_order?.account?.billing_street}
+                                     onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                     className="form-input flex-1"/>,
+            'Billing City': <input id="billing_city" name="billing_city"
+                                   required
+                                   value={formState.billing_city ?? formState.sales_order?.account?.billing_city}
+                                   onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                   className="form-input flex-1"/>,
+            'Billing State': <input id="billing_state" name="billing_state"
+                                    value={formState.billing_state ?? formState.sales_order?.account?.billing_state}
+                                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                    className="form-input flex-1"/>,
+            'Billing Code': <input id="billing_code" name="billing_code"
+                                   required
+                                   value={formState.billing_code ?? formState.sales_order?.account?.billing_code}
+                                   onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                   className="form-input flex-1"/>,
+            'Billing Country': <input id="billing_country" name="billing_country"
+                                      required
+                                      value={formState.billing_country ?? formState.sales_order?.account?.billing_country}
+                                      onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                      className="form-input flex-1"/>,
         },
         '': {
-            'Shipping Street': <input id="shipping_street" name="shipping_street" className="form-input flex-1"
-                                      onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
-            'Shipping City': <input id="shipping_city" name="shipping_city" className="form-input flex-1"
-                                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
-            'Shipping State': <input id="shipping_state" name="shipping_state" className="form-input flex-1"
-                                     onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
-            'Shipping Code': <input id="shipping_code" name="shipping_code" className="form-input flex-1"
-                                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
-            'Shipping Country': <input id="shipping_country" name="shipping_country" className="form-input flex-1"
-                                       onChange={(e) => handleChangeField(e.target.name, e.target.value)}/>,
+            'Shipping Street': <input id="shipping_street" name="shipping_street"
+                                      value={formState.shipping_street ?? formState.sales_order?.account?.shipping_street}
+                                      onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                      className="form-input flex-1"/>,
+            'Shipping City': <input id="shipping_city" name="shipping_city"
+                                    value={formState.shipping_city ?? formState.sales_order?.account?.shipping_city}
+                                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                    className="form-input flex-1"/>,
+            'Shipping State': <input id="shipping_state" name="shipping_state"
+                                     value={formState.shipping_state ?? formState.sales_order?.account?.shipping_state}
+                                     onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                     className="form-input flex-1"/>,
+            'Shipping Code': <input id="shipping_code" name="shipping_code"
+                                    value={formState.shipping_code ?? formState.sales_order?.account?.shipping_code}
+                                    onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                    className="form-input flex-1"/>,
+            'Shipping Country': <input id="shipping_country" name="shipping_country"
+                                       value={formState.shipping_country ?? formState.sales_order?.account?.shipping_country}
+                                       onChange={(e) => handleChangeField(e.target.name, e.target.value)}
+                                       className="form-input flex-1"/>,
         }
     }
     return (
