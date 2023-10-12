f5ce37c2c23a72cf571ff74381ea6542535e55c3
davood.thinker <davood.thinker@gmail.com>
2023-10-12 17:50:13 +0330

    chore: added handleUploadAttachments , handleDeleteAttachment , useEffect for fetchAttachment

--- resources/js/src/components/FormFields/AttachmentSection.tsx
+++ resources/js/src/components/FormFields/AttachmentSection.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useDropzone} from 'react-dropzone';
+import Api from "../../config/api";
 
 const thumbsContainer: any = {
     display: 'flex',
@@ -23,19 +24,8 @@ const baseStyle: any = {
     transition: 'border .24s ease-in-out'
 };
 
-const focusedStyle = {
-    borderColor: '#2196f3'
-};
-
-const acceptStyle = {
-    borderColor: '#00e676'
-};
-
-const rejectStyle = {
-    borderColor: '#ff1744'
-};
 const thumb: any = {
-    display: 'inline-flex',
+    display: 'inline-block',
     borderRadius: 2,
     border: '1px solid #eaeaea',
     marginBottom: 8,
@@ -59,40 +49,73 @@ const img = {
 };
 
 
-const AttachmentSection = () => {
+const AttachmentSection = ({modelName, modelId}: any) => {
     const [files, setFiles] = useState([]);
+    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
     const {getRootProps, getInputProps} = useDropzone({
         accept: {
             'image/*': []
         },
         onDrop: (acceptedFiles: any) => {
+            handleUploadAttachment(acceptedFiles);
             setFiles(acceptedFiles.map((file: any) => Object.assign(file, {
                 preview: URL.createObjectURL(file)
             })));
         }
     });
-
-    const handleDeleteAttachment = () => {
-
-    }
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-
+    const api_instance = new Api();
     const handleMouseEnter = (index: number) => {
         setHoveredIndex(index);
     };
-
     const handleMouseLeave = () => {
         setHoveredIndex(null);
     };
 
+    const handleUploadAttachment = (files: any) => {
+        api_instance.uploadAttachments(files, modelName, modelId)
+            .then((response) => {
+                console.log(response);
+            }).catch((error) => {
+            console.log(error);
+        });
+    }
+    const handleDeleteAttachment: any = (attachmentId: string) => {
+        api_instance.deleteAttachments(attachmentId)
+            .then((response) => {
+                response.status == 200
+            }).catch((error) => {
+
+        })
+    }
+
+    useEffect(() => {
+        api_instance.fetchAttachments({modelId: modelId, modelName: modelName})
+            .then(response => {
+                console.log(response)
+            }).catch((error: any) => {
+            console.log(error)
+        });
+
+    }, [modelId, modelName]);
+
+    useEffect(() => {
+        // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+        return () => files.forEach((file: any) => URL.revokeObjectURL(file.preview));
+    }, []);
+
+
     const thumbs = files.map((file: any, index: number) => (
         <div
+            className="relative  m-2"
             style={thumb}
             key={file.name}
             onMouseEnter={() => handleMouseEnter(index)}
             onMouseLeave={handleMouseLeave}
+            onClick={handleDeleteAttachment}
         >
-            <div style={thumbInner} position="relative">
+            <div
+                style={thumbInner}
+            >
                 <img
                     alt={file.name}
                     src={file.preview}
@@ -104,34 +127,17 @@ const AttachmentSection = () => {
                         URL.revokeObjectURL(file.preview);
                     }}
                 />
+                {/* Add a message on top of the image */}
                 {hoveredIndex === index && (
                     <div
-                       /* style={{
-                            position: 'absolute',
-                            top: '50%',
-                            left: '50%',
-                            transform: 'translate(-50%, -50%)',
-                            color: 'red',
-                            cursor: 'pointer',
-                            zIndex: 1, // Ensure the cross is above the blurred image
-                        }}*/
-                        onClick={() => {
-                            console.log("clicked")
-                        }}
-                    >
+                        className="cursor-pointer absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-center text-red-700 bg-opacity-70 p-2 rounded">
                         &#10006;
-                       <h1 className="red">red cross</h1>
-                    </div>
-                )}
+                        Delete
+                    </div>)}
             </div>
+
         </div>
     ));
-
-
-    useEffect(() => {
-        // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
-        return () => files.forEach((file: any) => URL.revokeObjectURL(file.preview));
-    }, []);
 
     return (
         <section className="container">
