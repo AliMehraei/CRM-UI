5134fbbb4fc6a24fb2a43897fa4e71eae6458639
davood.thinker <davood.thinker@gmail.com>
2023-10-22 16:14:36 +0330

    chore: handled image preview

--- resources/js/src/pages/Product/components/edit/ProductInformationSection.tsx
+++ resources/js/src/pages/Product/components/edit/ProductInformationSection.tsx
@@ -7,8 +7,9 @@ import {
     handleUploadFile,
     searchOwners,
     searchRFQ,
-    searchManufacturer
+    searchManufacturer, getImageSource
 } from "../../../../components/Functions/CommonFunctions";
+import ClearButtonComponent from "../../../../components/FormFields/ClearButtonComponent";
 
 const ProductInformationSection = () => {
     const dispatch = useDispatch();
@@ -26,18 +27,34 @@ const ProductInformationSection = () => {
 
     const fields = {
         'Product Information': {
-            'Product Image': (<input
-                id="image"
-                key="image"
-                type="file"
-                className="form-input file:py-2 file:px-4 file:border-0 file:font-semibold p-0 file:bg-primary/90 ltr:file:mr-5 rtl:file:ml-5 file:text-white file:hover:bg-primary flex-1"
-                accept="image/*"
-                onChange={(e) => handleUploadFile(e, (response: any) => {
-                    dispatch(updateFormData({ 'image': `${response?.data.data.file_url}` }));
-                })}
-
-                name="image"
-            />
+            'Product Image': (
+                <div className="">
+                    <div className="flex">
+                        <input
+                            id="image"
+                            key="image"
+                            type="file"
+                            className="form-input file:py-2 file:px-4 file:border-0 file:font-semibold p-0 file:bg-primary/90 ltr:file:mr-5 rtl:file:ml-5 file:text-white file:hover:bg-primary flex-1"
+                            accept="image/*"
+                            onChange={(e) => handleUploadFile(e, (response: any) => {
+                                dispatch(updateFormData({'image': `${response?.data.data.file_url}`}));
+                            })}
+                            name="image"
+                        />
+                        <ClearButtonComponent callBack={() => {
+                            const fileInput = document.getElementById('image') as HTMLInputElement | null;
+                            if (fileInput) {
+                                fileInput.value = '';
+                                fileInput.dispatchEvent(new Event('change', {bubbles: true}));
+                            }
+                            dispatch(updateFormData({'image': null}));
+                        }}/>
+                    </div>
+                    <img
+                        id="image_preview"
+                        src={getImageSource(formState.image || formState.oldImage)}
+                        alt="img" className="mt-4 w-20 h-20 rounded"/>
+                </div>
             ),
 
             'Product Name': (
