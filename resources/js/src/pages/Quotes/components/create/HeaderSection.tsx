b41d4218c19a3f7b84693b315db4e3fbc2756cb2
davood.thinker <davood.thinker@gmail.com>
2023-10-23 09:58:55 +0330

    chore: Used File upload component

--- resources/js/src/pages/Quotes/components/create/HeaderSection.tsx
+++ resources/js/src/pages/Quotes/components/create/HeaderSection.tsx
@@ -1,18 +1,21 @@
 import AsyncSelect from "react-select/async";
 import Select from "react-select";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {updateFormData} from "../../../../store/quoteFormSlice";
 import {
-    Currencies, handleUploadFile,
+    Currencies,
     searchAccounts, searchContacts, searchDeals,
     searchOwners,
     searchRFQ
 } from "../../../../components/Functions/CommonFunctions";
 import Api from "../../../../config/api";
+import FileUploadComponent from "../../../../components/FormFields/FileUploadComponent";
 
 const HeaderSection = () => {
     const api_instance = new Api();
+    const formState = useSelector((state: any) => state.quoteForm);
+
     const QuoteChances = [
         {value: 'none', label: '-None-'},
         {value: 'high', label: 'High'},
@@ -37,7 +40,7 @@ const HeaderSection = () => {
         dispatch(updateFormData({[field]: value}));
     };
 
-     const handleChangeAccount = async (value: string) => {
+    const handleChangeAccount = async (value: string) => {
         const accountResponse = await api_instance.fetchSingleAccount(value);
         if (accountResponse.status != 200)
             return;
@@ -69,7 +72,7 @@ const HeaderSection = () => {
     const fields = {
         'Header': {
             'Account Name': <AsyncSelect
-                    defaultOptions={true}
+                defaultOptions={true}
                 isMulti={false}
                 id="account_id"
                 required
@@ -82,21 +85,21 @@ const HeaderSection = () => {
                 }}
                 className="flex-1"/>,
             'Contact Name': <AsyncSelect
-                    defaultOptions={true} isMulti={false} id="contact_id" name="contact_id"
-                                         placeholder="Type at least 2 characters to search..."
-                                         loadOptions={searchContacts}
-                                         onChange={({value}: any) => {
-                                             handleChangeField('contact_id', value)
-                                         }}
-                                         className="flex-1"/>,
+                defaultOptions={true} isMulti={false} id="contact_id" name="contact_id"
+                placeholder="Type at least 2 characters to search..."
+                loadOptions={searchContacts}
+                onChange={({value}: any) => {
+                    handleChangeField('contact_id', value)
+                }}
+                className="flex-1"/>,
             'RFQ': <AsyncSelect
-                    defaultOptions={true} isMulti={false} id="rfq_id" name="rfq_id"
-                                placeholder="Type at least 2 characters to search..."
-                                loadOptions={searchRFQ}
-                                onChange={({value}: any) => {
-                                    handleChangeField('rfq_id', value)
-                                }}
-                                className="flex-1"/>,
+                defaultOptions={true} isMulti={false} id="rfq_id" name="rfq_id"
+                placeholder="Type at least 2 characters to search..."
+                loadOptions={searchRFQ}
+                onChange={({value}: any) => {
+                    handleChangeField('rfq_id', value)
+                }}
+                className="flex-1"/>,
 
             'Customer RFQ No': <input id="customer_rfq_no" type="text" name="customer_rfq_no"
                                       className="form-input flex-1 "
@@ -108,13 +111,13 @@ const HeaderSection = () => {
                               onChange={(e) => handleChangeField(e.target.name, e.target.value)}
             />,
             'Converted by': <AsyncSelect
-                    defaultOptions={true} isMulti={false} id="converted_by_id" name="converted_by_id"
-                                         placeholder="Type at least 2 characters to search..."
-                                         loadOptions={searchOwners}
-                                         className="flex-1"
-                                         onChange={({value}: any) => {
-                                             handleChangeField('converted_by_id', value)
-                                         }}
+                defaultOptions={true} isMulti={false} id="converted_by_id" name="converted_by_id"
+                placeholder="Type at least 2 characters to search..."
+                loadOptions={searchOwners}
+                className="flex-1"
+                onChange={({value}: any) => {
+                    handleChangeField('converted_by_id', value)
+                }}
             />,
             'Quote Chance': <Select name='quote_chance' required options={QuoteChances}
                                     className="flex-1"
@@ -132,30 +135,30 @@ const HeaderSection = () => {
         },
         '': {
             'Quote Owner': <AsyncSelect
-                    defaultOptions={true} isMulti={false} id="owner_id" name="owner_id"
-                                        placeholder="Type at least 2 characters to search..."
-                                        loadOptions={searchOwners}
-                                        onChange={({value}: any) => {
-                                            handleChangeField('owner_id', value)
-                                        }}
-                                        className="flex-1"/>,
+                defaultOptions={true} isMulti={false} id="owner_id" name="owner_id"
+                placeholder="Type at least 2 characters to search..."
+                loadOptions={searchOwners}
+                onChange={({value}: any) => {
+                    handleChangeField('owner_id', value)
+                }}
+                className="flex-1"/>,
             'PM User': <AsyncSelect
-                    defaultOptions={true} required isMulti={false} id="pm_user_id" name="pm_user_id"
-                                    placeholder="Type at least 2 characters to search..."
-                                    loadOptions={searchOwners}
-                                    onChange={({value}: any) => {
-                                        handleChangeField('pm_user_id', value)
-                                    }}
-                                    className="flex-1"/>,
+                defaultOptions={true} required isMulti={false} id="pm_user_id" name="pm_user_id"
+                placeholder="Type at least 2 characters to search..."
+                loadOptions={searchOwners}
+                onChange={({value}: any) => {
+                    handleChangeField('pm_user_id', value)
+                }}
+                className="flex-1"/>,
 
             'Deals Name': <AsyncSelect
-                    defaultOptions={true} isMulti={false} id="deal_id" name="deal_id"
-                                       placeholder="Type at least 2 characters to search..."
-                                       loadOptions={searchDeals}
-                                       onChange={({value}: any) => {
-                                           handleChangeField('deal_id', value)
-                                       }}
-                                       className="flex-1"/>,
+                defaultOptions={true} isMulti={false} id="deal_id" name="deal_id"
+                placeholder="Type at least 2 characters to search..."
+                loadOptions={searchDeals}
+                onChange={({value}: any) => {
+                    handleChangeField('deal_id', value)
+                }}
+                className="flex-1"/>,
 
             'Quote Stage': <Select name="quote_stage" required options={QuoteStages}
                                    className="flex-1"
@@ -165,14 +168,14 @@ const HeaderSection = () => {
                                    defaultValue={{value: 'draft', label: 'Draft'}}
             />,
 
-            'Quote File(Excel)': <input
-                type="file"
-                className="form-input file:py-2 file:px-4 file:border-0 file:font-semibold p-0 file:bg-primary/90 ltr:file:mr-5 rtl:file:ml-5 file:text-white file:hover:bg-primary flex-1"
-                accept="image/*,.zip,.pdf,.xls,.xlsx,.txt.doc,.docx"
-                onChange={(e) => handleUploadFile(e, (response: any) => {
-                    dispatch(updateFormData({'quote_file': `${response?.data.data.file_url}`}));
-                })}
-            />,
+            'Quote File(Excel)':
+                <FileUploadComponent
+                    id={'quote_file'}
+                    modelName="quote"
+                    formState={formState}
+                    formAttribute={'quote_file'}
+                    updateFormdata={updateFormData}
+                />,
             'Exchange Rate': <input id="exchangeRate" type="text" value="1" placeholder="Readonly input here…"
                                     className="flex-1 form-input disabled:pointer-events-none disabled:bg-[#eee] dark:disabled:bg-[#1b2e4b] cursor-not-allowed"
                                     disabled/>,
