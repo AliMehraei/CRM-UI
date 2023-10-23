51a91d49ce69c08d102eee1ca05f754bde5fe21f
davood.thinker <davood.thinker@gmail.com>
2023-10-23 09:55:28 +0330

    chore: Used Image and File upload component

--- resources/js/src/pages/Manufacturer/components/edit/ManufacturerInformationSection.tsx
+++ resources/js/src/pages/Manufacturer/components/edit/ManufacturerInformationSection.tsx
@@ -1,48 +1,28 @@
 import AsyncSelect from "react-select/async";
-import { useDispatch, useSelector } from "react-redux";
-import { updateFormData } from "../../../../store/manufacturerFormSlice";
+import {useDispatch, useSelector} from "react-redux";
+import {updateFormData} from "../../../../store/manufacturerFormSlice";
 import GenerateFields from "../../../../components/FormFields/GenerateFields";
-import { getImageSource, handleUploadFile, searchOwners } from "../../../../components/Functions/CommonFunctions";
-import ClearButtonComponent from "../../../../components/FormFields/ClearButtonComponent";
+import {searchOwners} from "../../../../components/Functions/CommonFunctions";
+import ImageUploadComponent from "../../../../components/FormFields/ImageUploadComponent";
 
 const ManufacturerInformationSection = () => {
     const dispatch = useDispatch();
     const formState = useSelector((state: any) => state.manufacturerForm);
 
     const handleChangeField = (field: any, value: any) => {
-        dispatch(updateFormData({ [field]: value }));
+        dispatch(updateFormData({[field]: value}));
     };
 
 
     const fields = {
         'Manufacturer Information': {
-            'Manufacturer Image': (<div className="">
-                <div className="flex">
-                    <input
-                        id="manufacturer_image"
-                        key="manufacturer_image"
-                        type="file"
-                        className="form-input file:py-2 file:px-4 file:border-0 file:font-semibold p-0 file:bg-primary/90 ltr:file:mr-5 rtl:file:ml-5 file:text-white file:hover:bg-primary flex-1"
-                        accept="image/*"
-                        onChange={(e) => handleUploadFile(e, (response: any) => {
-                            dispatch(updateFormData({ 'image': `${response?.data.data.file_url}` }));
-                        })}
-                        name="manufacturerImage"
-                    />
-                    <ClearButtonComponent callBack={() => {
-                        const fileInput = document.getElementById('manufacturer_image') as HTMLInputElement | null;
-                        if (fileInput) {
-                            fileInput.value = '';
-                            fileInput.dispatchEvent(new Event('change', { bubbles: true }));
-                        }
-                        dispatch(updateFormData({ 'image': null }));
-                    }} />
-                </div>
-                <img
-                    id="manufacturer_image_preview"
-                    src={getImageSource(formState.image || formState.oldImage)}
-                    alt="img" className="mt-4 w-20 h-20 rounded" />
-            </div>
+            'Manufacturer Image': (
+                <ImageUploadComponent formState={formState}
+                                      modelName={'manufacturer'}
+                                      id={'manufacturer_image'}
+                                      formAttribute={'image'}
+                                      updateFormData={updateFormData}
+                />
             ),
             'Manufacturer Name': (
                 <input
@@ -94,7 +74,7 @@ const ManufacturerInformationSection = () => {
                     placeholder="Type at least 2 characters to search..."
                     name="owner_id"
                     loadOptions={searchOwners}
-                    onChange={({ value }: any) => {
+                    onChange={({value}: any) => {
                         handleChangeField('owner_id', value)
                     }}
                     className="flex-1"
@@ -103,14 +83,15 @@ const ManufacturerInformationSection = () => {
                         label: (
                             <div key={formState.owner?.id} className="flex items-center">
                                 {formState.owner ? (
-                                <img
-                                    src={formState.owner.avatar ?? '/assets/images/user-profile.jpeg'}
-                                    alt="avatar"
-                                    className="w-8 h-8 mr-2 rounded-full"
-                                />
+                                    <img
+                                        src={formState.owner.avatar ?? '/assets/images/user-profile.jpeg'}
+                                        alt="avatar"
+                                        className="w-8 h-8 mr-2 rounded-full"
+                                    />
                                 ) : null}
                                 <div>
-                                    <div className="text-sm font-bold">{formState.owner?.first_name + " " + formState.owner?.last_name}</div>
+                                    <div
+                                        className="text-sm font-bold">{formState.owner?.first_name + " " + formState.owner?.last_name}</div>
                                     <div className="text-xs text-gray-500">{formState.owner?.email}</div>
                                 </div>
                             </div>
@@ -123,7 +104,7 @@ const ManufacturerInformationSection = () => {
     return (
         <>
             <div className="flex justify-between lg:flex-row flex-col">
-                <GenerateFields fields={fields} />
+                <GenerateFields fields={fields}/>
             </div>
         </>
     )
