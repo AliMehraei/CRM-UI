827886614eca76dc4058e67a05ee4309d927d635
davood.thinker <davood.thinker@gmail.com>
2023-10-24 09:43:23 +0330

    chore: added Add call file

---
+++ resources/js/src/pages/Call/Add.tsx
@@ -0,0 +1,41 @@
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {setPageTitle} from '../../store/themeConfigSlice';
+import CallFormFields from "./components/create/CallFormFields";
+import ActionButtonsComponent from "../../components/FormFields/ActionButtonsComponent";
+import 'flatpickr/dist/flatpickr.css';
+import {resetForm} from "../../store/callFormSlice";
+import {useUserStatus} from "../../config/authCheck";
+import LoadingSasCrm from '../../components/LoadingSasCrm';
+
+const Add = () => {
+    const {hasPermission} = useUserStatus();
+    const formState = useSelector((state: any) => state.callForm);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setPageTitle('Call Add'));
+    });
+
+    useEffect(() => {
+        dispatch(resetForm());
+    }, []);
+
+    return (
+        (!hasPermission(`create-call`) ) ? (
+            <LoadingSasCrm/>
+        ) : (
+        <div className='px-4'>
+            <ActionButtonsComponent formState={formState} resetForm={resetForm}/>
+            <div className="flex xl:flex-row flex-col gap-2.5">
+                <div className="panel px-0 flex-1 py-6 ltr:xl:mr-6 rtl:xl:ml-6 overflow-hidden">
+                    <CallFormFields/>
+                </div>
+            </div>
+        </div>
+        )
+
+    );
+};
+
+export default Add;
