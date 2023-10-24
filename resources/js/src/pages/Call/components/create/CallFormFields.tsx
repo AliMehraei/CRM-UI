b7b0c3a6767c42abafa2ea2da8c5ab7f3e916790
davood.thinker <davood.thinker@gmail.com>
2023-10-24 10:11:59 +0330

    chore: Added Call Information section

--- resources/js/src/pages/Call/components/create/CallFormFields.tsx
+++ resources/js/src/pages/Call/components/create/CallFormFields.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import CallInformationSection from "./CallInformationSection";
 
 
 
 const CallFormFields = () => {
     return (
         <div className="mt-8 px-4">
+            <CallInformationSection/>
+            <hr className="border-white-light dark:border-[#1b2e4b] my-6"/>
 
         </div>
     )
