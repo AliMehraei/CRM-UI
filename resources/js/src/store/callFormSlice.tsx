480158feeaa564263f8537f8edfa2660b556fb22
davood.thinker <davood.thinker@gmail.com>
2023-10-24 09:40:38 +0330

    chore: added callFormSlice

---
+++ resources/js/src/store/callFormSlice.tsx
@@ -0,0 +1,34 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+const initState = {
+    api: "createSingleCall",
+    createRoute: "/call/add",
+    redirectTo: "/call/edit/:id",
+    action: "create",
+};
+
+const callFormSlice = createSlice({
+    name: 'callForm',
+    initialState: initState,
+    reducers: {
+        updateFormData: (state, action) => {
+            return {
+                ...state,
+                ...action.payload,
+            };
+        },
+        resetForm: () => {
+            const fileInput = document.querySelector('[type="file"]') as HTMLInputElement | null;
+            for (let element in fileInput) {
+                fileInput.value = ''; // Clear the value
+                fileInput.dispatchEvent(new Event('change', {bubbles: true})); // Trigger a change event
+            }
+
+            return initState
+        }
+    },
+});
+
+export const {updateFormData, resetForm} = callFormSlice.actions;
+export default callFormSlice.reducer;
