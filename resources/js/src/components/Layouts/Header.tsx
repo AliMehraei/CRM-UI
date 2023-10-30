6a1568d5f4ff6d00e5f8b6164cd311bc27b5bc49
yektadg.dev <yektadg.dev@gmail.com>
2023-10-30 12:17:00 +0330

    feat: add menu for mobile

--- resources/js/src/components/Layouts/Header.tsx
+++ resources/js/src/components/Layouts/Header.tsx
@@ -168,7 +168,6 @@ const Header = () => {
                             </svg>
                         </button>
                     </div>
-                    {/* TODO: fix menu in mobile */}
                     <div
                         className="sm:flex-1 ltr:sm:ml-0 ltr:ml-auto sm:rtl:mr-0 rtl:mr-auto flex items-center space-x-1.5 lg:space-x-2 rtl:space-x-reverse dark:text-[#d0d2d6]">
                         <div className="sm:ltr:mr-auto sm:rtl:ml-auto">
