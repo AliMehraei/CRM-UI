5cdd4bed795aeac1b49f4dc001adb2d31f32044e
davood.thinker <davood.thinker@gmail.com>
2023-10-31 17:24:28 +0330

    chore: fixed some style

--- resources/js/src/components/Layouts/components/SeachSection.tsx
+++ resources/js/src/components/Layouts/components/SeachSection.tsx
@@ -1,7 +1,33 @@
 import React, {useState} from 'react';
+import AsyncSelect from "react-select/async";
+import {searchContacts, searchLead} from "../../Functions/CommonFunctions";
 
 const SearchSection = () => {
     const [search, setSearch] = useState(false);
+
+    const CustomDropDownIndicator = () => <>
+        <button type="button"
+                className="absolute w-9 h-9 inset-0 ltr:left-auto rtl:left-auto appearance-none peer-focus:text-primary">
+            <svg className="mx-auto" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+                <circle cx="11.5" cy="11.5" r="9.5" stroke="currentColor" strokeWidth="1.5"
+                        opacity="0.5"/>
+                <path d="M18.5 18.5L22 22" stroke="currentColor" strokeWidth="1.5"
+                      strokeLinecap="round"/>
+            </svg>
+        </button>
+        <button type="button"
+                className="hover:opacity-80 sm:hidden block absolute top-1/2 -translate-y-1/2 ltr:left-2 rtl:left-2"
+                onClick={() => setSearch(false)}>
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
+                 xmlns="http://www.w3.org/2000/svg">
+                <circle opacity="0.5" cx="12" cy="12" r="10" stroke="currentColor"
+                        strokeWidth="1.5"/>
+                <path d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5" stroke="currentColor"
+                      strokeWidth="1.5" strokeLinecap="round"/>
+            </svg>
+        </button>
+    </>
 
     return (
         <>
@@ -10,32 +36,27 @@ const SearchSection = () => {
                 onSubmit={() => setSearch(false)}
             >
                 <div className="relative">
-                    <input
-                        type="text"
-                        className="form-input ltr:pl-9 rtl:pr-9 ltr:sm:pr-4 rtl:sm:pl-4 ltr:pr-9 rtl:pl-9 peer sm:bg-transparent bg-gray-100 placeholder:tracking-widest"
-                        placeholder="Search.aaa.."
+                    <AsyncSelect
+                        components={{
+                            DropdownIndicator: CustomDropDownIndicator,
+                        }}
+                        styles={{
+                            control: (baseStyles, state) => ({
+                                ...baseStyles,
+                                width: '250px',  // Adjust the width as needed
+                                height: '40px', // Adjust the height as needed
+                            }),
+                        }}
+                        isMulti={false}
+                        id="callable_id"
+                        placeholder="Search..."
+                        name="callable_id"
+                        menuPortalTarget={document.body}
+                        className="flex-1"
+                        required
                     />
-                    <button type="button"
-                            className="absolute w-9 h-9 inset-0 ltr:right-auto rtl:left-auto appearance-none peer-focus:text-primary">
-                        <svg className="mx-auto" width="16" height="16" viewBox="0 0 24 24" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="11.5" cy="11.5" r="9.5" stroke="currentColor" strokeWidth="1.5"
-                                    opacity="0.5"/>
-                            <path d="M18.5 18.5L22 22" stroke="currentColor" strokeWidth="1.5"
-                                  strokeLinecap="round"/>
-                        </svg>
-                    </button>
-                    <button type="button"
-                            className="hover:opacity-80 sm:hidden block absolute top-1/2 -translate-y-1/2 ltr:right-2 rtl:left-2"
-                            onClick={() => setSearch(false)}>
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <circle opacity="0.5" cx="12" cy="12" r="10" stroke="currentColor"
-                                    strokeWidth="1.5"/>
-                            <path d="M14.5 9.50002L9.5 14.5M9.49998 9.5L14.5 14.5" stroke="currentColor"
-                                  strokeWidth="1.5" strokeLinecap="round"/>
-                        </svg>
-                    </button>
+
+
                 </div>
             </form>
         </>
