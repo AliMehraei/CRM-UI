c0e1ba58ddc33429bb21e8c03be2c8a77d829804
davood.thinker <davood.thinker@gmail.com>
2023-11-07 17:09:36 +0330

    chore: some enhancing and type handling

--- resources/js/src/pages/Search/Index.tsx
+++ resources/js/src/pages/Search/Index.tsx
@@ -1,19 +1,14 @@
-
-import { NavLink } from 'react-router-dom';
-import { useEffect } from 'react';
-import { setPageTitle } from '../../store/themeConfigSlice';
-import { formatDate } from "@fullcalendar/core";
-import { useDispatch } from "react-redux";
-import { Sidebar } from './components/Sidebar';
-import { SearchBar } from './components/SearchBar';
+import {useEffect} from 'react';
+import {setPageTitle} from '../../store/themeConfigSlice';
+import {useDispatch} from "react-redux";
+import Sidebar from './components/Sidebar';
+import SearchBar from "./components/SearchBar";
 
 const Index = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(setPageTitle('Search'));
     }, [dispatch]);
-
-
 
 
     return (
@@ -24,12 +19,12 @@ const Index = () => {
 
                     <div className="grid grid-cols-7 gap-6 mb-6">
                         <div className='panel col-span-1'>
-                        <Sidebar />
+                            <Sidebar/>
                         </div>
                         <div className="panel col-span-6 border rounded-lg shadow-lg bg-white p-5">
-                        <SearchBar />
+                            <SearchBar/>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
--- resources/js/src/pages/Search/components/Sidebar.tsx
+++ resources/js/src/pages/Search/components/Sidebar.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { SidebarItem } from './SidebarItem';
+import SidebarItem from './SidebarItem';
 
 const sidebarItems = [
-  { name: 'All Apps', icon: '/assets/images/app.svg', isActive: false },
-  // ... other items
-  { name: 'CRM', icon: '/assets/images/sascrm.svg', isActive: true },
+    {name: 'All Apps', icon: '/assets/images/app.svg', isActive: false},
+    // ... other items
+    {name: 'CRM', icon: '/assets/images/sascrm.svg', isActive: true},
 ];
 
-export const Sidebar: React.FC = () => {
-  return (
-    <div className="min-h-screen">
-      {sidebarItems.map((item) => (
-        <SidebarItem key={item.name} name={item.name} icon={item.icon} isActive={item.isActive} />
-      ))}
-    </div>
-  );
+const Sidebar = () => {
+    return (
+        <div className="min-h-screen">
+            {sidebarItems.map((item) => (
+                <SidebarItem key={item.name} name={item.name} icon={item.icon} isActive={item.isActive}/>
+            ))}
+        </div>
+    );
 };
+
+export default Sidebar;
