c3863788c5c06d6841ce0fc87cd8f76c82c68fe2
yektadg.dev <yektadg.dev@gmail.com>
2023-11-07 12:55:26 +0330

    feat: added fully dynamic widget for compare with last period for saleOrder YTD and close one

--- resources/js/src/components/Reports/LastPeriodCompareWidget.tsx
+++ resources/js/src/components/Reports/LastPeriodCompareWidget.tsx
@@ -1,4 +1,34 @@
 import React from 'react';
+import LoadingSpinner from "../LoadingSpinner";
+
+const formatAmount = (amount, isCurrency, currency) => {
+    return isCurrency
+        ? new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: currency,
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }).format(amount)
+        : amount.toFixed(2);
+};
+
+const calculatePercentageChange = (current, last) => {
+    if (last === 0) return current === 0 ? 'N/A' : 'New';
+    return ((current - last) / last) * 100;
+};
+
+const ComparisonBadge = ({ value }) => {
+    const badgeClass = value > 0 ? 'badge bg-green-500' : 'badge bg-red-500';
+    const displayValue = typeof value === 'number' ? `${value.toFixed(2)}%` : value;
+    return <div className={badgeClass}>{displayValue}</div>;
+};
+
+const StatisticLine = ({ title, value, isCurrency, currency }) => (
+    <div className="flex items-center font-bold mt-2">
+        <span className="ltr:mr-2 rtl:ml-2">{title}:</span>
+        <span>{formatAmount(value, isCurrency, currency)}</span>
+    </div>
+);
 
 interface LastPeriodCompareWidgetProps {
     currentAmount: number;
@@ -6,66 +36,60 @@ interface LastPeriodCompareWidgetProps {
     sectionTitle: string;
     isCurrency?: boolean;
     currency?: string;
-    firstColor?: string;
-    secondColor?: string;
     currentTitle?: string;
     lastPeriodTitle?: string;
+    currentAverage?: number;
+    currentCount?: number;
+    lastPeriodAverage?: number;
+    lastPeriodCount?: number;
+    firstColor?: string;
+    secondColor?: string;
 }
 
 const LastPeriodCompareWidget: React.FC<LastPeriodCompareWidgetProps> = ({
-     currentAmount,
-     lastPeriodAmount,
-     sectionTitle,
-     isCurrency = true,
-     currency = 'EUR',
-     firstColor = 'blue-500',
-     secondColor = 'blue-400',
-     currentTitle = 'Current Month',
-     lastPeriodTitle = 'Last Month'
-}) => {
-    const percentageChange = ((currentAmount - lastPeriodAmount) / lastPeriodAmount) * 100;
-
-    const formatAmount = (amount: number): string => {
-        if (isCurrency) {
-            return new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: currency,
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
-            }).format(amount);
-        } else {
-            return amount.toFixed(2);
-        }
-    };
-
-    const badgeClass = percentageChange > 0 ? 'badge bg-white/30' : 'badge bg-red-400';
+                                                                             currentAmount,
+                                                                             lastPeriodAmount,
+                                                                             sectionTitle,
+                                                                             firstColor = 'cyan-500',
+                                                                             secondColor = 'cyan-400',
+                                                                             isCurrency = true,
+                                                                             currency = 'EUR',
+                                                                             currentTitle = 'Current Period',
+                                                                             lastPeriodTitle = 'Last Period',
+                                                                             currentAverage,
+                                                                             currentCount,
+                                                                             lastPeriodAverage,
+                                                                             lastPeriodCount,
+                                                                         }) => {
+    const percentageChangeAmount = calculatePercentageChange(currentAmount, lastPeriodAmount);
+    const percentageChangeCount = calculatePercentageChange(currentCount, lastPeriodCount);
+    const percentageChangeAverage = calculatePercentageChange(currentAverage, lastPeriodAverage);
     const gradientClass = `panel bg-gradient-to-r from-${firstColor} to-${secondColor}`;
+    // const gradientClass = `panel bg-gradient-to-r from-cyan-500 to-cyan-400`;
 
     return (
-        <>
-                <div className={gradientClass}>
-
-                    <div className="flex justify-between">
-                        <div className="ltr:mr-1 rtl:ml-1 text-md font-semibold">{sectionTitle}</div>
-                    </div>
-                    <div className="flex items-center mt-5">
-                        <div className="text-3xl font-bold ltr:mr-3 rtl:ml-3"> {formatAmount(currentAmount)} </div>
-                        <div className={badgeClass}> {percentageChange.toFixed(2)}%</div>
-                    </div>
-                    <div className="flex items-center font-semibold mt-5">
-                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="ltr:mr-2 rtl:ml-2">
-                            <path
-                                opacity="0.5"
-                                d="M3.27489 15.2957C2.42496 14.1915 2 13.6394 2 12C2 10.3606 2.42496 9.80853 3.27489 8.70433C4.97196 6.49956 7.81811 4 12 4C16.1819 4 19.028 6.49956 20.7251 8.70433C21.575 9.80853 22 10.3606 22 12C22 13.6394 21.575 14.1915 20.7251 15.2957C19.028 17.5004 16.1819 20 12 20C7.81811 20 4.97196 17.5004 3.27489 15.2957Z"
-                                stroke="currentColor"
-                                strokeWidth="1.5"
-                            />
-                            <path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="currentColor" strokeWidth="1.5" />
-                        </svg>
-                        {lastPeriodTitle}: {formatAmount(lastPeriodAmount)}
-                    </div>
+            <div className={gradientClass}>
+            <div className="flex justify-between">
+                <div className="text-md font-semibold">{sectionTitle}</div>
             </div>
-        </>
+            <div className="flex items-center mt-5">
+                <div className="text-3xl font-bold ltr:mr-3 rtl:ml-3">{formatAmount(currentAmount, isCurrency, currency)}</div>
+                <ComparisonBadge value={percentageChangeAmount} />
+            </div>
+            {typeof currentAverage === 'number' && typeof lastPeriodAverage === 'number' && (
+                <div className="flex items-center justify-between">
+                    <StatisticLine title={`${currentTitle} Average`} value={currentAverage} isCurrency={isCurrency} currency={currency} />
+                    <ComparisonBadge value={percentageChangeAverage} />
+                </div>
+
+            )}
+            {typeof currentCount === 'number' && typeof lastPeriodCount === 'number' && (
+                <div className="flex items-center justify-between">
+                <StatisticLine title={`${currentTitle} Count`} value={currentCount} />
+                    <ComparisonBadge value={percentageChangeCount} />
+                </div>
+            )}
+        </div>
     );
 };
 
--- resources/js/src/pages/Index.tsx
+++ resources/js/src/pages/Index.tsx
@@ -7,16 +7,45 @@ import PerfectScrollbar from 'react-perfect-scrollbar';
 import Dropdown from '../components/Dropdown';
 import { setPageTitle } from '../store/themeConfigSlice';
 import LastPeriodCompareWidget from "../components/Reports/LastPeriodCompareWidget";
+import Api from "../config/api";
+import LoadingSasCrm from "../components/LoadingSasCrm";
+import { useUserStatus } from "../config/authCheck";
+import {updateFormData} from "../store/accountFormSlice";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const Index = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(setPageTitle('CRM Admin'));
     });
+    const api = new Api();
+
+    const [loading, setLoading] = useState(true);
+    const [salesOrderData, setSalesOrderData] = useState(null);
+    const { hasPermission } = useUserStatus();
+    const fetchData = async () => {
+        try {
+            const dashboardResponse = await api.dashboardData();
+            if (dashboardResponse.status === 200) {
+                setSalesOrderData(dashboardResponse.data.data.sales_order);
+            } else {
+                console.error('Failed to fetch dashboard data:', dashboardResponse);
+            }
+        } catch (error) {
+            console.error('An error occurred while fetching dashboard data:', error);
+        } finally {
+            setLoading(false);
+
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     const isDark = useSelector((state: IRootState) => state.themeConfig.theme) === 'dark' ? true : false;
     const isRtl = useSelector((state: IRootState) => state.themeConfig.rtlClass) === 'rtl' ? true : false;
 
-    const [loading] = useState(false);
 
     //Revenue Chart
     const revenueChart: any = {
@@ -707,6 +736,9 @@ const Index = () => {
     };
 
     return (
+        (loading) ? (
+            <LoadingSasCrm />
+        ) : (
         <div>
             <ul className="flex space-x-2 rtl:space-x-reverse">
                 <li>
@@ -718,16 +750,43 @@ const Index = () => {
             </ul>
             <div className="pt-5">
                 <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6 mb-6 text-white">
-                    <LastPeriodCompareWidget
-                        currentAmount={75}
-                        lastPeriodAmount={60}
-                        sectionTitle="User Engagement"
-                        isCurrency={false}
-                        firstColor="violet-500"
-                        secondColor="violet-400"
-                        currentTitle="Active Users"
-                        lastPeriodTitle="Previous Period"
-                    />
+
+                    {salesOrderData ? (
+                        <>
+                            <LastPeriodCompareWidget
+                                currentAmount={salesOrderData.ytd?.total_so}
+                                lastPeriodAmount={salesOrderData.last_year?.total_so}
+                                sectionTitle="Sales Order YTD"
+                                firstColor="blue-500"
+                                secondColor="blue-400"
+                                currentTitle="This Year"
+                                lastPeriodTitle="Last Year"
+                                currentAverage={parseFloat(salesOrderData.ytd?.average_so ?? "0")}
+                                currentCount={salesOrderData.ytd?.count_so}
+                                lastPeriodAverage={parseFloat(salesOrderData.last_year?.average_so ?? "0")}
+                                lastPeriodCount={salesOrderData.last_year?.count_so}
+                            />
+                            <LastPeriodCompareWidget
+                                currentAmount={salesOrderData.ytd?.closed_so?.total_so}
+                                lastPeriodAmount={salesOrderData.last_year?.closed_so?.total_so}
+                                sectionTitle="Sales Order (Closed) YTD"
+                                firstColor="yellow-500"
+                                secondColor="yellow-400"
+                                currentTitle="This Year"
+                                lastPeriodTitle="Last Year"
+                                currentAverage={parseFloat(salesOrderData.ytd?.closed_so?.average_so ?? "0")}
+                                currentCount={salesOrderData.ytd?.closed_so?.count_so}
+                                lastPeriodAverage={parseFloat(salesOrderData.last_year?.closed_so?.average_so ?? "0")}
+                                lastPeriodCount={salesOrderData.last_year?.closed_so?.count_so}
+                            />
+                        </>
+                    ) : (
+                        <LoadingSpinner />
+                    )}
+
+
+
+
                     <LastPeriodCompareWidget
                         currentAmount={75}
                         lastPeriodAmount={60}
@@ -738,16 +797,7 @@ const Index = () => {
                         currentTitle="Active Users"
                         lastPeriodTitle="Previous Period"
                     />
-                    <LastPeriodCompareWidget
-                        currentAmount={7500}
-                        lastPeriodAmount={7000}
-                        sectionTitle="Sales"
-                        currency="USD"
-                        firstColor="blue-500"
-                        secondColor="blue-400"
-                        currentTitle="This Quarter"
-                        lastPeriodTitle="Last Quarter"
-                    />
+
                     <LastPeriodCompareWidget
                         currentAmount={7500}
                         lastPeriodAmount={7000}
@@ -2377,6 +2427,7 @@ const Index = () => {
             </div>
 
         </div>
+        )
     );
 };
 
