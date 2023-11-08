85e7a2f48cfe869c320cb0e159610fa7f2f98116
davood.thinker <davood.thinker@gmail.com>
2023-11-08 09:31:03 +0330

    chore: handled style for OpenQuotes

--- resources/js/src/pages/Dashboard/components/OpenQuotes.tsx
+++ resources/js/src/pages/Dashboard/components/OpenQuotes.tsx
@@ -5,154 +5,6 @@ import LoadingSpinner from "../../../components/LoadingSpinner";
 const OpenQuotes = () => {
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState(null);
-    const revenueChart: any = {
-        series: [
-            {
-                name: 'Income',
-                data: [16800, 16800, 15500, 17800, 15500, 17000, 19000, 16000, 15000, 17000, 14000, 17000],
-            },
-            {
-                name: 'Expenses',
-                data: [16500, 17500, 16200, 17300, 16000, 19500, 16000, 17000, 16000, 19000, 18000, 19000],
-            },
-        ],
-        options: {
-            chart: {
-                height: 325,
-                type: 'area',
-                fontFamily: 'Nunito, sans-serif',
-                zoom: {
-                    enabled: false,
-                },
-                toolbar: {
-                    show: false,
-                },
-            },
-
-            dataLabels: {
-                enabled: false,
-            },
-            stroke: {
-                show: true,
-                curve: 'smooth',
-                width: 2,
-                lineCap: 'square',
-            },
-            dropShadow: {
-                enabled: true,
-                opacity: 0.2,
-                blur: 10,
-                left: -7,
-                top: 22,
-            },
-            colors: ['#1B55E2', '#E7515A'],
-            markers: {
-                discrete: [
-                    {
-                        seriesIndex: 0,
-                        dataPointIndex: 6,
-                        fillColor: '#1B55E2',
-                        strokeColor: 'transparent',
-                        size: 7,
-                    },
-                    {
-                        seriesIndex: 1,
-                        dataPointIndex: 5,
-                        fillColor: '#E7515A',
-                        strokeColor: 'transparent',
-                        size: 7,
-                    },
-                ],
-            },
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            xaxis: {
-                axisBorder: {
-                    show: false,
-                },
-                axisTicks: {
-                    show: false,
-                },
-                crosshairs: {
-                    show: true,
-                },
-                labels: {
-                    offsetX: 0,
-                    offsetY: 5,
-                    style: {
-                        fontSize: '12px',
-                        cssClass: 'apexcharts-xaxis-title',
-                    },
-                },
-            },
-            yaxis: {
-                tickAmount: 7,
-                labels: {
-                    formatter: (value: number) => {
-                        return value / 1000 + 'K';
-                    },
-                    offsetX: -10,
-                    offsetY: 0,
-                    style: {
-                        fontSize: '12px',
-                        cssClass: 'apexcharts-yaxis-title',
-                    },
-                },
-                opposite: false,
-            },
-            grid: {
-                borderColor: '#E0E6ED',
-                strokeDashArray: 5,
-                xaxis: {
-                    lines: {
-                        show: true,
-                    },
-                },
-                yaxis: {
-                    lines: {
-                        show: false,
-                    },
-                },
-                padding: {
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    left: 0,
-                },
-            },
-            legend: {
-                position: 'top',
-                horizontalAlign: 'right',
-                fontSize: '16px',
-                markers: {
-                    width: 10,
-                    height: 10,
-                    offsetX: -2,
-                },
-                itemMargin: {
-                    horizontal: 10,
-                    vertical: 5,
-                },
-            },
-            tooltip: {
-                marker: {
-                    show: true,
-                },
-                x: {
-                    show: false,
-                },
-            },
-            fill: {
-                type: 'gradient',
-                gradient: {
-                    shadeIntensity: 1,
-                    inverseColors: !1,
-                    opacityFrom: 0.28,
-                    opacityTo: 0.05,
-                    stops: [45, 100],
-                },
-            },
-        },
-    };
 
     // const fetchData = async () => {
     //     try {
@@ -196,8 +48,12 @@ const OpenQuotes = () => {
             colors: ['#ff8b60'],
             dataLabels: {
                 enabled: true,
-                formatter: function (val:) {
-                    return val + "%";
+                formatter: function (val: any) {
+                    return new Intl.NumberFormat('en-US', {
+                        style: 'currency',
+                        currency: 'EUR',
+                        minimumFractionDigits: 2,
+                    }).format(val);
                 },
                 offsetY: -20,
                 style: {
@@ -215,6 +71,9 @@ const OpenQuotes = () => {
                     horizontal: false,
                     columnWidth: '55%',
                     endingShape: 'rounded',
+                    dataLabels: {
+                        position: 'top',
+                    }
                 },
             },
             grid: {
@@ -242,7 +101,11 @@ const OpenQuotes = () => {
                 theme: 'light',
                 y: {
                     formatter: function (val: any) {
-                        return val;
+                        return new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'EUR',
+                            minimumFractionDigits: 2,
+                        }).format(val);
                     },
                 },
             },
