7a696a350547393e0d599c47f8d20550c6f4976b
davood.thinker <davood.thinker@gmail.com>
2023-11-08 12:28:37 +0330

    chore: added BillingYtdSales

--- resources/js/src/pages/Index.tsx
+++ resources/js/src/pages/Index.tsx
@@ -14,6 +14,7 @@ import {updateFormData} from "../store/accountFormSlice";
 import LoadingSpinner from "../components/LoadingSpinner";
 import OpenQuotes from "./Dashboard/components/OpenQuotes";
 import BookingRevenue from "./Dashboard/components/BookingRevenue";
+import BillingYtdSales from "./Dashboard/components/BillingYtdSales";
 
 const Index = () => {
     const dispatch = useDispatch();
@@ -152,7 +153,7 @@ const Index = () => {
                 <div className="flex flex-row gap-5">
 
                     <BookingRevenue />
-                    <BookingRevenue/>
+                    <BillingYtdSales/>
                 </div>
             </div>
         )
