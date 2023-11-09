0687a7a7d8fbefb14e143a9d29b948a9c4bf9388
davood.thinker <davood.thinker@gmail.com>
2023-11-09 10:48:09 +0330

    chore:  load owner and soe detail in view

--- resources/js/src/pages/Search/components/SearchResultItem.tsx
+++ resources/js/src/pages/Search/components/SearchResultItem.tsx
@@ -8,13 +8,20 @@ const SearchResultItem = ({ item, onSelect }:any) => {
     return (
         <>
         {item[group].map((result:any, index:any) => (
-                <div
+                <div key={index}
                     onClick={() => onSelect(result)}
                     className="p-2 hover:bg-gray-100 cursor-pointer"
                 >
                     <h4 className="text-sm font-semibold">{result.model_label_field}</h4>
-                    <p className="text-xs text-gray-600">{group}</p>
-                    {/* Add more item details here */}
+                    <div className="grid grid-cols-6 gap-6 mb-6">
+                        <div className='col-span-3'>
+                    <p className="text-xs text-gray-600">{result.owner?.first_name} {result.owner?.last_name}</p>
+                            <p className="text-xs text-gray-600">{result.created_at}</p>
+                        </div>
+                        <div className="relative col-span-3 ">
+                            <h4 className="absolute right-0 text-xs font-semibold ">{group}</h4>
+                        </div>
+                    </div>
                 </div>
             ))}
         </>
