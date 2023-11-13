775554e3cbc8acf1295c8a6ca252c0085046e6a9
davood.thinker <davood.thinker@gmail.com>
2023-11-13 10:08:00 +0330

    chore: changed value for select options

--- resources/js/src/pages/Search/components/FilterModal.tsx
+++ resources/js/src/pages/Search/components/FilterModal.tsx
@@ -20,11 +20,11 @@ const FilterModal: React.FC<FilterModalProps> = ({isOpen, onClose, onApply}: any
         // onApply(defaultFilters);
     };
     const ModuleOptions: any = [
-        {value: 'All Modules', label: 'All Modules'},
-        {value: 'Invoices', label: 'Invoices'},
-        {value: 'Sales Orders', label: 'Sales Orders'},
-        {value: 'Purchase order', label: 'Purchase order'},
-        {value: 'RFQ', label: 'RFQ'},
+        {value: 'All', label: 'All Modules'},
+        {value: 'Invoice', label: 'Invoices'},
+        {value: 'SalesOrder', label: 'Sales Orders'},
+        {value: 'PurchaseOrder', label: 'Purchase order'},
+        {value: 'Rfq', label: 'RFQ'},
         {value: 'Quote', label: 'Quote'},
         {value: 'Availability', label: 'Availability'},
         {value: 'Account', label: 'Account'},
@@ -35,7 +35,7 @@ const FilterModal: React.FC<FilterModalProps> = ({isOpen, onClose, onApply}: any
 
         {value: 'Call', label: 'Call'},
         {value: 'Product', label: 'Product'},
-        {value: 'VendorRFQ', label: 'VendorRFQ'},
+        {value: 'VendorRfq', label: 'VendorRFQ'},
         {value: 'Tasks', label: 'Tasks'},
         {value: 'Manufacturer', label: 'Manufacturer'},
         {value: 'Excess', label: 'Excess'},
@@ -44,7 +44,7 @@ const FilterModal: React.FC<FilterModalProps> = ({isOpen, onClose, onApply}: any
     const [selectedOptions, setSelectedOptions] = useState<any>([]);
     const handleChangeFilters = (selection: any, actionMeta: any) => {
         // When "All Modules" is selected, ensure all options are selected
-        const isAllModulesSelected = selection.some((option: any) => option.value === 'All Modules');
+        const isAllModulesSelected = selection.some((option: any) => option.value === 'All');
         if (isAllModulesSelected) {
             setSelectedOptions(ModuleOptions);
             return;
