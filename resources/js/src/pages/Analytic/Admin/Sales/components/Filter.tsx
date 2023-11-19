3138a4511ae2207a105f97fe2b4ac5828bb9f4a2
davood.thinker <davood.thinker@gmail.com>
2023-11-19 18:14:27 +0330

    style: change btn filter in sales order

--- resources/js/src/pages/Analytic/Admin/Sales/components/Filter.tsx
+++ resources/js/src/pages/Analytic/Admin/Sales/components/Filter.tsx
@@ -29,76 +29,6 @@ const Filter = ({setDateStartFilter,setDateEndFilter,setStatusFilter,setPersonFi
 
     return (
         <>
-        {/*<div className="mb-10 mt-3 flex md:items-center md:flex-row flex-col gap-5">*/}
-        {/*        <div className="flex items-center gap-5">*/}
-        {/*            /!* First Multi-Select *!/*/}
-        {/*            <label htmlFor="firstSelect" className="block text-[16px] font-semibold text-gray-800">*/}
-        {/*                Select Status :*/}
-        {/*            </label>*/}
-        {/*            <div className="md:flex-auto flex-1 ">*/}
-        {/*                <Select*/}
-        {/*                    isMulti*/}
-        {/*                    options={StatusSalesOrderOption}*/}
-        {/*                    name="status"*/}
-        {/*                    id="status"*/}
-        {/*                    value={selectedOptionsFirst}*/}
-        {/*                    onChange={handleFirstSelectChange}*/}
-        {/*                    className="w-48"*/}
-        {/*                    placeholder="Select Status"*/}
-
-        {/*                />*/}
-
-        {/*            </div>*/}
-
-
-        {/*            <label className="block text-[16px] font-semibold text-gray-800">*/}
-        {/*                Select Sales Person :*/}
-        {/*            </label>*/}
-        {/*            /!* Second Multi-Select *!/*/}
-        {/*            <div className="md:flex-auto flex-1">*/}
-        {/*                <AsyncSelect*/}
-        {/*                    defaultOptions={true}*/}
-        {/*                    isMulti*/}
-        {/*                    id="owner_id"*/}
-        {/*                    placeholder="Select Sales Person"*/}
-        {/*                    loadOptions={searchOwners}*/}
-        {/*                    onChange={handleSecondSelectChange}*/}
-        {/*                    className="flex-1 w-60"*/}
-        {/*                />*/}
-
-        {/*        </div>*/}
-        {/*        </div>*/}
-
-
-        {/*    <div className="flex ltr:ml-auto rtl:mr-auto">*/}
-        {/*        <div className="mr-4 mt-2">*/}
-        {/*        <label className="block text-[16px] font-semibold text-gray-800">*/}
-        {/*            Select Range Date :*/}
-        {/*        </label>*/}
-        {/*        </div>*/}
-        {/*        <div className="items-center">*/}
-        {/*            <Flatpickr*/}
-        {/*                options={{*/}
-        {/*                    mode: 'range',*/}
-        {/*                    dateFormat: 'Y-m-d',*/}
-        {/*                    position: 'auto left',*/}
-
-        {/*                }}*/}
-        {/*                value={date3}*/}
-        {/*                className="form-input w-56 flex-1"*/}
-        {/*                onChange={(date3) => setDate3(date3)}*/}
-        {/*            />*/}
-        {/*        </div>*/}
-        {/*    </div>*/}
-        {/*    <button*/}
-        {/*        type="button"*/}
-        {/*        onClick={handleFilterClick}*/}
-        {/*        className="ml-4 inline-flex items-center px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"*/}
-        {/*    >*/}
-        {/*        Filter*/}
-        {/*    </button>*/}
-        {/*</div>*/}
-
             <div className="panel  border-white-light dark:border-[#1b2e4b]">
                 <div className="grid sm:grid-cols-12 md:grid-cols-4 gap-3 mb-10">
 
@@ -156,7 +86,7 @@ const Filter = ({setDateStartFilter,setDateEndFilter,setStatusFilter,setPersonFi
                             />
 
                     </div>
-                        <div className="inline-block lg:justify-end  md:mt-4">
+                        <div className="inline-block lg:justify-end  lg:mt-36">
                         <button
                             type="button"
                             onClick={handleFilterClick}
