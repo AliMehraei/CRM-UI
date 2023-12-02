f58ae56f8504a16db1f4fb712f8039b274608209
davood.thinker <davood.thinker@gmail.com>
2023-12-02 08:44:44 +0330

    chore: working list

--- resources/js/src/pages/Analytic/Admin/Vendor/components/ListVendor.tsx
+++ resources/js/src/pages/Analytic/Admin/Vendor/components/ListVendor.tsx
@@ -2,13 +2,76 @@ import ReactApexChart from "react-apexcharts";
 import {useEffect, useState} from "react";
 import LoadingSpinner from "../../../../../components/LoadingSpinner";
 import Api from "../../../../../config/api";
+import GenerateTableList from "../../../../../components/FilterFields/GenerateTableList";
 
 const ListVendor = ({dateStartFilter,dateEndFilter,statusFilter,vendorFilter}:any) => {
     const [loading, setLoading] = useState(true);
     const api_instance = new Api();
     
     const [filterChange, setFilterChange] = useState(false);
-    const [chartData, setChartData] = useState<any>(columnChart);
+    const columns: any = [
+        {
+            accessor: 'id',
+            sortable: true,
+            render: ({ id }) => <div className="font-semibold">{id}</div>,
+        },
+        {
+            accessor: 'vendor Name',
+            sortable: true,
+            render: ({ vendor_name, id }) => (
+                hasPermission('update-vendor') ? (
+                    <NavLink to={`/vendor/edit/${id}`}>
+                        <div className="text-primary underline hover:no-underline font-semibold">{`#${vendor_name}`}</div>
+                    </NavLink>
+                ) : (
+                    <div className="font-semibold">{`#${vendor_name}`}</div>
+                )
+            )
+        },
+        {
+            accessor: 'Manufacturer Name',
+            title: 'Manufacturer Name',
+            sortable: true,
+            render: ({ manufacturer_name }) => (
+                <div className="font-semibold">{`${manufacturer_name}`}</div>
+            ),
+        },
+        {
+            accessor: 'sent_rfqs_count',
+            title: 'Sent RFQs count',
+            sortable: false,
+            render: ({ sent_rfqs_count }) => (
+                <div className="font-semibold">
+                    {sent_rfqs_count}
+                </div>
+            ),
+        },
+        {
+            accessor: 'sent_v_rfqs_count',
+            title: 'Sent VRFQs Count',
+            sortable: true,
+            render: ({ sent_v_rfqs_count }) => {
+               
+                return (
+                    <div className="font-semibold">
+                        {sent_v_rfqs_count}
+                    </div>
+                );
+            },
+        },
+        {
+            accessor: 'received_avas_count',
+            title: 'Received AVAs Count',
+            sortable: true,
+            render: ({ received_avas_count }) => (
+                <div className="font-semibold">
+                    {received_avas_count}
+                </div>
+            ),
+        },
+
+
+    ];
 
     const fetchData = async () => {
         try {
@@ -43,16 +106,17 @@ const ListVendor = ({dateStartFilter,dateEndFilter,statusFilter,vendorFilter}:an
             <div className="grid  gap-6 mb-6">
                 <div className="panel h-full xl:col-span-2">
                     <div className="relative">
-                        <h5 className="font-semibold text-lg">{filterChange === false ? 'YTD Sales' : 'Filtered Sales Performance Analysis'}</h5>
+                        <h5 className="font-semibold text-lg">List Vendor</h5>
 
                         <div className="bg-white dark:bg-black rounded-lg">
                             {loading ? (
                                 <LoadingSpinner/>
                             ) : (
-                                <ReactApexChart
-                                    series={chartData.series}
-                                    options={chartData.options as any}
-                                    className="rounded-lg bg-white dark:bg-black" type="bar" height={300}/>
+                                <GenerateTableList 
+                                    permissionName="admin-vendor-list-analytics"
+                                    tableColumns={columns} 
+                                    frontRoute="reportVendorList" 
+                                 />
                             )}
                         </div>
                     </div>
