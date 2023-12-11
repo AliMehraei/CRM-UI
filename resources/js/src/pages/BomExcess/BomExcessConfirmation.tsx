5288b5b77ddc9c7f1f067830cdd0d4e5a5c5fbe8
davood.thinker <davood.thinker@gmail.com>
2023-12-11 12:12:39 +0330

    feat: add Reload the sample data in confirmation bom

--- resources/js/src/pages/BomExcess/BomExcessConfirmation.tsx
+++ resources/js/src/pages/BomExcess/BomExcessConfirmation.tsx
@@ -182,11 +182,13 @@ const BomExcessConfirmation = () => {
                             <div className="">
                                 <div className="flex flex-col h-full">
                                     <div className="flex flex-row items-center justify-between h-full space-x-3">
-                                        <h3 className="text-lg font-medium leading-6 text-gray-900">
+                                        <h3 className="text-lg font-bold leading-6 text-gray-900">
                                             Step 1: Match your column names
                                         </h3>
                                         <div className="flex justify-start">
-                                            <button id="btn-reload-data-tour" className="bg-primary-500 cursor-pointer flex hover:bg-primary-600 items-center px-2 py-1 rounded space-x-1 text-sm text-white">
+                                            <button id="btn-reload-data-tour" onClick={ () => window.location.reload()}
+                                            className="bg-sky-700 cursor-pointer flex hover:bg-primary-600 items-center 
+                                            px-2 py-1 rounded space-x-1 text-base text-white">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"></path>
                                                 </svg>
