57c3870d57154e0fb84783e21e70b4b56433b07f
davood.thinker <davood.thinker@gmail.com>
2023-12-27 11:49:47 +0330

    fix: clear filter all date for report vendor

--- resources/js/src/pages/Analytic/Admin/Vendor/components/Filter.tsx
+++ resources/js/src/pages/Analytic/Admin/Vendor/components/Filter.tsx
@@ -285,8 +285,6 @@ const Filter = ({
                                 setDateAvailability('');
                                 setDateAvailabilityStartFilterTemp('');
                                 setDateAvailabilityEndFilterTemp('');
-                                setDateAvailabilityStartFilter('');
-                                setDateAvailabilityEndFilter('');
 
                             }}>
                                 <svg width="35" height="35" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
