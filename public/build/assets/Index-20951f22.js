bebbfb1f1c9f3b0e88753cffe540b1050202c57d
davood.thinker <davood.thinker@gmail.com>
2024-01-19 10:59:16 +0330

    build: react

---
+++ public/build/assets/Index-20951f22.js
@@ -0,0 +1,1 @@
+import{u as t,c as a,r as e,s as r,j as c,F as u}from"./main-5d4f75a1.js";const o=()=>{const s=t();return a(),e.useEffect(()=>{s(r("Task List"))},[s]),c(u,{})};export{o as default};
---
+++ public/build/assets/react-select.esm-2d1c0dcd.js
@@ -0,0 +1,1 @@
+import{r as e,ar as s,as as n,ah as S}from"./main-5d4f75a1.js";var c=e.forwardRef(function(a,t){var r=s(a);return e.createElement(n,S({ref:t},r))}),f=c;export{f as S};
