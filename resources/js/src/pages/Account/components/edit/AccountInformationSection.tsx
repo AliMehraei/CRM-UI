0e4941db8f4e4c365fc0698e036b7aa66b72e3e5
davood.thinker <davood.thinker@gmail.com>
2024-01-19 09:27:57 +0330

    fix: account

--- resources/js/src/pages/Account/components/edit/AccountInformationSection.tsx
+++ resources/js/src/pages/Account/components/edit/AccountInformationSection.tsx
@@ -201,7 +201,7 @@ const AccountInformationSection = () => {
                 name="d_check"
                 className="form-checkbox"
                 onChange={(e:any) => handleChangeField(e.target.name, e.target.checked)}
-                checked={formState.has_no_contacts}
+                checked={formState.d_check}
             />,
 
         }
