dd62458588729c49a34f7feb081d5ca414d7cfc1
davood.thinker <davood.thinker@gmail.com>
2024-01-22 12:11:40 +0330

    build: react

--- public/build/assets/Index-0485532a.js
+++
@@ -1,1 +0,0 @@
-import{u as t,c as a,r as e,s as r,j as c,F as u}from"./main-016d45e2.js";const o=()=>{const s=t();return a(),e.useEffect(()=>{s(r("Task List"))},[s]),c(u,{})};export{o as default};
---
+++ public/build/assets/Index-34db9be9.js
@@ -0,0 +1,1 @@
+import{u as t,c as a,r as e,s as r,j as c,F as u}from"./main-47e64be7.js";const o=()=>{const s=t();return a(),e.useEffect(()=>{s(r("Task List"))},[s]),c(u,{})};export{o as default};
