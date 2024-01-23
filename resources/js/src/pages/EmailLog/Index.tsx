19bccbacd0681c1c93ee461a2f9f08145d7ff1a1
davood.thinker <davood.thinker@gmail.com>
2024-01-23 17:14:07 +0330

    fix: fix name of status

--- resources/js/src/pages/EmailLog/Index.tsx
+++ resources/js/src/pages/EmailLog/Index.tsx
@@ -62,7 +62,7 @@ const List = () => {
                 {status === 'pending' && (
                     <span className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">{status}</span>
                 )}
-                {status === 'opend' && (
+                {status === 'opened' && (
                     <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">{status}</span>
                 )}
                 </div>
