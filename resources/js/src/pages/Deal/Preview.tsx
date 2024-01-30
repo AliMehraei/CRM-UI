def87dd027ca899be881f4d711f2cfebddbb14c7
davood.thinker <davood.thinker@gmail.com>
2024-01-30 09:36:25 +0330

    feat: add email log in preview deal

--- resources/js/src/pages/Deal/Preview.tsx
+++ resources/js/src/pages/Deal/Preview.tsx
@@ -17,8 +17,8 @@ import InformationSectionPreview from "../../components/Preview/InformationSecti
 import MultipleLineSectionPreview from "../../components/Preview/MultipleLineSectionPreview";
 import AttachmentSection from "../../components/FormFields/AttachmentSection";
 import { ViewIcon } from "../../components/FormFields/CommonIcons";
-import ExtraEmailLogDataSectionPreview from "../../components/Preview/ExtraEmailLogDataSectionPreview";
-import TableSectionPreview from "../../components/Preview/TableSectionPreview";
+import GenerateEmailLogList from "../../components/FilterFields/GenerateEmailLogList";
+
 
 const Preview = () => {
     const { hasPermission } = useUserStatus();
@@ -28,14 +28,7 @@ const Preview = () => {
     const modelId = params.id;
     const api = new Api();
     const formState = useSelector((state: any) => state.dealForm);
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
-    interface ExtraEmailLogInformation {
-        leftObjects: { label: string; value: string }[];
-        rightObjects: { label: string; value: string }[];
-    }
-
-    const [selectedItemEmailLog, setSelectedItemEmailLog] =
-        useState<ExtraEmailLogInformation | null>(null);
+    
 
     useEffect(() => {
         dispatch(setPageTitle("Deal Preview"));
@@ -76,117 +69,6 @@ const Preview = () => {
             value: `${formState.modifier?.first_name ?? ""} ${
                 formState.modifier?.last_name ?? ""
             }`,
-        },
-    ];
-
-    let extraEmailLogInformation: ExtraEmailLogInformation = {
-        leftObjects: [],
-        rightObjects: [],
-    };
-    const handleShowExtDataEmailLog = (item: any) => {
-        extraEmailLogInformation = {
-            leftObjects: [
-                {
-                    label: "Sender Name",
-                    value: `${(item.sender_name ?? "").substring(0, 20)}${
-                        item.sender_name && item.sender_name.length > 15
-                            ? "..."
-                            : ""
-                    }`,
-                },
-                { label: "Is Open", value: `${item.is_open ?? ""}` },
-                {
-                    label: "Creator",
-                    value: `${item.creator?.first_name ?? ""} ${
-                        item.creator?.last_name ?? ""
-                    }`,
-                },
-                {
-                    label: "Modifier",
-                    value: `${item.modifier?.first_name ?? ""} ${
-                        item.modifier?.last_name ?? ""
-                    }`,
-                },
-                { label: "Mail Status", value: `${item.mail_status ?? ""}` },
-            ],
-            rightObjects: [
-                {
-                    label: "Owner",
-                    value: `${item.owner?.first_name ?? ""} ${
-                        item.owner?.last_name ?? ""
-                    }`,
-                },
-                { label: "Sent At", value: `${item.sent_at ?? ""}` },
-                {
-                    label: "Tracking Uuid",
-                    value: `${item.tracking_uuid ?? ""}`,
-                },
-                { label: "Type", value: `${item.type ?? ""}` },
-                { label: "Opened At", value: `${item.opened_at ?? ""}` },
-            ],
-        };
-
-        setSelectedItemEmailLog(extraEmailLogInformation);
-
-        setIsPopupOpen(true);
-    };
-
-    const handleClosePopup = () => {
-        setIsPopupOpen(false);
-    };
-
-    const emailLogColumns = [
-        {
-            label: "Subject",
-            key: "subject",
-        },
-
-        {
-            label: "Receiver Mail",
-            key: "receiver_mail",
-        },
-        {
-            label: "Type - Class",
-            key: "type",
-        },
-        {
-            label: "Sender Mail",
-            key: "sender_email",
-        },
-
-        {
-            label: "Sender Type",
-            key: "sender_type",
-        },
-
-        {
-            label: "Status",
-            key: "status",
-        },
-
-        {
-            label: "Created time",
-            key: "created_at",
-            format: (value) => {
-                // Format the date to 'YYYY-MM-DD'
-                const formattedDate = new Date(value)
-                    .toISOString()
-                    .split("T")[0];
-                return formattedDate;
-            },
-        },
-
-        {
-            label: "Action",
-            key: "action",
-            render: (item) => (
-                <div
-                    onClick={() => handleShowExtDataEmailLog(item)}
-                    className="flex hover:text-info"
-                >
-                    <ViewIcon />
-                </div>
-            ),
         },
     ];
 
@@ -306,52 +188,13 @@ const Preview = () => {
                 <hr className="border-white-light dark:border-[#1b2e4b] my-6" />
                 <AttachmentSection modelId={modelId} modelName={"deal"} />
                 <hr className="border-white-light dark:border-[#1b2e4b] my-6" />
-                <div>
-                    <TableSectionPreview
-                        title="Email Logs"
-                        items={formState.emaillogs ?? []}
-                        columns={emailLogColumns}
-                    />
-                    {isPopupOpen && selectedItemEmailLog && (
-                        <div
-                            className={`fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 bg-white shadow-md rounded z-50 border  shadow-gray-300`}
-                        >
-                            <div
-                                className="absolute top-2 right-2 cursor-pointer"
-                                onClick={handleClosePopup}
-                            >
-                                <svg
-                                    className="w-6 h-6 text-gray-600 hover:text-red-600"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    ></path>
-                                </svg>
-                            </div>
-                            <ExtraEmailLogDataSectionPreview
-                                title="Email Log"
-                                leftObjects={selectedItemEmailLog.leftObjects}
-                                rightObjects={selectedItemEmailLog.rightObjects}
-                            />
-
-                            <div className="mt-auto px-2 py-3 sm:flex sm:flex-row-reverse sm:px-3 my-5">
-                                <button
-                                    className="fixed bottom-5 w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                                    onClick={handleClosePopup}
-                                >
-                                    Close
-                                </button>
-                            </div>
-                        </div>
-                    )}
-                </div>
+                <GenerateEmailLogList
+                    permissionName="read-deal"
+                    type="email-log"
+                    routeName="fetchDealEmailLogs"
+                    modelId={modelId}
+                    title="Email logs"
+                />
             </div>
         </div>
     );
