b0fa7d4073dc3d1f5f938029ddb3d47f7cad4638
davood.thinker <davood.thinker@gmail.com>
2024-01-30 12:38:42 +0330

    fix: generate call list

--- resources/js/src/components/FilterFields/GenerateCallList.tsx
+++ resources/js/src/components/FilterFields/GenerateCallList.tsx
@@ -61,28 +61,6 @@ const GenerateCallList = ({
         leftObjects: [],
         rightObjects: [],
     };    
-    // const showCallLog = (id: any = null) => {
-    //     Swal.fire({
-    //         title: "<strong>HTML <u>example</u></strong>",
-    //         icon: "info",
-    //         html: `
-    //           You can use <b>bold text</b>,
-    //           <a href="#">links</a>,
-    //           and other HTML tags
-    //         `,
-    //         showCloseButton: true,
-    //         showCancelButton: true,
-    //         focusConfirm: false,
-    //         confirmButtonText: `
-    //           <i class="fa fa-thumbs-up"></i> Great!
-    //         `,
-    //         confirmButtonAriaLabel: "Thumbs up, great!",
-    //         cancelButtonText: `
-    //           <i class="fa fa-thumbs-down"></i>
-    //         `,
-    //         cancelButtonAriaLabel: "Thumbs down"
-    //       });
-    // };
 
     const setMoreDataCallLog = (callLog: any) => {
 
