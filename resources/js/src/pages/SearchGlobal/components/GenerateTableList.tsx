b304bb5e75353f807cce023698a1495045407512
davood.thinker <davood.thinker@gmail.com>
2024-02-15 11:40:46 +0330

    fix: key for mapping

--- resources/js/src/pages/SearchGlobal/components/GenerateTableList.tsx
+++ resources/js/src/pages/SearchGlobal/components/GenerateTableList.tsx
@@ -201,7 +201,7 @@ const GenerateTableList = ({
                         const labelField = relatedValue?.[field.relation_model?.label_field];
                         const truncatedSubject = labelField ? labelField.slice(0, 20) : '';                        
                         const modelUser = ['owner','pmUser','approvedBy','modifier','creator','convertedBy','modifiedBy','createdBy','sales_person'];
-                        
+
                         if (modelUser.includes(field.relation_model?.model)){
                             content = (
                                     <div className="font-semibold">{`${owner?.[field.relation_model?.label_field]}`}</div>
@@ -421,7 +421,7 @@ const GenerateTableList = ({
 
                                                 const columns = prepareColumns(modelName);
 
-                                                return <TableListModule
+                                                return <TableListModule key={`${modelName}-${index}`}
                                                     columns={columns} modelArray={modelArray}
                                                     modelName={modelName} showSettingColumns={showSettingColumns}
                                                     selectedModel={selectedModel} searchColumns={searchColumns}
